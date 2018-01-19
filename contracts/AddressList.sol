f1f0439e36bd8bb2c4af9c23731cfb0d0337a15f
Benjamin Cosman <bcosman@caltech.edu>
2018-01-18 21:44:45 -0800

    Addresses "The changeWhiteList function..."

--- contracts/AddressList.sol
+++ contracts/AddressList.sol
@@ -16,7 +16,9 @@ contract AddressList is Ownable {
     // or not cannot be set here - it is set once and for all by the constructor.
     function changeList(address _to, bool _onList) onlyOwner public {
         require(_to != 0x0);
-        onList[_to] = _onList;
-        ChangeWhiteList(_to, _onList);
+        if (onList[_to] != _onList) {
+            onList[_to] = _onList;
+            ChangeWhiteList(_to, _onList);
+        }
     }
 }
