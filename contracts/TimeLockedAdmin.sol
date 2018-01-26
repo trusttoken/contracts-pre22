b0da3ef6e28040f32ac1663c92e8e77ea4b5b92d
Benjamin Cosman <bcosman@caltech.edu>
2018-01-26 00:45:13 -0800

    update insurance fees. fix events. owner can mimic admin without delay

--- contracts/TimeLockedAdmin.sol
+++ contracts/TimeLockedAdmin.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.18;
 
-import 'zeppelin-solidity/contracts/ownership/HasNoEther.sol';
-import 'zeppelin-solidity/contracts/ownership/HasNoTokens.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-import './TrueUSD.sol';
+import "zeppelin-solidity/contracts/ownership/HasNoEther.sol";
+import "zeppelin-solidity/contracts/ownership/HasNoTokens.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "./TrueUSD.sol";
 
 // The TimeLockedAdmin contract is intended to be the initial Owner of the TrueUSD
 // contract and TrueUSD's AddressLists. It splits ownership into two accounts: an "admin" account and an
@@ -18,8 +19,10 @@ import './TrueUSD.sol';
 // finalized by the beneficiary (the token recipient or the new owner,
 // respectively). Requests initiated by an admin that has since been deposed
 // cannot be finalized. The admin is also able to update TrueUSD's AddressLists
-// (without a day's delay).
+// (without a day's delay). Anything the admin can do, the owner can also do
+// without a delay.
 contract TimeLockedAdmin is Ownable, HasNoEther, HasNoTokens {
+    using SafeMath for uint256;
 
     // 24 hours, assuming a 15 second blocktime.
     // As long as this isn't too far off from reality it doesn't really matter.
@@ -45,9 +48,15 @@ contract TimeLockedAdmin is Ownable, HasNoEther, HasNoTokens {
         uint deferBlock;
     }
 
-    struct ChangeInsuranceFeeOperation {
-        uint80 newNumerator;
-        uint80 newDenominator;
+    struct ChangeInsuranceFeesOperation {
+        uint80 _transferFeeNumerator;
+        uint80 _transferFeeDenominator;
+        uint80 _mintFeeNumerator;
+        uint80 _mintFeeDenominator;
+        uint256 _mintFeeFlat;
+        uint80 _burnFeeNumerator;
+        uint80 _burnFeeDenominator;
+        uint256 _burnFeeFlat;
         address admin;
         uint deferBlock;
     }
@@ -66,12 +75,20 @@ contract TimeLockedAdmin is Ownable, HasNoEther, HasNoTokens {
     MintOperation[] public mintOperations;
     TransferOwnershipOperation public transferOwnershipOperation;
     ChangeBurnBoundsOperation public changeBurnBoundsOperation;
-    ChangeInsuranceFeeOperation public changeInsuranceFeeOperation;
+    ChangeInsuranceFeesOperation public changeInsuranceFeesOperation;
     ChangeInsurerOperation public changeInsurerOperation;
 
-    modifier onlyAdmin() {
-      require(msg.sender == admin);
-      _;
+    modifier onlyAdminOrOwner() {
+        require(msg.sender == admin || msg.sender == owner);
+        _;
+    }
+
+    function computeDeferBlock() private view returns (uint) {
+        if (msg.sender == owner) {
+            return block.number;
+        } else {
+            return block.number.add(blocksDelay);
+        }
     }
 
     // starts with no admin
@@ -82,48 +99,81 @@ contract TimeLockedAdmin is Ownable, HasNoEther, HasNoTokens {
         blackList = AddressList(_blackList);
     }
 
-    event MintOperationEvent(MintOperation op, uint opIndex, address indexed _to);
-    event TransferOwnershipOperationEvent(TransferOwnershipOperation op);
-    event ChangeBurnBoundsOperationEvent(ChangeBurnBoundsOperation op);
-    event ChangeInsuranceFeeOperationEvent(ChangeInsuranceFeeOperation op);
-    event ChangeInsurerOperationEvent(ChangeInsurerOperation op);
+    event MintOperationEvent(address indexed _to, uint256 amount, uint deferBlock, uint opIndex);
+    event TransferOwnershipOperationEvent(address newOwner, uint deferBlock);
+    event ChangeBurnBoundsOperationEvent(uint newMin, uint newMax, uint deferBlock);
+    event ChangeInsuranceFeesOperationEvent(uint80 _transferFeeNumerator,
+                                            uint80 _transferFeeDenominator,
+                                            uint80 _mintFeeNumerator,
+                                            uint80 _mintFeeDenominator,
+                                            uint256 _mintFeeFlat,
+                                            uint80 _burnFeeNumerator,
+                                            uint80 _burnFeeDenominator,
+                                            uint256 _burnFeeFlat,
+                                            uint deferBlock);
+    event ChangeInsurerOperationEvent(address newInsurer, uint deferBlock);
     event AdminshipTransferred(address indexed previousAdmin, address indexed newAdmin);
 
     // admin initiates a request to mint _amount TrueUSD for account _to
-    function requestMint(address _to, uint256 _amount) public onlyAdmin {
-        MintOperation memory op = MintOperation(_to, _amount, admin, block.number + blocksDelay);
-        MintOperationEvent(op, mintOperations.length, _to);
+    function requestMint(address _to, uint256 _amount) public onlyAdminOrOwner {
+        uint deferBlock = computeDeferBlock();
+        MintOperation memory op = MintOperation(_to, _amount, admin, deferBlock);
+        MintOperationEvent(_to, _amount, deferBlock, mintOperations.length);
         mintOperations.push(op);
     }
 
     // admin initiates a request to transfer ownership of the TrueUSD contract and all AddressLists to newOwner.
     // Can be used e.g. to upgrade this TimeLockedAdmin contract.
-    function requestTransferOwnership(address newOwner) public onlyAdmin {
-        TransferOwnershipOperation memory op = TransferOwnershipOperation(newOwner, admin, block.number + blocksDelay);
-        TransferOwnershipOperationEvent(op);
-        transferOwnershipOperation = op;
+    function requestTransferOwnership(address newOwner) public onlyAdminOrOwner {
+        uint deferBlock = computeDeferBlock();
+        transferOwnershipOperation = TransferOwnershipOperation(newOwner, admin, deferBlock);
+        TransferOwnershipOperationEvent(newOwner, deferBlock);
     }
 
     // admin initiates a request that the minimum and maximum amounts that any TrueUSD user can
     // burn become newMin and newMax
-    function requestChangeBurnBounds(uint newMin, uint newMax) public onlyAdmin {
-        ChangeBurnBoundsOperation memory op = ChangeBurnBoundsOperation(newMin, newMax, admin, block.number + blocksDelay);
-        ChangeBurnBoundsOperationEvent(op);
-        changeBurnBoundsOperation = op;
+    function requestChangeBurnBounds(uint newMin, uint newMax) public onlyAdminOrOwner {
+        uint deferBlock = computeDeferBlock();
+        changeBurnBoundsOperation = ChangeBurnBoundsOperation(newMin, newMax, admin, deferBlock);
+        ChangeBurnBoundsOperationEvent(newMin, newMax, deferBlock);
     }
 
     // admin initiates a request that the insurance fee be changed
-    function requestChangeInsuranceFee(uint80 newNumerator, uint80 newDenominator) public onlyAdmin {
-        ChangeInsuranceFeeOperation memory op = ChangeInsuranceFeeOperation(newNumerator, newDenominator, admin, block.number + blocksDelay);
-        ChangeInsuranceFeeOperationEvent(op);
-        changeInsuranceFeeOperation = op;
+    function requestChangeInsuranceFees(uint80 _transferFeeNumerator,
+                                        uint80 _transferFeeDenominator,
+                                        uint80 _mintFeeNumerator,
+                                        uint80 _mintFeeDenominator,
+                                        uint256 _mintFeeFlat,
+                                        uint80 _burnFeeNumerator,
+                                        uint80 _burnFeeDenominator,
+                                        uint256 _burnFeeFlat) public onlyAdminOrOwner {
+        uint deferBlock = computeDeferBlock();
+        changeInsuranceFeesOperation = ChangeInsuranceFeesOperation(_transferFeeNumerator,
+                                                                    _transferFeeDenominator,
+                                                                    _mintFeeNumerator,
+                                                                    _mintFeeDenominator,
+                                                                    _mintFeeFlat,
+                                                                    _burnFeeNumerator,
+                                                                    _burnFeeDenominator,
+                                                                    _burnFeeFlat,
+                                                                    admin,
+                                                                    deferBlock);
+        ChangeInsuranceFeesOperationEvent(_transferFeeNumerator,
+                                          _transferFeeDenominator,
+                                          _mintFeeNumerator,
+                                          _mintFeeDenominator,
+                                          _mintFeeFlat,
+                                          _burnFeeNumerator,
+                                          _burnFeeDenominator,
+                                          _burnFeeFlat,
+                                          deferBlock);
     }
 
     // admin initiates a request that the recipient of the insurance fee be changed to newInsurer
-    function requestChangeInsurer(address newInsurer) public onlyAdmin {
-        ChangeInsurerOperation memory op = ChangeInsurerOperation(newInsurer, admin, block.number + blocksDelay);
-        ChangeInsurerOperationEvent(op);
-        changeInsurerOperation = op;
+    function requestChangeInsurer(address newInsurer) public onlyAdminOrOwner {
+        uint deferBlock = computeDeferBlock();
+        changeInsurerOperation = ChangeInsurerOperation(newInsurer, admin, deferBlock);
+        ChangeInsurerOperationEvent(newInsurer, deferBlock);
     }
 
     // after a day, beneficiary of a mint request finalizes it by providing the
@@ -153,7 +203,7 @@ contract TimeLockedAdmin is Ownable, HasNoEther, HasNoTokens {
     }
 
     // after a day, admin finalizes the burn bounds change
-    function finalizeChangeBurnBounds() public onlyAdmin {
+    function finalizeChangeBurnBounds() public onlyAdminOrOwner {
         require(changeBurnBoundsOperation.admin == admin);
         require(changeBurnBoundsOperation.deferBlock <= block.number);
         uint newMin = changeBurnBoundsOperation.newMin;
@@ -163,17 +213,30 @@ contract TimeLockedAdmin is Ownable, HasNoEther, HasNoTokens {
     }
 
     // after a day, admin finalizes the insurance fee change
-    function finalizeChangeInsuranceFee() public onlyAdmin {
-        require(changeInsuranceFeeOperation.admin == admin);
-        require(changeInsuranceFeeOperation.deferBlock <= block.number);
-        uint80 newNumerator = changeInsuranceFeeOperation.newNumerator;
-        uint80 newDenominator = changeInsuranceFeeOperation.newDenominator;
-        delete changeInsuranceFeeOperation;
-        child.changeInsuranceFee(newNumerator, newDenominator);
+    function finalizeChangeInsuranceFees() public onlyAdminOrOwner {
+        require(changeInsuranceFeesOperation.admin == admin);
+        require(changeInsuranceFeesOperation.deferBlock <= block.number);
+        uint80 _transferFeeNumerator = changeInsuranceFeesOperation._transferFeeNumerator;
+        uint80 _transferFeeDenominator = changeInsuranceFeesOperation._transferFeeDenominator;
+        uint80 _mintFeeNumerator = changeInsuranceFeesOperation._mintFeeNumerator;
+        uint80 _mintFeeDenominator = changeInsuranceFeesOperation._mintFeeDenominator;
+        uint256 _mintFeeFlat = changeInsuranceFeesOperation._mintFeeFlat;
+        uint80 _burnFeeNumerator = changeInsuranceFeesOperation._burnFeeNumerator;
+        uint80 _burnFeeDenominator = changeInsuranceFeesOperation._burnFeeDenominator;
+        uint256 _burnFeeFlat = changeInsuranceFeesOperation._burnFeeFlat;
+        delete changeInsuranceFeesOperation;
+        child.changeInsuranceFees(_transferFeeNumerator,
+                                  _transferFeeDenominator,
+                                  _mintFeeNumerator,
+                                  _mintFeeDenominator,
+                                  _mintFeeFlat,
+                                  _burnFeeNumerator,
+                                  _burnFeeDenominator,
+                                  _burnFeeFlat);
     }
 
     // after a day, admin finalizes the insurance fees recipient change
-    function finalizeChangeInsurer() public onlyAdmin {
+    function finalizeChangeInsurer() public onlyAdminOrOwner {
         require(changeInsurerOperation.admin == admin);
         require(changeInsurerOperation.deferBlock <= block.number);
         address newInsurer = changeInsurerOperation.newInsurer;
@@ -188,7 +251,7 @@ contract TimeLockedAdmin is Ownable, HasNoEther, HasNoTokens {
     }
 
     // admin (immediately) updates a whitelist/blacklist
-    function updateList(address list, address entry, bool flag) public onlyAdmin {
+    function updateList(address list, address entry, bool flag) public onlyAdminOrOwner {
         AddressList(list).changeList(entry, flag);
     }
 }
--- contracts/TrueUSD.sol
+++ contracts/TrueUSD.sol
@@ -13,14 +13,20 @@ contract TrueUSD is StandardToken, BurnableToken, NoOwner {
     AddressList public canReceiveMintWhitelist;
     AddressList public canBurnWhiteList;
     AddressList public blackList;
-    uint public burnMin = 10000 * 10**uint256(decimals);
-    uint public burnMax = 20000000 * 10**uint256(decimals);
+    uint256 public burnMin = 10000 * 10**uint256(decimals);
+    uint256 public burnMax = 20000000 * 10**uint256(decimals);
 
-    uint80 public insuranceFeeNumerator;
-    uint80 public insuranceFeeDenominator;
+    uint80 public transferFeeNumerator = 7;
+    uint80 public transferFeeDenominator = 10000;
+    uint80 public mintFeeNumerator = 0;
+    uint80 public mintFeeDenominator = 10000;
+    uint256 public mintFeeFlat = 0;
+    uint80 public burnFeeNumerator = 0;
+    uint80 public burnFeeDenominator = 10000;
+    uint256 public burnFeeFlat = 0;
     address public insurer;
 
-    event ChangeBurnBoundsEvent(uint newMin, uint newMax);
+    event ChangeBurnBoundsEvent(uint256 newMin, uint256 newMax);
     event Mint(address indexed to, uint256 amount);
 
     function TrueUSD(address _canMintWhiteList, address _canBurnWhiteList, address _blackList) public {
@@ -28,8 +34,6 @@ contract TrueUSD is StandardToken, BurnableToken, NoOwner {
         canReceiveMintWhitelist = AddressList(_canMintWhiteList);
         canBurnWhiteList = AddressList(_canBurnWhiteList);
         blackList = AddressList(_blackList);
-        insuranceFeeNumerator = 7;
-        insuranceFeeDenominator = 10000;
         insurer = msg.sender;
     }
 
@@ -39,6 +43,8 @@ contract TrueUSD is StandardToken, BurnableToken, NoOwner {
         require(canBurnWhiteList.onList(msg.sender));
         require(_value >= burnMin);
         require(_value <= burnMax);
+        uint256 fee = payInsuranceFee(msg.sender, _value, burnFeeNumerator, burnFeeDenominator, burnFeeFlat);
+        _value = _value.sub(fee);
         super.burn(_value);
     }
 
@@ -50,6 +56,7 @@ contract TrueUSD is StandardToken, BurnableToken, NoOwner {
         balances[_to] = balances[_to].add(_amount);
         Mint(_to, _amount);
         Transfer(address(0), _to, _amount);
+        payInsuranceFee(_to, _amount, mintFeeNumerator, mintFeeDenominator, mintFeeFlat);
     }
 
     //Change the minimum and maximum amount that can be burned at once. Burning
@@ -69,7 +76,7 @@ contract TrueUSD is StandardToken, BurnableToken, NoOwner {
         require(!blackList.onList(msg.sender));
         require(!blackList.onList(to));
         bool result = super.transfer(to, value);
-        payInsuranceFee(to, value);
+        payInsuranceFee(to, value, transferFeeNumerator, transferFeeDenominator, 0);
         return result;
     }
 
@@ -77,13 +84,15 @@ contract TrueUSD is StandardToken, BurnableToken, NoOwner {
         require(!blackList.onList(from));
         require(!blackList.onList(to));
         bool result = super.transferFrom(from, to, value);
-        payInsuranceFee(to, value);
+        payInsuranceFee(to, value, transferFeeNumerator, transferFeeDenominator, 0);
         return result;
     }
 
-    function payInsuranceFee(address payer, uint256 value) private {
-        uint256 insuranceFee = value.mul(insuranceFeeNumerator).div(insuranceFeeDenominator);
-        transferFromWithoutAllowance(payer, insurer, insuranceFee);
+    function payInsuranceFee(address payer, uint256 value, uint80 numerator, uint80 denominator, uint256 flatRate) private returns (uint256 insuranceFee) {
+        insuranceFee = value.mul(numerator).div(denominator).add(flatRate);
+        if (insuranceFee > 0) {
+            transferFromWithoutAllowance(payer, insurer, insuranceFee);
+        }
     }
 
     // based on 'transfer' in https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/token/ERC20/BasicToken.sol
@@ -95,12 +104,29 @@ contract TrueUSD is StandardToken, BurnableToken, NoOwner {
         Transfer(from, _to, _value);
     }
 
-    function changeInsuranceFee(uint80 newNumerator, uint80 newDenominator) public onlyOwner {
-        insuranceFeeNumerator = newNumerator;
-        insuranceFeeDenominator = newDenominator;
+    function changeInsuranceFees(uint80 _transferFeeNumerator,
+                                 uint80 _transferFeeDenominator,
+                                 uint80 _mintFeeNumerator,
+                                 uint80 _mintFeeDenominator,
+                                 uint256 _mintFeeFlat,
+                                 uint80 _burnFeeNumerator,
+                                 uint80 _burnFeeDenominator,
+                                 uint256 _burnFeeFlat) public onlyOwner {
+        require(_transferFeeDenominator != 0);
+        require(_mintFeeDenominator != 0);
+        require(_burnFeeDenominator != 0);
+        transferFeeNumerator = _transferFeeNumerator;
+        transferFeeDenominator = _transferFeeDenominator;
+        mintFeeNumerator = _mintFeeNumerator;
+        mintFeeDenominator = _mintFeeDenominator;
+        mintFeeFlat = _mintFeeFlat;
+        burnFeeNumerator = _burnFeeNumerator;
+        burnFeeDenominator = _burnFeeDenominator;
+        burnFeeFlat = _burnFeeFlat;
     }
 
     function changeInsurer(address newInsurer) public onlyOwner {
+        require(newInsurer != address(0));
         insurer = newInsurer;
     }
 }
