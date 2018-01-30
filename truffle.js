468a81d45ed03283c15cb7607a91873910f7c21c
Benjamin Cosman <bcosman@caltech.edu>
2018-01-30 12:15:31 -0800

    update gas parameters

--- truffle.js
+++ truffle.js
@@ -5,26 +5,26 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      gas: 4600000,
+      gas: 5500000,
       network_id: "*" // Match any network id
     },
     ropsten: {
       provider: new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/"),
       network_id: "3",
-      gas: 4500000,
+      gas: 5500000,
       gasPrice: 22000000000 // Specified in Wei
     },
     rinkeby: {
       provider: new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/"),
       network_id: "4",
-      gas: 4500000,
+      gas: 5500000,
       gasPrice: 22000000000 // Specified in Wei
     },
     production: {
       provider: new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/dYWKKqsJkbv9cZlQFEpI "),
       network_id: "1",
-      gas: 4500000,
-      gasPrice: 20000000000
+      gas: 5500000,
+      gasPrice: 8000000000
     },
 
   }
