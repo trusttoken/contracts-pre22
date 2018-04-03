94ccfe742580429c94107dd1020f0600016819f1
Benjamin Cosman <bcosman@caltech.edu>
2018-04-03 14:00:31 -0700

    AddressList now never contains 0x0

--- contracts/AddressList.sol
+++ contracts/AddressList.sol
@@ -6,14 +6,12 @@ contract AddressList is Claimable {
     string public name;
     mapping (address => bool) public onList;
 
-    function AddressList(string _name, bool nullValue) public {
+    function AddressList(string _name) public {
         name = _name;
-        onList[0x0] = nullValue;
     }
     event ChangeWhiteList(address indexed to, bool onList);
 
-    // Set whether _to is on the list or not. Whether 0x0 is on the list
-    // or not cannot be set here - it is set once and for all by the constructor.
+    // Set whether _to is on the list or not. 0x0 is never on the list.
     function changeList(address _to, bool _onList) onlyOwner public {
         require(_to != 0x0);
         if (onList[_to] != _onList) {
