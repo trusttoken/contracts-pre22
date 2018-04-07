5ba4e4f37f5cc690c2e677aa0308170a9f845196
Benjamin Cosman <bcosman@caltech.edu>
2018-04-06 23:16:09 -0700

    test

--- test/GatedToken.js
+++ test/GatedToken.js
@@ -32,25 +32,61 @@ contract('GatedToken', function ([_, owner, oneHundred, anotherAccount]) {
 
 function gatedTokenTests([_, owner, oneHundred, anotherAccount]) {
     describe('--GatedToken Tests--', function () {
-        describe('when user is on mint whitelist', function () {
-            beforeEach(async function () {
-                await this.mintWhiteList.changeList(anotherAccount, true, { from: owner })
+        describe('minting', function () {
+            describe('when user is on mint whitelist', function () {
+                beforeEach(async function () {
+                    await this.mintWhiteList.changeList(anotherAccount, true, { from: owner })
+                })
+
+                mintableTokenTests([_, owner, oneHundred, anotherAccount])
             })
 
-            mintableTokenTests([_, owner, oneHundred, anotherAccount])
+            it('rejects mint when user is not on mint whitelist', async function () {
+                await assertRevert(this.token.mint(anotherAccount, 100, { from: owner }))
+            })
         })
 
-        describe('when user is on burn whitelist', function () {
-            beforeEach(async function () {
-                await this.burnWhiteList.changeList(oneHundred, true, { from: owner })
+        describe('burning', function () {
+            describe('when user is on burn whitelist', function () {
+                beforeEach(async function () {
+                    await this.burnWhiteList.changeList(oneHundred, true, { from: owner })
+                })
+
+                burnableTokenTests([_, owner, oneHundred, anotherAccount])
+
+                //TODO do we actually want this feature? removing from whitelist can already accomplish this
+                // it('rejects burn when user is on blacklist', async function () {
+                //     await this.blackList.changeList(oneHundred, true, { from: owner })
+                //     await assertRevert(this.token.burn(20, { from: oneHundred }))
+                // })
             })
 
-            burnableTokenTests([_, owner, oneHundred, anotherAccount])
+            it('rejects burn when user is not on burn whitelist', async function () {
+                await this.burnWhiteList.changeList(oneHundred, true, { from: owner }) //TODO remove and assert revert
+                await this.token.burn(20, { from: oneHundred })
+            })
         })
 
-        describe('when user is not on blacklist', function () {
-            basicTokenTests([_, owner, oneHundred, anotherAccount])
-            standardTokenTests([_, owner, oneHundred, anotherAccount])
+        describe('transferring', function () {
+            describe('when user is not on blacklist', function () {
+                basicTokenTests([_, owner, oneHundred, anotherAccount])
+                standardTokenTests([_, owner, oneHundred, anotherAccount])
+            })
+
+            describe('when user is on blacklist', function () {
+                beforeEach(async function () {
+                    await this.blackList.changeList(oneHundred, true, { from: owner })
+                })
+
+                it('rejects transfer', async function () {
+                    await assertRevert(this.token.transfer(anotherAccount, 100, { from: oneHundred }))
+                })
+
+                it('rejects transferFrom', async function () {
+                    await this.token.approve(anotherAccount, 100, { from: oneHundred })
+                    await assertRevert(this.token.transferFrom(oneHundred, anotherAccount, 100, { from: anotherAccount }))
+                })
+            })
         })
     })
 
