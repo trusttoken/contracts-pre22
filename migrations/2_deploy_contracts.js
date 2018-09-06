16b8a831300edfb57232208288e6fe0b97ec94df
Terry Li <terryli0095@gmail.com>
2018-09-06 02:45:56 -0700

    update deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,69 +1,84 @@
-// var NamableAddressList = artifacts.require("NamableAddressList");
-// var BalanceSheet = artifacts.require("BalanceSheet");
-// var AllowanceSheet = artifacts.require("AllowanceSheet");
-// var TrueUSD = artifacts.require("TrueUSD");
-// var TimeLockedController = artifacts.require("TimeLockedController");
-// var AddressValidation = artifacts.require("AddressValidation");
-// var Web3 = require('web3');
+// const BalanceSheet = artifacts.require("BalanceSheet");
+// const AllowanceSheet = artifacts.require("AllowanceSheet");
+// const TrueUSD = artifacts.require("TrueUSD");
+// const TimeLockedController = artifacts.require("TimeLockedController");
+// const DateTimeMock = artifacts.require("DateTimeMock");
+// const Registry = artifacts.require("Registry");
+// const FastPause = artifacts.require("FastPause")
+// const pauseKey = "0x75f87fbbf0d57c24d76046ec023d611db8652034"
+// const mintKey = "0x75f87fbbf0d57c24d76046ec023d611db8652034"
+// const approver1 = "0x75f87fbbf0d57c24d76046ec023d611db8652034"
+// const approver2 = "0x88990c90d5e6345adfe45d21e7547d405727d2aa"
+// const approver3 = "0x5ae70e90105e3a7ec9bfda781ec4f80b00ead2a2"
+// const user = "0x5ae70e90105e3a7ec9bfda781ec4f80b00ead2a2"
 
-module.exports = async function(deployer) {
-  // await deployer;
+module.exports = async (deployer, network, accounts)=> {
+  // let balanceSheet, allowanceSheet,trueUSD,timeLockedController,dateTime, registry, fastPause;
 
-  // // const addressValidation = AddressValidation.at("0x42Af5f733e8ecE22063FD516a5f1b246D6923eC0")
-  // // const mintWhiteList = NamableAddressList.at("0xf2ec422d6eeb805eff207b1e358947cbd73b129d")
-  // // const canBurnWhiteList = NamableAddressList.at("0x844De59c9A8D428283923fb752002fafe2aa694a")
-  // // const blackList = NamableAddressList.at("0xf5de41317a8fde99108e2fa2d26822bebad1427e")
-  // // const noFeesList = NamableAddressList.at("0x9f59b4f7d3bd00caa85e61c57761768291155084")
-  // // const balances = BalanceSheet.at("0x6dea55ba04a37fddd05e1fd979c30aa0e634e837")
-  // // const allowances = AllowanceSheet.at("0x811c5f8dfbdd70c245e66e4cd181040b2630424a")
-  // // const trueUSD = TrueUSD.at("0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E")
-  // // const timeLockedController = TimeLockedController.at("0x9978d2d229a69b3aef93420d132ab22b44e3578f")
+  // deployer.then(async ()=>{
+  // console.log("deploying BalanceSheet")
+  // balanceSheet = BalanceSheet.at("0xd343e626d36e6e653b17abc9c76421d1180f44ed")//await BalanceSheet.new();
+  // console.log("deploying AllowanceSheet")
+  // allowanceSheet = AllowanceSheet.at("0xd2e62312ef99dfed58e0e7353063895add27e728")//await AllowanceSheet.new();
+  // console.log("deploying TrueUSD")
+  // trueUSD = TrueUSD.at("0xaeb5ddbeb7f855aa44a40b0d17c742031642414b")//await TrueUSD.new();
+  // console.log("deploying TimeLockedController")
+  // timeLockedController = TimeLockedController.at("0xf2b3bf1917d2a7890820a70edac8fb85a8211b59")//await TimeLockedController.new();
+  // console.log("deploying DateTime")
+  // dateTime = DateTimeMock.at("0x45f51f427d0f5ef9044e054081320e21d40b3516")//await DateTimeMock.new();
+  // console.log("deploying registry")
+  // registry = Registry.at("0xb8ba104c9479faa475d00bfe789693c0a25f6ca8")//await Registry.new();
+  // console.log("deploying FastPause")
+  // fastPause = FastPause.at("0x99dd152342291cd31ddccbf635f94c0cf217d4d2")//await FastPause.new(); 
+  // console.log("######################")
+  // console.log("tusd Address", trueUSD.address)
+  // console.log("controller address", timeLockedController.address)
+  // console.log("registry Address", registry.address)
+  // console.log("fastPause Address", fastPause.address)
+  // console.log("dateTime Address", dateTime.address)
+  // console.log("balanceSheet Address", balanceSheet.address)
+  // console.log("allowanceSheet Address", allowanceSheet.address)
 
-  // console.log("Create the small contracts that TrueUSD depends on...")
-  // const addressValidation = await AddressValidation.new()
-  // console.log("addressValidation Address: ", addressValidation.address)
-  // const mintWhiteList = await NamableAddressList.new("mintWhiteList")
-  // console.log("mintWhiteList Address: ", mintWhiteList.address)
-  // const canBurnWhiteList = await NamableAddressList.new("canBurnWhiteList")
-  // console.log("canBurnWhiteList Address: ", canBurnWhiteList.address)
-  // const blackList = await NamableAddressList.new("blackList")
-  // console.log("blackList Address: ", blackList.address)
-  // const noFeesList = await NamableAddressList.new("noFeesList")
-  // console.log("noFeesList Address: ", noFeesList.address)
-  // const balances = await BalanceSheet.new()
-  // console.log("balanceSheet Address: ", balances.address)
-  // const allowances = await AllowanceSheet.new()
-  // console.log("allowanceSheet Address: ", allowances.address)
-
-  // console.log("Create and configure TrueUSD...")
-  // const trueUSD = await TrueUSD.new()
-  // console.log("trueUSD Address: ", trueUSD.address)
-  // await balances.transferOwnership(trueUSD.address)
-  // await allowances.transferOwnership(trueUSD.address)
-  // await trueUSD.setBalanceSheet(balances.address)
-  // await trueUSD.setAllowanceSheet(allowances.address)
-  // await trueUSD.setLists(mintWhiteList.address, canBurnWhiteList.address, blackList.address)
-  // await trueUSD.setNoFeesList(noFeesList.address)
-  // await trueUSD.changeStaker("0x960Ab0dea96ab2dB293F162e6047306154588E8B")
-
-  // console.log("Create TimeLockedController and transfer ownership of other contracts to it...")
-  // const timeLockedController = await TimeLockedController.new()
-  // console.log("timeLockedController Address: ", timeLockedController.address)
-  // await mintWhiteList.transferOwnership(timeLockedController.address)
-  // await timeLockedController.issueClaimOwnership(mintWhiteList.address)
-  // await canBurnWhiteList.transferOwnership(timeLockedController.address)
-  // await timeLockedController.issueClaimOwnership(canBurnWhiteList.address)
-  // await blackList.transferOwnership(timeLockedController.address)
-  // await timeLockedController.issueClaimOwnership(blackList.address)
-  // await noFeesList.transferOwnership(timeLockedController.address)
-  // await timeLockedController.issueClaimOwnership(noFeesList.address)
+  // console.log("transfer balanceSheet and allowanceSheet to TrueUSD")
+  // await balanceSheet.transferOwnership(trueUSD.address)
+  // await allowanceSheet.transferOwnership(trueUSD.address)
+  // await trueUSD.setBalanceSheet(balanceSheet.address)
+  // await trueUSD.setAllowanceSheet(allowanceSheet.address)
+  // console.log("transfer trueUSD to timeLockedController")
   // await trueUSD.transferOwnership(timeLockedController.address)
   // await timeLockedController.issueClaimOwnership(trueUSD.address)
   // await timeLockedController.setTrueUSD(trueUSD.address)
+  // console.log("configure controller");
+  // await timeLockedController.setDateTime(dateTime.address);
+  // await timeLockedController.setTusdRegistry(registry.address);
+  // await timeLockedController.setRegistry(registry.address)
+  // await timeLockedController.addMintCheckTime(10,0)
+  // await timeLockedController.addMintCheckTime(17,0)
+  // await timeLockedController.setMintLimit(100000)
+  // await timeLockedController.setSmallMintThreshold(10000)
+  // await timeLockedController.setMinimalApprovals(1,2)
+  // await timeLockedController.addHoliday(2018,9,4)
+  // await timeLockedController.setBurnBounds(100,300)
+  // console.log("set permissions")
+  // await timeLockedController.transferMintKey(mintKey)
+  // await registry.setAttribute(approver1, "isTUSDMintApprover", 1, "notes")
+  // await registry.setAttribute(approver2, "isTUSDMintApprover", 1, "notes")
+  // await registry.setAttribute(approver3, "isTUSDMintApprover", 1, "notes")
+  // await registry.setAttribute(pauseKey, "isTUSDMintChecker", 1, "notes")
+  // await registry.setAttribute(user, "hasPassedKYC/AML", 1, "notes",)
+  // await registry.setAttribute(user, "noFees", 1, "notes")
+  // console.log("configure fastPause");
+  // await fastPause.modifyPauseKey(pauseKey, true)
+  // await fastPause.setController(timeLockedController.address)
+  // await registry.setAttribute(fastPause.address, "isTUSDMintChecker", 1, "notes")
+  // console.log("######################")
+  // console.log("tusd Address", trueUSD.address)
+  // console.log("controller address", timeLockedController.address)
+  // console.log("registry Address", registry.address)
+  // console.log("fastPause Address", fastPause.address)
+  // })
 
-  // console.log("Transfer admin/ownership of TimeLockedController...")
-  // await timeLockedController.transferAdminship("0xFdBCF49d3C47E20545E14046C4ECe9c02457646f")
-  // await timeLockedController.transferOwnership("0x8Dc4e7E8dD13FB489070d432Dfa89a0b93315d8B")
 
-  // console.log("Deployment successful")
+
+
 };
