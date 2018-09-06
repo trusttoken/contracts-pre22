2611ab1e8ddf0564659f84abedd6363946e83a6d
Terry Li <terryli0095@gmail.com>
2018-09-06 02:30:21 -0700

    capitalize constants

--- contracts/TrueUSD.sol
+++ contracts/TrueUSD.sol
@@ -12,23 +12,31 @@ import "./WithdrawalToken.sol";
 
 // This is the top-level ERC20 contract, but most of the interesting functionality is
 // inherited - see the documentation on the corresponding contracts.
-contract TrueUSD is ModularPausableToken, NoOwner, BurnableTokenWithBounds, CompliantToken, TokenWithFees, WithdrawalToken, StandardDelegate, CanDelegate {
+contract TrueUSD is 
+ModularPausableToken, 
+NoOwner, 
+BurnableTokenWithBounds, 
+CompliantToken, 
+TokenWithFees, 
+WithdrawalToken, 
+StandardDelegate, 
+CanDelegate {
     using SafeMath for *;
 
     string public name = "TrueUSD";
     string public symbol = "TUSD";
-    uint8 public constant decimals = 18;
-    uint8 public constant rounding = 2;
+    uint8 public constant DECIMALS = 18;
+    uint8 public constant ROUNDING = 2;
 
     event ChangeTokenName(string newName, string newSymbol);
 
     constructor() public {
         totalSupply_ = 0;
-        burnMin = 10000 * 10**uint256(decimals);
-        burnMax = 20000000 * 10**uint256(decimals);
+        burnMin = 10000 * 10**uint256(DECIMALS);
+        burnMax = 20000000 * 10**uint256(DECIMALS);
     }
 
-    function changeTokenName(string _name, string _symbol) onlyOwner public {
+    function changeTokenName(string _name, string _symbol) public onlyOwner {
         name = _name;
         symbol = _symbol;
         emit ChangeTokenName(_name, _symbol);
@@ -37,31 +45,38 @@ contract TrueUSD is ModularPausableToken, NoOwner, BurnableTokenWithBounds, Comp
     // disable most onlyOwner functions upon delegation, since the owner should
     // use the new version of the contract
     modifier onlyWhenNoDelegate() {
-        require(address(delegate) == address(0),"a delegate contract exist");
+        require(address(delegate) == address(0), "a delegate contract exist");
         _;
     }
 
-    function mint(address _to, uint256 _value) onlyWhenNoDelegate public returns (bool) {
+    function mint(address _to, uint256 _value) public onlyWhenNoDelegate returns (bool) {
         super.mint(_to, _value);
     }
-    function setBalanceSheet(address _sheet) onlyWhenNoDelegate public returns (bool) {
+
+    function setBalanceSheet(address _sheet) public onlyWhenNoDelegate returns (bool) {
         return super.setBalanceSheet(_sheet);
     }
-    function setAllowanceSheet(address _sheet) onlyWhenNoDelegate public returns (bool) {
+
+    function setAllowanceSheet(address _sheet) public onlyWhenNoDelegate returns (bool) {
         return super.setAllowanceSheet(_sheet);
     }
-    function setBurnBounds(uint256 _min, uint256 _max) onlyWhenNoDelegate public {
+
+    function setBurnBounds(uint256 _min, uint256 _max) public onlyWhenNoDelegate {
         super.setBurnBounds(_min, _max);
     }
-    function setRegistry(Registry _registry) onlyWhenNoDelegate public {
+
+    function setRegistry(Registry _registry) public onlyWhenNoDelegate {
         super.setRegistry(_registry);
     }
-    function changeStaker(address _newStaker) onlyWhenNoDelegate public {
+
+    function changeStaker(address _newStaker) public onlyWhenNoDelegate {
         super.changeStaker(_newStaker);
     }
-    function wipeBlacklistedAccount(address _account) onlyWhenNoDelegate public {
+
+    function wipeBlacklistedAccount(address _account) public onlyWhenNoDelegate {
         super.wipeBlacklistedAccount(_account);
     }
+
     function changeStakingFees(
         uint256 _transferFeeNumerator,
         uint256 _transferFeeDenominator,
@@ -71,7 +86,7 @@ contract TrueUSD is ModularPausableToken, NoOwner, BurnableTokenWithBounds, Comp
         uint256 _burnFeeNumerator,
         uint256 _burnFeeDenominator,
         uint256 _burnFeeFlat
-    ) onlyWhenNoDelegate public {
+    ) public onlyWhenNoDelegate {
         super.changeStakingFees(
             _transferFeeNumerator,
             _transferFeeDenominator,
@@ -83,12 +98,12 @@ contract TrueUSD is ModularPausableToken, NoOwner, BurnableTokenWithBounds, Comp
             _burnFeeFlat
         );
     }
-    function burnAllArgs(address _burner, uint256 _value ,string _note) internal {
-      //round down burn amount to cent
-      uint burnAmount = _value / (10 **uint256(decimals-rounding)) * (10 **uint256(decimals-rounding));
-      super.burnAllArgs(_burner, burnAmount, _note);
-    }
 
+    function burnAllArgs(address _burner, uint256 _value, string _note) internal {
+        //round down burn amount to cent
+        uint burnAmount = _value / (10 ** uint256(DECIMALS - ROUNDING)) * (10 ** uint256(DECIMALS - ROUNDING));
+        super.burnAllArgs(_burner, burnAmount, _note);
+    }
 
     // Alternatives to the normal NoOwner functions in case this contract's owner
     // can't own ether or tokens.
@@ -98,9 +113,9 @@ contract TrueUSD is ModularPausableToken, NoOwner, BurnableTokenWithBounds, Comp
         _to.transfer(address(this).balance);
     }
 
-    function reclaimToken(ERC20Basic token, address _to) external onlyOwner {
+    function reclaimToken(ERC20 token, address _to) external onlyOwner {
         uint256 balance = token.balanceOf(this);
-        token.safeTransfer(_to, balance);
+        assert(token.transfer(_to, balance));
     }
 
 }
