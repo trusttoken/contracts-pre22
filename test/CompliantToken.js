883ca82c286c636ea78a0a2acc246abc7e44070b
William Morriss <wjmelements@gmail.com>
2018-11-26 13:37:56 -0800

    fix event arg name

--- test/CompliantToken.js
+++ test/CompliantToken.js
@@ -138,7 +138,7 @@ function compliantTokenTests([owner, oneHundred, anotherAccount], transfersToZer
                 assert.equal(logs[0].args.account, oneHundred)
                 assert.equal(logs[0].args.balance, 100*10**18)
                 assert.equal(logs[1].event, 'Transfer')
-                assert.equal(logs[1].args.amount, oneHundred)
+                assert.equal(logs[1].args.value, oneHundred)
                 assert.equal(logs[1].args.to, 0)
                 assert.equal(logs[1].args.from, owner)
             })
