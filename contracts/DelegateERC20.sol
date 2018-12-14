86be5478be83783113c0139ca36df73096ad687c
terryli0095 <terryli0095@gmail.com>
2018-12-14 01:51:24 -0800

    use modifier instead of require

--- contracts/DelegateERC20.sol
+++ contracts/DelegateERC20.sol
@@ -9,6 +9,11 @@ functions in the old contract still works (except Burn).
 contract DelegateERC20 is ModularStandardToken {
 
     address public constant DELEGATE_FROM = 0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E;
+    
+    modifier onlyDelegateFrom() {
+        require(msg.sender == DELEGATE_FROM);
+        _;
+    }
 
     function delegateTotalSupply() public view returns (uint256) {
         return totalSupply();
@@ -18,8 +23,7 @@ contract DelegateERC20 is ModularStandardToken {
         return balanceOf(who);
     }
 
-    function delegateTransfer(address to, uint256 value, address origSender) public returns (bool) {
-        require(msg.sender == DELEGATE_FROM);
+    function delegateTransfer(address to, uint256 value, address origSender) public onlyDelegateFrom returns (bool) {
         _transferAllArgs(origSender, to, value);
         return true;
     }
@@ -28,26 +32,22 @@ contract DelegateERC20 is ModularStandardToken {
         return allowance(owner, spender);
     }
 
-    function delegateTransferFrom(address from, address to, uint256 value, address origSender) public returns (bool) {
-        require(msg.sender == DELEGATE_FROM);
+    function delegateTransferFrom(address from, address to, uint256 value, address origSender) public onlyDelegateFrom returns (bool) {
         _transferFromAllArgs(from, to, value, origSender);
         return true;
     }
 
-    function delegateApprove(address spender, uint256 value, address origSender) public returns (bool) {
-        require(msg.sender == DELEGATE_FROM);
+    function delegateApprove(address spender, uint256 value, address origSender) public onlyDelegateFrom returns (bool) {
         _approveAllArgs(spender, value, origSender);
         return true;
     }
 
-    function delegateIncreaseApproval(address spender, uint addedValue, address origSender) public returns (bool) {
-        require(msg.sender == DELEGATE_FROM);
+    function delegateIncreaseApproval(address spender, uint addedValue, address origSender) public onlyDelegateFrom returns (bool) {
         _increaseApprovalAllArgs(spender, addedValue, origSender);
         return true;
     }
 
-    function delegateDecreaseApproval(address spender, uint subtractedValue, address origSender) public returns (bool) {
-        require(msg.sender == DELEGATE_FROM);
+    function delegateDecreaseApproval(address spender, uint subtractedValue, address origSender) public onlyDelegateFrom returns (bool) {
         _decreaseApprovalAllArgs(spender, subtractedValue, origSender);
         return true;
     }
