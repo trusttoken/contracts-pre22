6f0d1371ef878f737bb9be683b36f53587dde970
William Morriss <wjmelements@gmail.com>
2019-01-07 15:48:37 -0800

    move initialization to mock

--- contracts/mocks/TrueUSDMock.sol
+++ contracts/mocks/TrueUSDMock.sol
@@ -10,4 +10,19 @@ contract TrueUSDMock is TrueUSD {
         totalSupply_ = initialBalance;
         initialize();
     }
+
+    function initialize() public {
+        require(!initialized, "already initialized");
+        initialized = true;
+        owner = msg.sender;
+        burnMin = 10000 * 10**uint256(DECIMALS);
+        burnMax = 20000000 * 10**uint256(DECIMALS);
+        name = "TrueUSD";
+        symbol = "TUSD";
+    }
+
+    function setTotalSupply(uint _totalSupply) public onlyOwner {
+        require(totalSupply_ == 0);
+        totalSupply_ = _totalSupply;
+    }
 }
