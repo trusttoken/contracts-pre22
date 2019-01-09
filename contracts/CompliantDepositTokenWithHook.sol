ab18d8d1cbfec1e86a287be585f851c612f8b18d
William Morriss <wjmelements@gmail.com>
2019-01-09 14:55:37 -0800

    support mint token with hook

--- contracts/CompliantDepositTokenWithHook.sol
+++ contracts/CompliantDepositTokenWithHook.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.23;
 
 import "./CompliantToken.sol";
 import "./DepositToken.sol";
-import "./TrueCoinReceiver.sol";
+import "./TokenWithHook.sol";
 
-contract CompliantDepositTokenWithHook is CompliantToken, DepositToken {
+contract CompliantDepositTokenWithHook is CompliantToken, DepositToken, TokenWithHook {
 
     function _transferFromAllArgs(address _from, address _to, uint256 _value, address _sender) internal {
         bool hasHook;
@@ -50,19 +50,24 @@ contract CompliantDepositTokenWithHook is CompliantToken, DepositToken {
     function mint(address _to, uint256 _value) public onlyOwner {
         require(_to != address(0), "to address cannot be zero");
         require(registry.hasAttribute1ButNotAttribute2(_to, HAS_PASSED_KYC_AML, IS_BLACKLISTED), "_to cannot mint");
-        address shiftedAddress = address(uint(_to) >> 20);
-        uint depositAddressValue = registry.getAttributeValue(shiftedAddress, IS_DEPOSIT_ADDRESS);
+        address shiftedAddress = address(uint256(_to) >> 20);
+        uint256 depositAddressValue = registry.getAttributeValue(shiftedAddress, IS_DEPOSIT_ADDRESS);
         totalSupply_ = totalSupply_.add(_value);
+        address originalTo = _to;
+        emit Mint(_to, _value);
         if (depositAddressValue != 0) {
-            address originalTo = _to;
-            emit Mint(originalTo, _value);
             _to = address(depositAddressValue);
             emit Transfer(address(0), originalTo, _value);
-            emit Transfer(originalTo, _to, _value);
-        } else {
-            emit Mint(_to, _value);
-            emit Transfer(address(0), _to, _value);
         }
+        emit Transfer(address(0), _to, _value);
         balances.addBalance(_to, _value);
+        uint256 hasHook = registry.getAttributeValue(_to, IS_REGISTERED_CONTRACT);
+        if (hasHook != 0) {
+            if (depositAddressValue != 0) {
+                TrueCoinReceiver(_to).tokenFallback(originalTo, _value);
+            } else {
+                TrueCoinReceiver(_to).tokenFallback(address(0), _value);
+            }
+        }
     }
 }
--- contracts/TokenWithHook.sol
+++ contracts/TokenWithHook.sol
@@ -12,14 +12,4 @@ contract TokenWithHook is ModularPausableToken {
     
     bytes32 public constant IS_REGISTERED_CONTRACT = "isRegisteredContract"; 
 
-    function _transferAllArgs(address _from, address _to, uint256 _value) internal {
-        uint length;
-        assembly { length := extcodesize(_to) }
-        super._transferAllArgs(_from, _to, _value);
-        if (length > 0) {
-            if(registry.hasAttribute(_to, IS_REGISTERED_CONTRACT)) {
-                TrueCoinReceiver(_to).tokenFallback(_from, _value);
-            }
-        }
-    }
 }
