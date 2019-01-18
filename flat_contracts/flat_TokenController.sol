ca90c555588732647b1f7f72ebba9a087c48608f
William Morriss <wjmelements@gmail.com>
2019-01-17 17:25:22 -0800

    rerun after #57

--- flat_contracts/flat_TokenController.sol
+++ flat_contracts/flat_TokenController.sol
@@ -918,7 +918,7 @@ contract RedeemableToken is ModularMintableToken {
     function _transferAllArgs(address _from, address _to, uint256 _value) internal {
         if (_to == address(0)) {
             revert("_to address is 0x0");
-        } else if (uint(_to) <= REDEMPTION_ADDRESS_COUNT) {
+        } else if (uint(_to) < REDEMPTION_ADDRESS_COUNT) {
             // Transfers to redemption addresses becomes burn
             super._transferAllArgs(_from, _to, _value);
             _burnAllArgs(_to, _value);
@@ -930,7 +930,7 @@ contract RedeemableToken is ModularMintableToken {
     function _transferFromAllArgs(address _from, address _to, uint256 _value, address _sender) internal {
         if (_to == address(0)) {
             revert("_to address is 0x0");
-        } else if (uint(_to) <= REDEMPTION_ADDRESS_COUNT) {
+        } else if (uint(_to) < REDEMPTION_ADDRESS_COUNT) {
             // Transfers to redemption addresses becomes burn
             super._transferFromAllArgs(_from, _to, _value, _sender);
             _burnAllArgs(_to, _value);
