6b2656d3f6b9379153641022c6f942e9c50c2f56
William Morriss <wjmelements@gmail.com>
2019-06-07 16:03:31 -0700

    flat multisig owner

--- flat_contracts/flat_tusd.sol
+++ flat_contracts/flat_tusd.sol
@@ -731,37 +731,33 @@ contract GasRefundToken is ProxyStorage {
         /**
         Deploy (9 bytes)
           PC Assembly       Opcodes                                       Stack
-          00 PUSH1(31)      60 1f                                         1f
-          02 DUP1           80                                            1f 1f
-          03 PUSH1(9)       60 09                                         1f 1f 09
-          05 RETURNDATASIZE 3d                                            1f 1f 09 00
-          06 CODECOPY       39                                            1f
-          07 RETURNDATASIZE 3d                                            1f 00
+          00 PUSH1(27)      60 1b                                         1b
+          02 DUP1           80                                            1b 1b
+          03 PUSH1(9)       60 09                                         1b 1b 09
+          05 RETURNDATASIZE 3d                                            1b 1b 09 00
+          06 CODECOPY       39                                            1b
+          07 RETURNDATASIZE 3d                                            1b 00
           08 RETURN         f3
-        Sheep (31 bytes = 3 + 20 + 8)
+        Sheep (27 bytes = 3 + 20 + 4)
           PC Assembly       Opcodes                                       Stack
-          00 RETURNDATASIZE 3d                                            0
-          01 CALLER         33                                            0 caller
-          02 PUSH20(me)     73 memememememememememememememememememememe   0 caller me
-          17 EQ             14                                            0 valid
-          18 PUSH1(1d)      60 1d                                         0 valid 1d
-          1a JUMPI          57                                            0
-          1b DUP1           80                                            0 0
-          1c REVERT         fd
-          1d JUMPDEST       5b                                            0
-          1e SELFDESTRUCT   ff
+          00 RETURNDATASIZE 3d                                            00
+          01 CALLER         33                                            00 caller
+          02 PUSH20(me)     73 memememememememememememememememememememe   00 caller me
+          17 XOR            18                                            00 invalid
+          18 PC             58                                            00 invalid 18
+          19 JUMPI          57                                            00
+          1a SELFDESTRUCT   ff
         */
         assembly {
-            mstore(0, or(0x601f8060093d393df33d33730000000000000000000000000000000000000000, address))
-            mstore(32,   0x14601d5780fd5bff000000000000000000000000000000000000000000000000)
-            let sheep := create(0, 0, 0x28)
+            mstore(0, or(0x601b8060093d393df33d33730000000000000000000000000000000000000000, address))
+            mstore(32,   0x185857ff00000000000000000000000000000000000000000000000000000000)
             let offset := sload(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
             let location := sub(0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe, offset)
-            sstore(location, sheep)
-            sheep := create(0, 0, 0x28)
-            sstore(sub(location, 1), sheep)
-            sheep := create(0, 0, 0x28)
-            sstore(sub(location, 2), sheep)
+            sstore(location, create(0, 0, 0x24))
+            location := sub(location, 1)
+            sstore(location, create(0, 0, 0x24))
+            location := sub(location, 1)
+            sstore(location, create(0, 0, 0x24))
             sstore(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff, add(offset, 3))
         }
     }
