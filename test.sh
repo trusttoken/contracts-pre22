b9ee8b50f6cf6b60b0fab1b92135b10374565875
William Morriss <wjmelements@gmail.com>
2019-12-09 15:50:14 -0800

    shared ganache works

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@
 
 cd $(dirname $0)
 
+ganache-cli -l 0x8954400 --allowUnlimitedContractSize -k istanbul >/dev/null &
+GPID=$!
+sleep 1
 for testfile in tests/*.test.js ; do
     testfile=$(basename $testfile)
-    ganache-cli -l 0x8954400 --allowUnlimitedContractSize -k istanbul >/dev/null &
-    GPID=$!
     echo $testfile
     mv tests/$testfile test
-    sleep 1
     truffle test $@
     mv test/$testfile tests
-    kill -15 $GPID
 done
+kill -15 $GPID
 
 exit
