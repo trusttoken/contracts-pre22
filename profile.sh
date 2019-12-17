07346c1e9c63cf5768edd16deaf29f56a77a53d7
William Morriss <wjmelements@gmail.com>
2019-12-17 15:10:22 -0800

    fix profile.sh

--- profile.sh
+++ profile.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 ganache-cli -l 0x8954400 --allowUnlimitedContractSize -k istanbul >/dev/null &
 GPID=$!
-sleep 1
+sleep 0.1
 testfile=GasProfile.test.js
-mv tests/$testfile test
 truffle test test/$testfile
-mv test/$testfile tests
 kill -15 $GPID
 exit
