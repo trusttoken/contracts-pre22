b702e6b8ace99df33da0030896232a7ec2c099fe
Harold Hyatt <haroldhyatt@Hals-MBP.hsd1.ca.comcast.net>
2020-04-06 18:17:17 -0700

    documentation for aave opportunity

--- contracts/TrueReward/AaveFinancialOpportunity.sol
+++ contracts/TrueReward/AaveFinancialOpportunity.sol
@@ -8,25 +8,34 @@ import "./FinancialOpportunity.sol";
 import "./ILendingPoolCore.sol";
 import "../TrueCurrencies/modularERC20/InstantiatableOwnable.sol";
 
+/**
+ * @title AaveFinancialOpportunity
+ * @dev Financial Opportunity to earn TrueUSD with Aave.
+ * stakeToken = aTUSD on Aave
+ * We assume perTokenValue always increases
+ */
 contract AaveFinancialOpportunity is FinancialOpportunity, InstantiatableOwnable {
     using SafeMath for uint256;
 
-    IAToken public sharesToken;
-    ILendingPool public lendingPool;
-    TrueRewardBackedToken public token;
+    IAToken public stakeToken; // aToken
+    ILendingPool public lendingPool; // lending pool
+    TrueRewardBackedToken public token; // TrueRewardBackedToken (TrueUSD)
 
     modifier onlyProxyOwner() {
         require(msg.sender == proxyOwner(), "only proxy owner");
         _;
     }
 
+    /**
+     * @dev Set up Aave Opportunity
+     */
     function configure(
-        IAToken _sharesToken,
-        ILendingPool _lendingPool,
-        TrueRewardBackedToken _token,
-        address _owner
+        IAToken _stakeToken,            // aToken
+        ILendingPool _lendingPool,      // lendingPool interface
+        TrueRewardBackedToken _token,   // TrueRewardBackedToken
+        address _owner                  // owner
     ) public onlyProxyOwner {
-        sharesToken = _sharesToken;
+        stakeToken = _stakeToken;
         lendingPool = _lendingPool;
         token = _token;
         owner = _owner;
@@ -42,13 +51,15 @@ contract AaveFinancialOpportunity is FinancialOpportunity, InstantiatableOwnable
     }
 
     function getBalance() public view returns(uint256) {
-        return sharesToken.balanceOf(address(this));
+        return stakeToken.balanceOf(address(this));
     }
 
-    function getValueInShares(uint256 _amount) public view returns(uint256) {
+    /** @dev Return value of stake in TUSD */
+    function getValueInStake(uint256 _amount) public view returns(uint256) {
         return _amount.mul(10**18).div(perTokenValue());
     }
 
+    /** @dev Deposit TUSD into Aave */
     function deposit(address _from, uint256 _amount) external onlyOwner returns(uint256) {
         require(token.transferFrom(_from, address(this), _amount), "transfer from failed");
         require(token.approve(address(lendingPool), _amount), "approve failed");
@@ -57,18 +68,19 @@ contract AaveFinancialOpportunity is FinancialOpportunity, InstantiatableOwnable
         lendingPool.deposit(address(token), _amount, 0);
         uint256 balanceAfter = getBalance();
 
-        return getValueInShares(balanceAfter.sub(balanceBefore));
+        return getValueInStake(balanceAfter.sub(balanceBefore));
     }
 
+    /** @dev Helper to withdraw TUSD from Aave */
     function _withdraw(address _to, uint256 _amount) internal returns(uint256) {
         uint256 balanceBefore = token.balanceOf(address(this));
-        sharesToken.redeem(_amount);
+        stakeToken.redeem(_amount);
         uint256 balanceAfter = token.balanceOf(address(this));
         uint256 fundsWithdrawn = balanceAfter.sub(balanceBefore);
 
         require(token.transfer(_to, fundsWithdrawn), "transfer failed");
 
-        return getValueInShares(fundsWithdrawn);
+        return getValueInStake(fundsWithdrawn);
     }
 
     function withdrawTo(address _to, uint256 _amount) external onlyOwner returns(uint256) {
