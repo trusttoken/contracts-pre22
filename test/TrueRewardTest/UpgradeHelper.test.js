14931b472981d38d8c6cbc8baf9d1bb35a87a612
Marik-D <maretskii@gmail.com>
2020-04-08 21:44:02 +0200

    Remove from owner everywhere

--- test/TrueRewardTest/UpgradeHelper.test.js
+++ test/TrueRewardTest/UpgradeHelper.test.js
@@ -9,30 +9,30 @@ const TokenController = artifacts.require('TokenController')
 
 contract('UpgradeHelper', function ([owner]) {
   beforeEach(async function () {
-    this.fractionalExponents = await FractionalExponents.new({from: owner})
+    this.fractionalExponents = await FractionalExponents.new()
 
-    this.tusdProxy = await OwnedUpgradeabilityProxy.new({from: owner})
-    this.assuredFinancialOpportunityProxy = await OwnedUpgradeabilityProxy.new({from: owner})
+    this.tusdProxy = await OwnedUpgradeabilityProxy.new()
+    this.assuredFinancialOpportunityProxy = await OwnedUpgradeabilityProxy.new()
 
-    this.oldTusdImplementation = await TrueUSDMock.new(owner, 0, {from: owner})
+    this.oldTusdImplementation = await TrueUSDMock.new(owner, 0)
     await this.tusdProxy.upgradeTo(this.oldTusdImplementation.address)
     await (await TrueUSDMock.at(this.tusdProxy.address)).setOwner(owner)
+    this.tusdImplementation = await TrueUSD.new()
 
-    this.tusdImplementation = await TrueUSD.new({from: owner})
-    this.assuredFinancialOpportunityImplementation = await AssuredFinancialOpportunity.new({from: owner})
+    this.assuredFinancialOpportunityImplementation = await AssuredFinancialOpportunity.new()
     this.assuredFinancialOpportunity = await AssuredFinancialOpportunity.at(this.assuredFinancialOpportunityProxy.address)
 
-    this.financialOpportunityMock = await ConfigurableFinancialOpportunityMock.new(this.tusdProxy.address, { from: owner })
+    this.financialOpportunityMock = await ConfigurableFinancialOpportunityMock.new(this.tusdProxy.address)
 
-    this.upgradeHelper = await UpgradeHelper.new({ from: owner })
+    this.tokenControllerProxy = await OwnedUpgradeabilityProxy.new()
+    this.tokenControllerImplementation = await TokenController.new()
 
-    await this.tusdProxy.transferProxyOwnership(this.upgradeHelper.address, {from: owner})
+    this.upgradeHelper = await UpgradeHelper.new()
+
+    await this.tusdProxy.transferProxyOwnership(this.upgradeHelper.address)
     this.tusd = await TrueUSD.at(this.tusdProxy.address)
     await this.tusd.transferOwnership(this.upgradeHelper.address)
-    await this.assuredFinancialOpportunityProxy.transferProxyOwnership(this.upgradeHelper.address, {from: owner})
-
-    this.tokenControllerProxy = await OwnedUpgradeabilityProxy.new({ from: owner })
-    this.tokenControllerImplementation = await TokenController.new({ from: owner })
+    await this.assuredFinancialOpportunityProxy.transferProxyOwnership(this.upgradeHelper.address)
     await this.tokenControllerProxy.transferProxyOwnership(this.upgradeHelper.address)
 
     await this.upgradeHelper.performUpgrade(
