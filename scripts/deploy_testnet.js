02c5a4c110be922d7f1b6a2201a745dafb7e65a9
Harold Hyatt <haroldhyatt@Hals-MBP.hsd1.ca.comcast.net>
2020-04-14 05:45:40 -0700

    uniswap deploy

--- scripts/deploy_testnet.js
+++ scripts/deploy_testnet.js
@@ -10,68 +10,77 @@
 // this might be unsafe. We want to use ethereum accounts instead of hardcode pk
 
 (async () => {
-    const config = {
-        rpc: process.argv[3] || 'http://localhost:7545',
-        accountPrivateKey: process.argv[2],
-        network: 5777,
-        gas: 40000000
-    }
+  const config = {
+    rpc: process.argv[3] || 'http://localhost:7545',
+    accountPrivateKey: process.argv[2],
+    network: 5777,
+    gas: 40000000
+  }
 
-    const ethers = require('ethers')
-    const { setupDeployer, validatePrivateKey } = require('./utils')
-    
-    validatePrivateKey(config.accountPrivateKey)
+  const ethers = require('ethers')
+  const { setupDeployer, getContract, validatePrivateKey } = require('./utils')
+  
+  validatePrivateKey(config.accountPrivateKey)
 
-    const provider = new ethers.providers.JsonRpcProvider(config.rpc)
-    const wallet = new ethers.Wallet(config.accountPrivateKey, provider)
+  const provider = new ethers.providers.JsonRpcProvider(config.rpc)
+  const wallet = new ethers.Wallet(config.accountPrivateKey, provider)
 
-    const deploy = setupDeployer(ethers, wallet)
+  const deploy = setupDeployer(ethers, wallet)
+  const contractAt = getContract(ethers, wallet)
 
-    this.tusdProxy = await deploy('OwnedUpgradeabilityProxy')
-    this.controllerProxy = await deploy('OwnedUpgradeabilityProxy')
-    this.assuranceProxy = await deploy('OwnedUpgradeabilityProxy')
+  this.tusdProxy = await deploy('OwnedUpgradeabilityProxy')
+  this.controllerProxy = await deploy('OwnedUpgradeabilityProxy')
+  this.assuranceProxy = await deploy('OwnedUpgradeabilityProxy')
 
-    // Deploy all contracts
-    this.tusd = await deploy('TrueUSD')
-    this.registry = await deploy('ProvisionalRegistryImplementation')
-    this.tokenController = await deploy('TokenController')
-    this.aaveFinancialOpportunity = await deploy('AaveFinancialOpportunity')
-    this.assuredFinancialOpportunity = await deploy('AssuredFinancialOpportunity')
-    this.ownedUpgradeabilityProxy = await deploy('OwnedUpgradeabilityProxy')
-    this.fractionalExponents = await deploy('FractionalExponents')
+  // Deploy all contracts
+  this.tusd = await deploy('TrueUSD')
+  this.registry = await deploy('ProvisionalRegistryImplementation')
+  this.tokenController = await deploy('TokenController')
+  this.aaveFinancialOpportunity = await deploy('AaveFinancialOpportunity')
+  this.assuredFinancialOpportunity = await deploy('AssuredFinancialOpportunity')
+  this.ownedUpgradeabilityProxy = await deploy('OwnedUpgradeabilityProxy')
+  this.fractionalExponents = await deploy('FractionalExponents')
 
-    // deploy trusttoken
-    this.trusttoken = await deploy('MockTrustToken', this.registry.address)
+  // deploy trusttoken
+  this.trusttoken = await deploy('MockTrustToken', this.registry.address)
 
-    // setup uniswap
-    this.uniswapFactory = await deploy('contracts/uniswap_factory')
-    this.uniswapTemplate = await deploy('contracts/uniswap_exchange')
-    //this.tusdUniswapAddress = (await this.uniswapFactory.createExchange(
-    //    this.tusdProxy.address))//.logs[0].args.exchange
-    this.tusdUniswap = await UniswapExchange.at(this.tusdUniswapAddress)
-    this.tusdUniswap = new ethers.Contract(this.tusdUniswapAddress, contractJson.abi, wallet)
-    this.trustUniswapAddress = (await this.uniswapFactory.createExchange(
-        this.trusttoken.address)).logs[0].args.exchange
-    this.trustUniswap = await UniswapExchange.at(this.trustUniswapAddress)
+  // setup uniswap
+  // needs to compile using truffle compile
+  this.uniswapFactory = await deploy('contracts/uniswap_factory')
+  this.uniswapTemplate = await deploy('contracts/uniswap_exchange')
+  await this.uniswapFactory.initializeFactory(this.uniswapTemplate.address)
 
-    // deploy liquidator
-    this.liquidator = await deploy('Liquidator', this.registry.address, 
-        this.tusd.address, this.trusttoken.address, this.tusdUniswap.address, 
-        this.trustUniswap.address)
-    
-    // deploy assurance pool
-    this.assurancePool = await deploy('StakedToken', this.trusttoken.address,
-        this.tusd.address, this.registry.address,
-        this.liquidator.address)
-    
-    // Deploy UpgradeHelper
-    this.deployHelper = await deploy('DeployHelper')
-    
-    // transfer proxy ownership to deploy helper
-    await this.controllerProxy.transferProxyOwnership(this.deployHelper.address)
-    await this.tusdProxy.transferProxyOwnership(this.deployHelper.address)
-    await this.liquidator.transferOwnership(this.deployHelper.address)
-    await this.assuredOpportunityProxy.transferProxyOwnership(this.deployHelper.address)
-    await this.registry.transferOwnership(this.deployHelper.address)
+  this.tusdUniswapExchange = await this.uniswapFactory.createExchange(this.tusd.address, {gasPrice: 70000000})
+  
+  console.log(this.tusdUniswapExchange)
+  // this.tusdUniswapAddress = (await this.uniswapFactory.createExchange(
+  //   this.tusdProxy.address))//.logs[0].args.exchange
+  this.tusdUniswap = await contractAt('contracts/uniswap_exchange', 
+    this.tusdUniswap.address)
+
+  this.trustUniswapAddress = (await this.uniswapFactory.createExchange(
+    this.trusttoken.address)).logs[0].args.exchange
+
+  this.trustUniswap = await UniswapExchange.at(this.trustUniswapAddress)
+
+  // deploy liquidator
+  this.liquidator = await deploy('Liquidator', this.registry.address, 
+    this.tusd.address, this.trusttoken.address, this.tusdUniswap.address, 
+    this.trustUniswap.address)
+  
+  // deploy assurance pool
+  this.assurancePool = await deploy('StakedToken', this.trusttoken.address,
+    this.tusd.address, this.registry.address,
+    this.liquidator.address)
+  
+  // Deploy UpgradeHelper
+  this.deployHelper = await deploy('DeployHelper')
+  
+  // transfer proxy ownership to deploy helper
+  await this.controllerProxy.transferProxyOwnership(this.deployHelper.address)
+  await this.tusdProxy.transferProxyOwnership(this.deployHelper.address)
+  await this.liquidator.transferOwnership(this.deployHelper.address)
+  await this.assuredOpportunityProxy.transferProxyOwnership(this.deployHelper.address)
+  await this.registry.transferOwnership(this.deployHelper.address)
 
 })()
