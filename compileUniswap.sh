4f58509b230e0b3794789e3eb1276045edba59d3
Anze1m <bartek@ethworks.io>
2020-04-14 14:42:29 +0200

    Read proper output directory

--- compileUniswap.sh
+++ compileUniswap.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+ODline=$(grep 'outputDirectory' .waffle.json)
+regex=': \"(.+)\"'
+[[ $ODline =~ $regex ]]
+outputDir=${BASH_REMATCH[1]}
+
 UFabi=$(vyper ./contracts/lib/uniswap/contracts/uniswap_factory.vy -f abi)
 UFbytecode=$(vyper ./contracts/lib/uniswap/contracts/uniswap_factory.vy -f bytecode)
-rm -rf build/uniswap_factory.json
+rm -rf $outputDir/uniswap_factory.json
 echo "{
     \"bytecode\": \"$UFbytecode\",
     \"abi\": $UFabi
-}" >> build/uniswap_factory.json
+}" >> $outputDir/uniswap_factory.json
 
 UEabi=$(vyper ./contracts/lib/uniswap/contracts/uniswap_exchange.vy -f abi)
 UEbytecode=$(vyper ./contracts/lib/uniswap/contracts/uniswap_exchange.vy -f bytecode)
-rm -rf build/uniswap_exchange.json
+rm -rf $outputDir/uniswap_exchange.json
 echo "{
     \"bytecode\": \"$UEbytecode\",
     \"abi\": $UEabi
-}" >> build/uniswap_exchange.json
+}" >> $outputDir/uniswap_exchange.json
