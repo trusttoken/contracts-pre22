dce01a02521d2577fab29128254c696232c16362
Anze1m <bartek@ethworks.io>
2020-04-15 16:26:46 +0200

    Fix dependencies

--- contracts/mocks/Dependencies.sol
+++ contracts/mocks/Dependencies.sol
@@ -6,10 +6,11 @@ import "wjm-airswap-swap/contracts/Swap.sol";
 import "wjm-airswap-transfers/contracts/TransferHandlerRegistry.sol";
 import "wjm-airswap-transfers/contracts/handlers/ERC20TransferHandler.sol";
 import "@trusttoken/trusttokens/contracts/StakedToken.sol";
-import "@trusttoken/trusttokens/contracts/mocks/MockTrustToken.sol";
 import "@trusttoken/trusttokens/contracts/StakingOpportunityFactory.sol";
 import "@trusttoken/trusttokens/contracts/Liquidator.sol";
+import "@trusttoken/trusttokens/contracts/mocks/MockTrustToken.sol";
 import "@trusttoken/trusttokens/contracts/mocks/MultisigLiquidatorMock.sol";
+import { UnlockTrustTokens } from "@trusttoken/trusttokens/contracts/UnlockTrustTokens.sol";
 
 
 contract Airswap is Swap {}
@@ -21,3 +22,5 @@ contract AirswapERC20TransferHandler is ERC20TransferHandler {}
 contract TrustTokenDependencies is StakedToken {}
 
 contract TrustTokenLiquidator is Liquidator {}
+
+contract TrustTokenUnlockTrustTokens is UnlockTrustTokens {}
