2a12f019beaf739ca5289d1607f4ad6033b899ce
Harold Hyatt <haroldhyatt@Hals-MacBook-Pro.local>
2020-06-03 12:38:52 -0700

    script to deploy pause contract

--- scripts/pause_contract_deploy.ts
+++ scripts/pause_contract_deploy.ts
@@ -1,14 +1,15 @@
 /**
- * ts-node scripts/multisig_deploy.ts "{private_key}" "{network}"
+ * ts-node scripts/pause_contract_deploy.ts "{private_key}" "{network}"
  */
 import { ethers, providers } from 'ethers'
-import { PausedDelegateERC20Factory } from '../build/types/PausedDelegateERC20Factory'
+import { PausedTrueUsdFactory } from '../build/types/PausedTrueUsdFactory'
 
 async function deployPause () {
+  const txnArgs = { gasLimit: 5_000_000, gasPrice: 16_000_000_000 }
   const provider = new providers.InfuraProvider(process.argv[3], '81447a33c1cd4eb09efb1e8c388fb28e')
   const wallet = new ethers.Wallet(process.argv[2], provider)
 
-  const pausedTokenContract = await (await new PausedDelegateERC20Factory(wallet).deploy()).deployed()
+  const pausedTokenContract = await (await new PausedTrueUsdFactory(wallet).deploy(txnArgs)).deployed()
   console.log("PausedToken at: ", pausedTokenContract.address)
 }
 
