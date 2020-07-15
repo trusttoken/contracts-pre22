5a60d4f8bcc16234fb1e651f5b14e26bac304994
Krzysztof Jelski <krzysztof@ethworks.io>
2020-07-15 14:38:11 +0200

    Move TimeOwnedUpgradeabilityProxy tests to TS suite.

--- test-ts/trueCurrencies/proxy/TimeOwnedUpgradeabilityProxy.test.ts
+++ test-ts/trueCurrencies/proxy/TimeOwnedUpgradeabilityProxy.test.ts
@@ -1,29 +1,33 @@
-import assertRevert from '../../../test/helpers/assertRevert.js'
-import timeMachine from 'ganache-time-traveler'
 import { solidity } from 'ethereum-waffle'
 import { Wallet } from 'ethers'
 import { beforeEachWithFixture } from '../../utils/beforeEachWithFixture'
 import { setupDeploy } from '../../../scripts/utils'
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import { TimeOwnedUpgradeabilityProxyFactory } from '../../../build/types/TimeOwnedUpgradeabilityProxyFactory'
+import { timeTravel } from '../../utils/timeTravel'
+import { TimeOwnedUpgradeabilityProxy } from '../../../build/types/TimeOwnedUpgradeabilityProxy'
 
 chai.use(solidity)
 
 describe('TimeOwnedUpgradeabilityProxy', () => {
   const address = Wallet.createRandom().address
-  let timeOwnedUpgradeabilityProxy
+  let timeOwnedUpgradeabilityProxy: TimeOwnedUpgradeabilityProxy
   let getCurrentTimestamp: () => Promise<number>
+  let advanceTime: (time: number) => Promise<void>
 
-  const getCurrentExpirationTimestamp = async (timeOwnedUpgradeabilityProxy) => parseInt((await timeOwnedUpgradeabilityProxy.expiration()).toString())
+  async function getCurrentExpirationTimestamp () {
+    return (await timeOwnedUpgradeabilityProxy.expiration()).toNumber()
+  }
 
   beforeEachWithFixture(async (_provider, [owner]) => {
-    const getCurrentTimestampFrom = (provider) => async () => (await provider.getBlock('latest')).timestamp
     timeOwnedUpgradeabilityProxy = await setupDeploy(owner)(TimeOwnedUpgradeabilityProxyFactory)
-    getCurrentTimestamp = getCurrentTimestampFrom(_provider)
+    getCurrentTimestamp = async () => (await _provider.getBlock('latest')).timestamp
+    advanceTime = (time: number) => timeTravel(_provider, time)
   })
 
   it('does not allow upgrade after certain time passes', async () => {
-    await timeMachine.advanceTime(60 * 60 * 24 * 124 + 10)
-    await assertRevert(timeOwnedUpgradeabilityProxy.upgradeTo(address))
+    await advanceTime(60 * 60 * 24 * 124 + 10)
+    await expect(timeOwnedUpgradeabilityProxy.upgradeTo(address)).to.be.revertedWith('after expiration date')
   })
 
   it('allows upgrade before certain time passes', async () => {
@@ -32,43 +36,43 @@ describe('TimeOwnedUpgradeabilityProxy', () => {
   })
 
   it('allows upgrade before some, but not enough time passes', async () => {
-    await timeMachine.advanceTime(60 * 60 * 24 * 124 - 10)
+    await advanceTime(60 * 60 * 24 * 124 - 10)
     await timeOwnedUpgradeabilityProxy.upgradeTo(address)
     expect(await timeOwnedUpgradeabilityProxy.implementation()).to.equal(address)
   })
 
   it('allows set before certain time passes', async () => {
-    await timeMachine.advanceTime(60 * 60 * 24 * 124 - 10)
-    const currentExpiration = await getCurrentExpirationTimestamp(timeOwnedUpgradeabilityProxy)
+    await advanceTime(60 * 60 * 24 * 124 - 10)
+    const currentExpiration = await getCurrentExpirationTimestamp()
     await timeOwnedUpgradeabilityProxy.setExpiration(currentExpiration + 30)
-    await timeMachine.advanceTime(20)
+    await advanceTime(20)
     await timeOwnedUpgradeabilityProxy.upgradeTo(address)
     expect(await timeOwnedUpgradeabilityProxy.implementation()).to.equal(address)
   })
 
   it('does not allow to upgrade if extended time already passes', async () => {
-    await timeMachine.advanceTime(60 * 60 * 24 * 124 - 10)
-    const currentExpiration = await getCurrentExpirationTimestamp(timeOwnedUpgradeabilityProxy)
+    await advanceTime(60 * 60 * 24 * 124 - 10)
+    const currentExpiration = await getCurrentExpirationTimestamp()
     await timeOwnedUpgradeabilityProxy.setExpiration(currentExpiration + 20)
-    await timeMachine.advanceTime(40)
-    expect(timeOwnedUpgradeabilityProxy.upgradeTo(address)).to.be.revertedWith('dsfa')
+    await advanceTime(40)
+    await expect(timeOwnedUpgradeabilityProxy.upgradeTo(address)).to.be.revertedWith('after expiration date')
   })
 
   it('does not allow to upgrade if expiration time was decreased and passed', async () => {
-    const currentExpiration = await getCurrentExpirationTimestamp(timeOwnedUpgradeabilityProxy)
+    const currentExpiration = await getCurrentExpirationTimestamp()
     await timeOwnedUpgradeabilityProxy.setExpiration(currentExpiration - 20)
-    await timeMachine.advanceTime(60 * 60 * 24 * 124 - 10)
-    expect(timeOwnedUpgradeabilityProxy.upgradeTo(address)).to.be.revertedWith('dsfa')
+    await advanceTime(60 * 60 * 24 * 124 - 10)
+    await expect(timeOwnedUpgradeabilityProxy.upgradeTo(address)).to.be.revertedWith('after expiration date')
   })
 
   it('does not allow to set expiration time to the past', async function () {
     const currentTimestamp = await getCurrentTimestamp()
-    expect(timeOwnedUpgradeabilityProxy.setExpiration(currentTimestamp - 20)).to.be.revertedWith('dsfa')
+    await expect(timeOwnedUpgradeabilityProxy.setExpiration(currentTimestamp - 20)).to.be.revertedWith('new expiration time must be in the future')
   })
 
   it('does not allow set after certain time passes', async function () {
-    const currentExpiration = await getCurrentExpirationTimestamp(timeOwnedUpgradeabilityProxy)
-    await timeMachine.advanceTime(60 * 60 * 24 * 124 + 10)
-    expect(timeOwnedUpgradeabilityProxy.setExpiration(currentExpiration + 20)).to.be.revertedWith('dsfa')
+    const currentExpiration = await getCurrentExpirationTimestamp()
+    await advanceTime(60 * 60 * 24 * 124 + 10)
+    await expect(timeOwnedUpgradeabilityProxy.setExpiration(currentExpiration + 20)).to.be.revertedWith('after expiration time')
   })
 })
