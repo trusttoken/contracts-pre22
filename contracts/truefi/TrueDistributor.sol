4cb0b552e48cb826da18398faeb87215d680d8ec
Anze1m <bartek@ethworks.io>
2020-09-03 15:48:51 +0200

    refactor shares to be in a struct

--- contracts/truefi/TrueDistributor.sol
+++ contracts/truefi/TrueDistributor.sol
@@ -7,17 +7,21 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract TrueDistributor is Ownable {
     using SafeMath for uint256;
 
+    struct Farm {
+        uint256 share;
+    }
+
     uint256 public constant DISTRIBUTION_FACTOR = 26824995976250469437449703116;
     uint256 public constant TOTAL_BLOCKS = 1e7;
     uint256 public constant PRECISION = 1e33;
     uint256 public constant TOTAL_SHARES = 1e7;
 
     uint256 public startingBlock;
-    mapping(address => uint256) shares;
+    mapping(address => Farm) public farms;
 
     constructor(uint256 _startingBlock) public {
         startingBlock = _startingBlock;
-        shares[msg.sender] = TOTAL_SHARES;
+        farms[msg.sender].share = TOTAL_SHARES;
     }
 
     function transfer(
@@ -25,12 +29,12 @@ contract TrueDistributor is Ownable {
         address toFarm,
         uint256 sharesAmount
     ) public onlyOwner {
-        shares[fromFarm] = shares[fromFarm].sub(sharesAmount);
-        shares[toFarm] = shares[toFarm].add(sharesAmount);
+        farms[fromFarm].share = farms[fromFarm].share.sub(sharesAmount);
+        farms[toFarm].share = farms[toFarm].share.add(sharesAmount);
     }
 
     function getShares(address farm) public view returns (uint256) {
-        return shares[farm];
+        return farms[farm].share;
     }
 
     function reward(uint256 fromBlock, uint256 toBlock) public view returns (uint256) {
