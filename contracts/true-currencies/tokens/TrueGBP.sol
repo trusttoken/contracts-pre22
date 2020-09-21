75c9a3de5c3c793409325048210c0b7d38ebe3ec
hal <61718761+hal909@users.noreply.github.com>
2020-09-21 02:52:32 -0700

    organize folders (#302)
    Co-authored-by: Harold Hyatt <haroldhyatt@Hals-MBP.hsd1.ca.comcast.net>

--- contracts/true-currencies/tokens/TrueGBP.sol
+++ contracts/true-currencies/tokens/TrueGBP.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.10;
 
-import {TrueCurrencyWithLegacyAutosweep} from "./TrueCurrencyWithLegacyAutosweep.sol";
+import {TrueCurrencyWithGasRefund} from "../TrueCurrencyWithGasRefund.sol";
 
 /**
- * @title TrueUSD
+ * @title TrueGBP
  * @dev This is the top-level ERC20 contract, but most of the interesting functionality is
  * inherited - see the documentation on the corresponding contracts.
  */
-contract TrueUSD is TrueCurrencyWithLegacyAutosweep {
+contract TrueGBP is TrueCurrencyWithGasRefund {
     uint8 constant DECIMALS = 18;
     uint8 constant ROUNDING = 2;
 
@@ -21,10 +21,10 @@ contract TrueUSD is TrueCurrencyWithLegacyAutosweep {
     }
 
     function name() public override pure returns (string memory) {
-        return "TrueUSD";
+        return "TrueGBP";
     }
 
     function symbol() public override pure returns (string memory) {
-        return "TUSD";
+        return "TGBP";
     }
 }
--- contracts/true-currencies/tokens/TrueHKD.sol
+++ contracts/true-currencies/tokens/TrueHKD.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.10;
 
-import {TrueCurrencyWithGasRefund} from "./TrueCurrencyWithGasRefund.sol";
+import {TrueCurrencyWithGasRefund} from "../TrueCurrencyWithGasRefund.sol";
 
 /**
- * @title TrueGBP
+ * @title TrueHKD
  * @dev This is the top-level ERC20 contract, but most of the interesting functionality is
  * inherited - see the documentation on the corresponding contracts.
  */
-contract TrueGBP is TrueCurrencyWithGasRefund {
+contract TrueHKD is TrueCurrencyWithGasRefund {
     uint8 constant DECIMALS = 18;
     uint8 constant ROUNDING = 2;
 
@@ -21,10 +21,10 @@ contract TrueGBP is TrueCurrencyWithGasRefund {
     }
 
     function name() public override pure returns (string memory) {
-        return "TrueGBP";
+        return "TrueHKD";
     }
 
     function symbol() public override pure returns (string memory) {
-        return "TGBP";
+        return "THKD";
     }
 }
