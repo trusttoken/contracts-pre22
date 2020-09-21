0c6c39fa675e004ceb769cdde6962d24b56c5b57
hal <61718761+hal909@users.noreply.github.com>
2020-09-21 02:52:32 -0700

    organize folders (#302)
    Co-authored-by: Harold Hyatt <haroldhyatt@Hals-MBP.hsd1.ca.comcast.net>

--- contracts/true-currencies/tokens/TrueAUD.sol
+++ contracts/true-currencies/tokens/TrueAUD.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.10;
 
-import {TrueCurrencyWithGasRefund} from "./TrueCurrencyWithGasRefund.sol";
+import {TrueCurrencyWithGasRefund} from "../TrueCurrencyWithGasRefund.sol";
 
 /**
  * title TrueAUD
