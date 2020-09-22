f3c0e346c8af0a7e4b4a286c66c2c49fcd512e11
Anze1m <bartek@ethworks.io>
2020-09-22 11:48:02 +0200

    Add minor refactoring

--- scripts/deploy_curve_pool.ts
+++ scripts/deploy_curve_pool.ts
@@ -10,8 +10,8 @@ async function deployCurvePool () {
   const provider = new providers.InfuraProvider(process.argv[3], '81447a33c1cd4eb09efb1e8c388fb28e')
   const wallet = new ethers.Wallet(process.argv[2], provider)
 
-  const tusdAddress = await ask('TUSD token address')
-  const curveAddress = await ask('Curve TUSD deposit address')
+  const tusdAddress = await ask('TUSD token address: ')
+  const curveAddress = await ask('Curve TUSD deposit address: ')
 
   const curvePool = await (await new CurvePoolFactory(wallet).deploy(curveAddress, tusdAddress, txnArgs)).deployed()
   console.log('Curve Pool address: ', curvePool.address)
--- scripts/deploy_truefi.ts
+++ scripts/deploy_truefi.ts
@@ -12,21 +12,21 @@ async function deployTrueFi () {
   const provider = new providers.InfuraProvider(process.argv[3], '81447a33c1cd4eb09efb1e8c388fb28e')
   const wallet = new ethers.Wallet(process.argv[2], provider)
 
-  const truAddress = await ask('TrustToken address')
+  const truAddress = await ask('TrustToken address: ')
   console.log('Current block ', await provider.getBlockNumber())
-  const startingBlock = Number.parseInt(await ask('Starting block'))
+  const startingBlock = Number.parseInt(await ask('Starting block: '))
   const slowDistributor = await (await new SlowTrueDistributorFactory(wallet).deploy(startingBlock, truAddress, txnArgs)).deployed()
   const fastDistributor = await (await new FastTrueDistributorFactory(wallet).deploy(startingBlock, truAddress, txnArgs)).deployed()
 
-  const blpAddress = await ask('Balancer BAL/TRU LP address')
+  const blpAddress = await ask('Balancer BAL/TRU LP address: ')
   const balancerFarm = await (await new TrueFarmFactory(wallet).deploy(blpAddress, fastDistributor.address, txnArgs)).deployed()
   console.log('Balancer TrueFarm address: ', balancerFarm.address)
 
-  const ulpEthAddress = await ask('Uniswap ETH/TRU LP address')
+  const ulpEthAddress = await ask('Uniswap ETH/TRU LP address: ')
   const uniswapEthFarm = await (await new TrueFarmFactory(wallet).deploy(ulpEthAddress, fastDistributor.address, txnArgs)).deployed()
   console.log('Uniswap ETH/TRU TrueFarm address: ', uniswapEthFarm.address)
 
-  const ulpTusdAddress2 = await ask('Uniswap TUSD/TrueFiLP address')
+  const ulpTusdAddress2 = await ask('Uniswap TUSD/TrueFiLP address: ')
   const uniswapTusdFarm = await (await new TrueFarmFactory(wallet).deploy(ulpTusdAddress2, slowDistributor.address, txnArgs)).deployed()
   console.log('Uniswap TUSD/TrueFiLP TrueFarm address: ', uniswapTusdFarm.address)
 
