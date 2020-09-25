f2caac59ccaf65b0b01f6acd3dfd2dadd309fe4e
Ivan Rukhavets <ivanruch@gmail.com>
2020-09-25 17:13:09 +0200

    parametrize LendingPool

--- contracts/truefi/TrueLendingPool.sol
+++ contracts/truefi/TrueLendingPool.sol
@@ -11,11 +11,63 @@ contract TrueLendingPool is Ownable {
     mapping(address => bool) public whitelisted;
     ITrueFiPool public immutable pool;
 
+    // ===== Pool parameters =====
+    uint256 public minLoanSize = 1000000 ether;
+    uint256 public maxLoanSize = 10000000 ether;
+    /**
+     * @notice Minimal APY for a loan
+     * @dev % multiplied by 100. e.g. 10.5% = 1050
+     */
+    uint256 public minApy = 1000;
+    /**
+     * @notice Minimum TRU voted YES required to approve a loan:
+     * Loan size in TUSD * factor -> initial factor is 0.7
+     * @dev % multiplied by 100. e.g. 10.5% = 1050
+     */
+    uint256 public minLoanApprovalFactor = 7000;
+    /**
+     * @notice Minimum ratio YES votes required to pass
+     * @dev % multiplied by 100. e.g. 10.5% = 1050
+     */
+    uint256 public minLoanApprovalVoteRatio = 7000;
+    /**
+     * @notice How many TRU tokens are burnt for yes-voters in case loan defaults
+     * @dev % multiplied by 100. e.g. 10.5% = 1050
+     */
+    uint256 public burnFactor = 8000;
+
     event Whitelisted(address indexed who, bool status);
+    event LoanBoundsChanged(uint256 minLoanSize, uint256 maxLoanSize);
+    event MinApyChanged(uint256 minApy);
+    event LoanApprovalConditionsChanged(uint256 minLoanApprovalFactor, uint256 minLoanApprovalVoteRatio);
+    event BurnFactorChanged(uint256 burnFactor);
 
     constructor(ITrueFiPool _pool) public {
         pool = _pool;
         _pool.token().approve(address(_pool), uint256(-1));
+    }
+
+    function setLoanBounds(uint256 min, uint256 max) external onlyOwner {
+        require(max >= min, "maximum loan size smaller than minimal");
+        minLoanSize = min;
+        maxLoanSize = max;
+        emit LoanBoundsChanged(min, max);
+    }
+
+    function setMinApy(uint256 newMinApy) external onlyOwner {
+        minApy = newMinApy;
+        emit MinApyChanged(newMinApy);
+    }
+
+    function setLoanApprovalConditions(uint256 newMinLoanApprovalFactor, uint256 newMinLoanApprovalVoteRatio) external onlyOwner {
+        minLoanApprovalFactor = newMinLoanApprovalFactor;
+        minLoanApprovalVoteRatio = newMinLoanApprovalVoteRatio;
+        emit LoanApprovalConditionsChanged(newMinLoanApprovalFactor, newMinLoanApprovalVoteRatio);
+    }
+
+    function setBurnFactor(uint256 newBurnFactor) external onlyOwner {
+        burnFactor = newBurnFactor;
+        emit BurnFactorChanged(newBurnFactor);
     }
 
     function whitelistForLoan(address who, bool status) external onlyOwner {
--- test-ts/truefi/TrueLendingPool.test.ts
+++ test-ts/truefi/TrueLendingPool.test.ts
@@ -10,6 +10,7 @@ import { MockTrueCurrency } from '../../build/types/MockTrueCurrency'
 import { MockTrueCurrencyFactory } from '../../build/types/MockTrueCurrencyFactory'
 import ITrueFiPoolJson from '../../build/ITrueFiPool.json'
 import { deployMockContract } from 'ethereum-waffle'
+import { BigNumberish, parseEther } from 'ethers/utils'
 
 describe('TrueLendingPool', () => {
   let owner: Wallet
@@ -35,6 +36,15 @@ describe('TrueLendingPool', () => {
     it('approves infinite amount to underlying pool', async () => {
       expect(await tusd.allowance(lendingPool.address, underlyingPool.address)).to.equal(MaxUint256)
     })
+
+    it('default params', async () => {
+      expect(await lendingPool.minLoanSize()).to.equal(parseEther('1000000'))
+      expect(await lendingPool.maxLoanSize()).to.equal(parseEther('10000000'))
+      expect(await lendingPool.minApy()).to.equal('1000')
+      expect(await lendingPool.minLoanApprovalFactor()).to.equal('7000')
+      expect(await lendingPool.minLoanApprovalVoteRatio()).to.equal('7000')
+      expect(await lendingPool.burnFactor()).to.equal('8000')
+    })
   })
 
   describe('Whitelisting', () => {
@@ -56,6 +66,50 @@ describe('TrueLendingPool', () => {
     it('reverts when performed by non-owner', async () => {
       await expect(lendingPool.connect(otherWallet).whitelistForLoan(otherWallet.address, true))
         .to.be.revertedWith('caller is not the owner')
+    })
+  })
+
+  describe('Parameters set up', () => {
+    async function shouldChangeParameters (
+      methodName: string,
+      values: BigNumberish[],
+      getterNames: string[],
+      eventName: keyof TrueLendingPool['filters'],
+    ) {
+      if (typeof methodName !== 'string') {
+        return
+      }
+      describe(methodName, () => {
+        it(`changes ${getterNames}`, async () => {
+          await lendingPool.functions[methodName](...values)
+          for (let i = 0; i < getterNames.length; i++) {
+            expect(await lendingPool.functions[getterNames[i]]()).to.equal(values[i])
+          }
+        })
+
+        it(`emits ${eventName}`, async () => {
+          await expect(lendingPool.functions[methodName](...values)).to.emit(lendingPool, eventName).withArgs(...values)
+        })
+
+        it('must be called by owner', async () => {
+          await expect(lendingPool.connect(otherWallet).functions[methodName](...values)).to.be.revertedWith('caller is not the owner')
+        })
+      })
+    }
+
+    shouldChangeParameters('setLoanBounds', [1000, 100000], ['minLoanSize', 'maxLoanSize'], 'LoanBoundsChanged')
+    shouldChangeParameters('setMinApy', [1000], ['minApy'], 'MinApyChanged')
+    shouldChangeParameters('setLoanApprovalConditions', [1000, 2000], ['minLoanApprovalFactor', 'minLoanApprovalVoteRatio'], 'LoanApprovalConditionsChanged')
+    shouldChangeParameters('setBurnFactor', [1000], ['burnFactor'], 'BurnFactorChanged')
+
+    describe('setLoanBounds', () => {
+      it('cannot set minLoanSize to be bigger than maxLoanSize', async () => {
+        await expect(lendingPool.setLoanBounds(2, 1)).to.be.revertedWith('maximum loan size smaller than minimal')
+      })
+
+      it('can set minLoanSize to same value as maxLoanSize', async () => {
+        await expect(lendingPool.setLoanBounds(2, 2)).to.be.not.reverted
+      })
     })
   })
 })
