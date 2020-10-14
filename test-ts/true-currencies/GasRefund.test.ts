a62be3aff3d8909a938702c52d02000954b8e8b0
Ivan Rukhavets <ivanruch@gmail.com>
2020-10-14 12:35:12 +0200

    fix lint

--- test-ts/true-currencies/GasRefund.test.ts
+++ test-ts/true-currencies/GasRefund.test.ts
@@ -1,5 +1,4 @@
 import { Wallet } from 'ethers'
-import { MockProvider } from 'ethereum-waffle'
 import { expect } from 'chai'
 import { MockHookFactory } from '../../build/types/MockHookFactory'
 import { MockHook } from '../../build/types/MockHook'
