3bd75cd83cd60534dbec60b15e3c09a3cb0ff11e
Anze1m <bartek@ethworks.io>
2020-10-22 15:30:41 +0200

    wip

--- contracts/truefi/LoanToken.sol
+++ contracts/truefi/LoanToken.sol
@@ -117,6 +117,10 @@ contract LoanToken is ILoanToken, ERC20 {
         emit Funded(msg.sender);
     }
 
+    function reclaim(uint256 amount) public onlyFunder onlyClosed {
+        require(currencyToken.transfer(msg.sender, amount));
+    }
+
     function allowTransfer(address account, bool _status) external override onlyFunder {
         canTransfer[account] = _status;
         emit TransferAllowanceChanged(account, _status);
