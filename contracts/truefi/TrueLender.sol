7c38036bf63963315b75bfd04665f7dd935ea213
Anze1m <bartek@ethworks.io>
2020-10-26 14:37:19 +0100

    Return total debt value

--- contracts/truefi/TrueLender.sol
+++ contracts/truefi/TrueLender.sol
@@ -13,6 +13,7 @@ contract TrueLender is Ownable {
     using SafeMath for uint256;
 
     mapping(address => bool) public allowedBorrowers;
+    ILoanToken[] loans;
 
     ITruePool public pool;
     IERC20 public currencyToken;
@@ -111,7 +112,30 @@ contract TrueLender is Ownable {
         pool.borrow(amount);
         currencyToken.approve(address(loanToken), amount);
         loanToken.fund();
+        loans.push(loanToken);
         emit Funded(address(loanToken), amount);
+    }
+
+    function valueFor(ILoanToken loanToken) public view returns (uint256) {
+        (uint256 amount, uint256 apy, uint256 duration) = loanToken.getParameters();
+        uint256 start = loanToken.start();
+        uint256 passed = block.timestamp.sub(start);
+
+        uint256 helper = amount.mul(apy).mul(duration).mul(loanToken.balanceOf(address(this)));
+        uint256 interest = helper.div(360 days).div(10000).div(loanToken.totalSupply());
+
+        if (passed > duration) {
+            passed = duration;
+        }
+        return amount.add(interest);
+    }
+
+    function value() external view returns (uint256) {
+        uint256 totalValue;
+        for (uint256 index = 0; index < loans.length; index++) {
+            totalValue = totalValue.add(valueFor(loans[index]));
+        }
+        return totalValue;
     }
 
     function reclaim(ILoanToken loanToken) external onlyOwner {
--- contracts/truefi/interface/ILoanToken.sol
+++ contracts/truefi/interface/ILoanToken.sol
@@ -14,6 +14,12 @@ interface ILoanToken is IERC20 {
 
     function apy() external view returns (uint256);
 
+    function start() external view returns (uint256);
+
+    function lender() external view returns (address);
+
+    function debt() external view returns (uint256);
+
     function status() external view returns (Status);
 
     function isLoanToken() external pure returns (bool);
