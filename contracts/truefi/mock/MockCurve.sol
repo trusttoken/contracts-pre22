ae4154f16eb26fc9f9f5420031c764b1f71e72f8
Anze1m <bartek@ethworks.io>
2020-10-27 17:13:54 +0100

    fix typo

--- contracts/truefi/mock/MockCurve.sol
+++ contracts/truefi/mock/MockCurve.sol
@@ -28,7 +28,7 @@ contract MockCurvePool is ICurvePool, Initializable {
     MockERC20Token cToken;
     MockCurve _curve;
 
-    function initalize(IERC20 _token) public initializer {
+    function initialize(IERC20 _token) public initializer {
         poolToken = _token;
         cToken = new MockERC20Token();
         _curve = new MockCurve();
