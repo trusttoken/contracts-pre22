11be722a5b3325f0ebaec18bb1c1061bcb389b8a
Anze1m <bartek@ethworks.io>
2020-10-29 12:13:49 +0100

    Add ArbitraryDistributor

---
+++ contracts/truefi/distributors/ArbitraryDistributor.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {Initializable} from "../upgradeability/Initializable.sol";
+
+contract ArbitraryDistributor is Initializable {
+    using SafeMath for uint256;
+
+    IERC20 public trustToken;
+    address public beneficiary;
+    uint256 public amount;
+    uint256 public remaining;
+
+    function initialize(
+        address _beneficiary,
+        IERC20 _trustToken,
+        uint256 _amount
+    ) public initializer {
+        trustToken = _trustToken;
+        beneficiary = _beneficiary;
+        amount = _amount;
+        remaining = _amount;
+    }
+
+    modifier onlyBeneficiary {
+        require(msg.sender == beneficiary, "ArbitraryDistributor: Only beneficiary can distribute tokens");
+        _;
+    }
+
+    function distribute(uint256 _amount) public onlyBeneficiary {
+        remaining = remaining.sub(_amount);
+        require(trustToken.transfer(msg.sender, _amount));
+    }
+}
---
+++ test-ts/truefi/distributors/ArbitraryDistributor.test.ts
@@ -0,0 +1,66 @@
+import { Wallet } from 'ethers'
+
+import { beforeEachWithFixture } from '../../utils/beforeEachWithFixture'
+
+import { MockErc20Token } from '../../../build/types/MockErc20Token'
+import { MockErc20TokenFactory } from '../../../build/types/MockErc20TokenFactory'
+import { ArbitraryDistributor } from '../../../build/types/ArbitraryDistributor'
+import { ArbitraryDistributorFactory } from '../../../build/types/ArbitraryDistributorFactory'
+import { parseEther } from 'ethers/lib/utils'
+import { expect } from 'chai'
+
+describe('ArbitraryDistributor', () => {
+    let trustToken: MockErc20Token
+    let distributor: ArbitraryDistributor
+    let owner: Wallet
+    let otherWallet: Wallet
+
+    const totalAmount = parseEther('1000')
+
+    beforeEachWithFixture(async (wallets) => {
+        [owner, otherWallet] = wallets
+        
+        trustToken = await new MockErc20TokenFactory(owner).deploy()
+        distributor = await new ArbitraryDistributorFactory(owner).deploy()
+        
+        await trustToken.mint(distributor.address, totalAmount)
+        await distributor.initialize(owner.address, trustToken.address, totalAmount)
+    })
+
+    describe('initialize', () => {
+      it('properly saves beneficiary address', async () => {
+        expect(await distributor.beneficiary()).to.equal(owner.address)
+      })
+
+      it('properly saves trustToken address', async () => {
+        expect(await distributor.trustToken()).to.equal(trustToken.address)
+      })
+
+      it('properly saves amount to distribute', async () => {
+        expect(await distributor.amount()).to.equal(totalAmount)
+      })
+
+      it('properly saves remaining amount to distribute', async () => {
+        expect(await distributor.remaining()).to.equal(totalAmount)
+      })
+    })
+
+    describe('distribute', () => {
+      const distributedAmount = 100
+
+        it('only predefined beneficiary can call it', async () => {
+          await expect(distributor.connect(otherWallet).distribute(distributedAmount))
+            .to.be.revertedWith('ArbitraryDistributor: Only beneficiary can distribute tokens')
+        })
+
+        it('properly sends tokens', async () => {
+          await expect(() => distributor.distribute(distributedAmount))
+            .to.changeTokenBalances(trustToken, [distributor, owner], [-distributedAmount, distributedAmount])
+        })
+
+        it('properly updates remaining tokens value', async () => {
+          await distributor.distribute(distributedAmount)
+          expect(await distributor.remaining()).to.equal(totalAmount.sub(distributedAmount))
+        })
+    })
+  })
