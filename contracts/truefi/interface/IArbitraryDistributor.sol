33078c8c5e80c95c3f00c6ab585c306673c1f1fa
Bartek Rutkowski <bartek@ethworks.io>
2020-10-30 12:33:39 +0100

    TRU rewards (#339)
    * Add ArbitraryDistributor
    
    * WIP
    
    * Improve claiming
    
    * Work on tests
    
    * Work on tests
    
    * more tests
    
    * fix lint
    
    * Lint fix
    
    * fix tests
    
    Co-authored-by: Ivan Rukhavets <ivanruch@gmail.com>

---
+++ contracts/truefi/interface/IArbitraryDistributor.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+interface IArbitraryDistributor {
+    function amount() external returns (uint256);
+
+    function remaining() external returns (uint256);
+
+    function distribute(uint256 _amount) external;
+}
