6e0828234be242eb91d841e7b5a85aadd568dc64
Anze1m <bartek@ethworks.io>
2020-11-01 19:20:02 +0100

    Fix linting

--- test-ts/truefi/LoanToken.test.ts
+++ test-ts/truefi/LoanToken.test.ts
@@ -429,14 +429,14 @@ describe('LoanToken', () => {
     it('returns proper value at the beginning of the loan', async () => {
       expect(await loanToken.value(lender.address)).to.equal(parseEther('1000'))
     })
-    
+
     it('returns proper value in the middle of the loan', async () => {
       await timeTravel(provider, monthInSeconds * 6)
       expect(await loanToken.value(lender.address)).to.equal(parseEther('1050'))
       await timeTravel(provider, monthInSeconds * 3)
       expect(await loanToken.value(lender.address)).to.equal(parseEther('1075'))
     })
-    
+
     it('returns proper value at the end of the loan', async () => {
       await timeTravel(provider, monthInSeconds * 12)
       expect(await loanToken.value(lender.address)).to.equal(parseEther('1100'))
