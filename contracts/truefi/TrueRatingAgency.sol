ab042d7cd8528ba4cf1ed35f7681e3616383c82a
Hal Hyatt <feewet@gmail.com>
2020-11-02 07:59:33 -0600

    rating agency docs

--- contracts/truefi/TrueRatingAgency.sol
+++ contracts/truefi/TrueRatingAgency.sol
@@ -15,7 +15,23 @@ import {Ownable} from "./upgradeability/UpgradeableOwnable.sol";
  * @title TrueRatingAgency
  * @dev Credit prediction market for LoanTokens
  *
- * The RatingAgency has several states:
+ * TrueFi uses use a prediction market to signal how risky a loan is. 
+ * The Credit Prediction Market estimates the likelihood of a loan defaulting. 
+ * Any TRU holder can vote YES or NO and stake TRU as collateral on their vote. 
+ * If a loan is funded, TRU is locked into the market until expiry.
+ * Locking TRU into the prediction market allows voters to earn and claim 
+ * incentive TRU throughout the course of the loan. After the loan's term, 
+ * if the voter is correct, they earn a TRU reward plus a portion of the 
+ * losing side's vote. A portion of the losing side's TRU is burned.
+ *
+ * Voting Lifecycle: 
+ * - Borrowers can apply for loans at any time by deploying a LoanToken
+ * - LoanTokens are registered with the prediction market contract
+ * - Once registered, TRU holders can vote at any time
+ * - If a loan is funded, TRU is locked for the term of the loan
+ * - At the end of the term, payouts are determined based on the loan outcome
+ *
+ * States:
  * Void:        Rated loan is invalid
  * Pending:     Waiting to be funded
  * Retracted:   Rating has been cancelled
@@ -59,72 +75,135 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
     event Voted(address loanToken, address voter, bool choice, uint256 stake);
     event Withdrawn(address loanToken, address voter, uint256 stake, uint256 received, uint256 burned);
 
+    /**
+     * @dev Only whitelisted borrwers can submit for credit ratings
+     */
     modifier onlyAllowedSubmitters() {
         require(allowedSubmitters[msg.sender], "TrueRatingAgency: Sender is not allowed to submit");
         _;
     }
 
+    /**
+     * @dev Only loan submitter can perform certain actions
+     */
     modifier onlyCreator(address id) {
         require(loans[id].creator == msg.sender, "TrueRatingAgency: Not sender's loan");
         _;
     }
 
+    /**
+     * @dev Cannot submit the same loan muliple times
+     */
     modifier onlyNotExistingLoans(address id) {
         require(status(id) == LoanStatus.Void, "TrueRatingAgency: Loan was already created");
         _;
     }
 
+    /**
+     * @dev Only loans in Pending state
+     */
     modifier onlyPendingLoans(address id) {
         require(status(id) == LoanStatus.Pending, "TrueRatingAgency: Loan is not currently pending");
         _;
     }
 
+    /**
+     * @dev Only loans in Running state
+     */
     modifier onlyNotRunningLoans(address id) {
         require(status(id) != LoanStatus.Running, "TrueRatingAgency: Loan is currently running");
         _;
     }
 
+    /**
+     * @dev Only loans that have been funded
+     */
     modifier onlyFundedLoans(address id) {
         require(status(id) >= LoanStatus.Running, "TrueRatingAgency: Loan was not funded");
         _;
     }
 
-    function initialize(IBurnableERC20 _trustToken, IArbitraryDistributor _distributor) public initializer {
+    /**
+     * @dev Initalize Rating Agenct
+     * Distributor contract decides how much TRU is rewarded to stakers
+     * @param _trustToken TRU contract
+     * @param _distributor Distributor contract
+     */
+    function initialize(
+        IBurnableERC20 _trustToken, 
+        IArbitraryDistributor _distributor
+    ) public initializer {
         Ownable.initialize();
         trustToken = _trustToken;
         distributor = _distributor;
     }
 
+    /**
+     * @dev Set loss factor.
+     * Loss factor decides what percentage of TRU is lost for incorrect votes
+     * @param newLossFactor New loss factor
+     */
     function setLossFactor(uint256 newLossFactor) external onlyOwner {
         lossFactor = newLossFactor;
         emit LossFactorChanged(newLossFactor);
     }
 
+    /**
+     * @dev Set burn factor.
+     * Burn factor decides what percentage of lost TRU is burned
+     */
     function setBurnFactor(uint256 newBurnFactor) external onlyOwner {
         burnFactor = newBurnFactor;
         emit BurnFactorChanged(newBurnFactor);
     }
 
+    /**
+     * @dev Get number of NO votes for a specific account and loan
+     * @param id Loan ID
+     * @param voter Voter account
+     */
     function getNoVote(address id, address voter) public view returns (uint256) {
         return loans[id].votes[voter][false];
     }
 
+    /**
+     * @dev Get number of YES votes for a specific account and loan
+     * @param id Loan ID
+     * @param voter Voter account
+     */
     function getYesVote(address id, address voter) public view returns (uint256) {
         return loans[id].votes[voter][true];
     }
 
+    /**
+     * @dev Get total NO votes for a specific loan
+     * @param id Loan ID
+     */
     function getTotalNoVotes(address id) public view returns (uint256) {
         return loans[id].prediction[false];
     }
 
+    /**
+     * @dev Get total YES votes for a specific loan
+     * @param id Loan ID
+     */
     function getTotalYesVotes(address id) public view returns (uint256) {
         return loans[id].prediction[true];
     }
 
+    /**
+     * @dev Get timestamp at which voting started for a specific loan
+     * @param id Loan ID
+     */
     function getVotingStart(address id) public view returns (uint256) {
         return loans[id].timestamp;
     }
 
+    /**
+     * @dev Get current results for a specific loan
+     * @param id Loan ID
+     * @return (start_time, total_no, total_yes)
+     */
     function getResults(address id)
         external
         override
@@ -138,17 +217,32 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
         return (getVotingStart(id), getTotalNoVotes(id), getTotalYesVotes(id));
     }
 
+    /**
+     * @dev Whitelist borrwers to submit loans for rating
+     * @param who Account to whitelist
+     * @param status Flag to whitelist accounts
+     */
     function allow(address who, bool status) external onlyOwner {
         allowedSubmitters[who] = status;
         emit Allowed(who, status);
     }
 
+    /**
+     * @dev Submit a loan for rating
+     * Cannot submit the same loan twice
+     * @param id Loan ID
+     */
     function submit(address id) external override onlyAllowedSubmitters onlyNotExistingLoans(id) {
         require(ILoanToken(id).isLoanToken(), "TrueRatingAgency: Only LoanTokens are supported");
         loans[id] = Loan({creator: msg.sender, timestamp: block.timestamp, reward: 0});
         emit LoanSubmitted(id);
     }
 
+    /**
+     * @dev Remove Loan from rating agency
+     * Can only be retracted by loan creator
+     * @param id Loan ID
+     */
     function retract(address id) external override onlyPendingLoans(id) onlyCreator(id) {
         loans[id].creator = address(0);
         loans[id].prediction[true] = 0;
@@ -157,6 +251,12 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
         emit LoanRetracted(id);
     }
 
+    /**
+     * @dev Vote on a loan by staking TRU
+     * @param id Loan ID
+     * @param stake Amount of TRU to stake
+     * @param choice Voter choice. false = NO, true = YES
+     */
     function vote(
         address id,
         uint256 stake,
@@ -171,14 +271,30 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
         emit Voted(id, msg.sender, choice, stake);
     }
 
+    /**
+     * @dev Vote YES on a loan by staking TRU
+     * @param id Loan ID
+     * @param stake Amount of TRU to stake
+     */
     function yes(address id, uint256 stake) external override onlyPendingLoans(id) {
         vote(id, stake, true);
     }
 
+    /**
+     * @dev Vote NO on a loan by staking TRU
+     * @param id Loan ID
+     * @param stake Amount of TRU to stake
+     */
     function no(address id, uint256 stake) external override onlyPendingLoans(id) {
         vote(id, stake, false);
     }
 
+    /**
+     * @dev Withdraw stake on a loan and remove votes.
+     * Unstaking only allowed for loans that are not Running
+     * @param id Loan ID
+     * @param stake Amount of TRU to unstake
+     */
     function withdraw(address id, uint256 stake) external override onlyNotRunningLoans(id) {
         bool choice = loans[id].votes[msg.sender][true] > 0;
         LoanStatus loanStatus = status(id);
@@ -210,14 +326,29 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
         emit Withdrawn(id, msg.sender, stake, amountToTransfer, burned);
     }
 
+    /**
+     * @dev Internal function to calculate TRU loss for incorrect voters
+     * @param id Loan ID
+     * @param incorrectChoice Vote which was incorrect
+     * @return TRU amount remaining for incorrect voters
+     */
     function bounty(address id, bool incorrectChoice) internal view returns (uint256) {
         return loans[id].prediction[incorrectChoice].mul(lossFactor).mul(uint256(10000).sub(burnFactor)).div(10000**2);
     }
 
+    /**
+     * @dev Internal view to convert values to 8 decimals precision
+     * @param input Value to convert to TRU precision
+     * @return TRU amonut
+     */
     function toTrustToken(uint256 input) internal pure returns (uint256 output) {
         output = input.div(TOKEN_PRECISION_DIFFERENCE);
     }
 
+    /**
+     * @dev Update total TRU reward for a Loan
+     * @id Loan ID
+     */
     modifier calculateTotalReward(address id) {
         if (loans[id].reward == 0) {
             uint256 interest = ILoanToken(id).profit();
@@ -230,6 +361,20 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
         _;
     }
 
+    /**
+     * @dev Claim TRU rewards for voters
+     * Only can claim TRU rewards for funded loans
+     * Voters can claim a portion of their total rewards over time
+     * chi = (TRU remaining in distributor) / (Total TRU allocated for distribution)
+     * interest = (loan APY * term * principal)
+     * R = Total Reward = (interest * chi)
+     * R is distributed to voters based on their proportion of votes/total_votes
+     * Voters can claim their proportion based on time.
+     * Claimable reward = R x (current time / total time) 
+     *      * (account TRU staked / total TRU staked) - (amount claimed)
+     * @param id Loan ID
+     * @param voter Voter account
+     */
     function claim(address id, address voter) public override onlyFundedLoans(id) calculateTotalReward(id) {
         uint256 totalTime = ILoanToken(id).duration();
         uint256 passedTime = block.timestamp.sub(ILoanToken(id).start());
@@ -249,6 +394,11 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
         }
     }
 
+    /**
+     * @dev Check if a prediction was correct for a specific loan and vote
+     * @param id Loan ID
+     * @param choice Outcome prediction
+     */
     function wasPredictionCorrect(address id, bool choice) internal view returns (bool) {
         if (status(id) == LoanStatus.Settled && choice) {
             return true;
@@ -259,6 +409,11 @@ contract TrueRatingAgency is ITrueRatingAgency, Ownable {
         return false;
     }
 
+    /**
+     * @dev Get status for a specific loan
+     * @param id Loan ID
+     * @return Status of loan
+     */
     function status(address id) public view returns (LoanStatus) {
         Loan storage loan = loans[id];
         if (loan.creator == address(0) && loan.timestamp == 0) {
