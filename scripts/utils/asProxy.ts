c4f1b7498f2b3f7a6d7f51c039ab1fabedf26475
Anze1m <bartek@ethworks.io>
2020-11-03 12:10:15 +0100

    Fix imports

--- scripts/utils/asProxy.ts
+++ scripts/utils/asProxy.ts
@@ -1,6 +1,6 @@
 import { Contract } from 'ethers'
-import { OwnedUpgradeabilityProxyFactory } from '../../build/OwnedUpgradeabilityProxyFactory'
-import { OwnedUpgradeabilityProxy } from '../../build/OwnedUpgradeabilityProxy'
+import { OwnedUpgradeabilityProxyFactory } from '../../build/types/OwnedUpgradeabilityProxyFactory'
+import { OwnedUpgradeabilityProxy } from '../../build/types/OwnedUpgradeabilityProxy'
 
 export function asProxy (contract: Contract): OwnedUpgradeabilityProxy {
   return OwnedUpgradeabilityProxyFactory.connect(contract.address, contract.signer)
