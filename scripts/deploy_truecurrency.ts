1dc225b145ab0e42b6488cfe8e5eea70e33f1f82
Anze1m <bartek@ethworks.io>
2020-11-03 17:33:22 +0100

    Fix linting

--- scripts/deploy_truecurrency.ts
+++ scripts/deploy_truecurrency.ts
@@ -13,16 +13,16 @@
 //   const provider = new providers.InfuraProvider(process.argv[3], '81447a33c1cd4eb09efb1e8c388fb28e')
 //   const wallet = new ethers.Wallet(process.argv[2], provider)
 
-  // const taud = await (await new TrueAudFactory(wallet).deploy()).deployed()
-  // const tcad = await (await new TrueCadFactory(wallet).deploy()).deployed()
-  // const tgbp = await (await new TrueGbpFactory(wallet).deploy()).deployed()
-  // const thkd = await (await new TrueHkdFactory(wallet).deploy()).deployed()
-  // const tusd = await (await new TrueUsdFactory(wallet).deploy(txnArgs)).deployed()
+// const taud = await (await new TrueAudFactory(wallet).deploy()).deployed()
+// const tcad = await (await new TrueCadFactory(wallet).deploy()).deployed()
+// const tgbp = await (await new TrueGbpFactory(wallet).deploy()).deployed()
+// const thkd = await (await new TrueHkdFactory(wallet).deploy()).deployed()
+// const tusd = await (await new TrueUsdFactory(wallet).deploy(txnArgs)).deployed()
 
-  // console.log('tusd address: ', tusd.address)
-  // console.log(tcad.address)
-  // console.log(tgbp.address)
-  // console.log(thkd.address)
+// console.log('tusd address: ', tusd.address)
+// console.log(tcad.address)
+// console.log(tgbp.address)
+// console.log(thkd.address)
 // }
 
 // deployTrueCurrency().catch(console.error)
