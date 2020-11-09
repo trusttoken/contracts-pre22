76878c7dae525ded4e061adca22813153ea6f08c
Anze1m <bartek@ethworks.io>
2020-11-09 16:04:43 +0100

    WIP

--- test-ts/true-currencies/TokenController.test.ts
+++ test-ts/true-currencies/TokenController.test.ts
@@ -1,19 +1,19 @@
 import { expect } from 'chai'
 import { Wallet } from 'ethers'
-import { loadFixture } from 'ethereum-waffle'
 import { formatBytes32String } from '@ethersproject/strings'
 import { parseEther } from '@ethersproject/units'
 
 import { beforeEachWithFixture } from 'utils'
-import { trueCurrency } from 'fixtures/trueCurrency'
 
-import { TrueCurrency } from 'contracts/types/TrueCurrency'
 import {
   TokenControllerMockFactory,
   TokenControllerMock,
-  Registry,
-  RegistryFactory,
+  RegistryMock,
+  RegistryMockFactory,
   OwnedUpgradeabilityProxy,
+  OwnedUpgradeabilityProxyFactory,
+  MockTrueCurrencyFactory,
+  MockTrueCurrency,
 } from 'contracts'
 
 describe.only('TokenController', () => {
@@ -27,41 +27,49 @@ describe.only('TokenController', () => {
   let ratifier2: Wallet
   let ratifier3: Wallet
 
-  let token: TrueCurrency
-  let proxy: OwnedUpgradeabilityProxy
+  let token: MockTrueCurrency
+  let tokenImplementation: MockTrueCurrency
+  let tokenProxy: OwnedUpgradeabilityProxy
   let controller: TokenControllerMock
-  let registry: Registry
+  let registry: RegistryMock
 
   const notes = formatBytes32String('notes')
   const CAN_BURN = formatBytes32String('canBurn')
 
-  beforeEachWithFixture(async () => {
-    ({ wallets: [owner, registryAdmin, otherWallet, thirdWallet, mintKey, pauseKey, ratifier1, ratifier2, ratifier3], token, proxy } = await loadFixture(trueCurrency))
+  beforeEachWithFixture(async (wallets) => {
+    [owner, registryAdmin, otherWallet, thirdWallet, mintKey, pauseKey, ratifier1, ratifier2, ratifier3] = wallets
     
+    registry = await new RegistryMockFactory(owner).deploy()
     controller = await new TokenControllerMockFactory(owner).deploy()
-    registry = await new RegistryFactory(owner).deploy()
+    
+    tokenProxy = await new OwnedUpgradeabilityProxyFactory(owner).deploy()
+    tokenImplementation = await new MockTrueCurrencyFactory(owner).deploy()
+    await tokenProxy.upgradeTo(tokenImplementation.address)
+
+    token = new MockTrueCurrencyFactory(owner).attach(tokenProxy.address)
+    await token.initialize()
 
     await token.transferOwnership(controller.address)
     await controller.initialize()
-    await controller.setToken(token.address)
     await controller.issueClaimOwnership(token.address)
     await controller.setRegistryAdmin(registryAdmin.address)
     await controller.setRegistry(registry.address)
+    await controller.setToken(token.address)
     await controller.transferMintKey(mintKey.address)
-    await proxy.transferProxyOwnership(controller.address)
+    await tokenProxy.transferProxyOwnership(controller.address)
     await controller.claimTrueCurrencyProxyOwnership()
-    // await registry.setAttribute(thirdWallet.address, CAN_BURN, 1, notes)
-    // await registry.setAttribute(ratifier1.address, formatBytes32String('isTUSDMintRatifier'), 1, notes)
-    // await registry.setAttribute(ratifier2.address, formatBytes32String('isTUSDMintRatifier'), 1, notes)
-    // await registry.setAttribute(ratifier3.address, formatBytes32String('isTUSDMintRatifier'), 1, notes)
-    // await registry.setAttribute(pauseKey.address, formatBytes32String('isTUSDMintPausers'), 1, notes)
-    // await controller.setMintThresholds(parseEther('200'), parseEther('1000'), parseEther('1001'))
-    // await controller.setMintLimits(parseEther('200'), parseEther('300'), parseEther('3000'))
-    // await controller.refillMultiSigMintPool()
-    // await controller.refillRatifiedMintPool()
-    // await controller.refillInstantMintPool()
-    // await controller.instantMint(thirdWallet.address, parseEther('100'))
-    // await controller.setMintLimits(0, 0, 0)
+    await registry.setAttribute(thirdWallet.address, CAN_BURN, 1, notes, {gasLimit: 5_000_000})
+    await registry.setAttribute(ratifier1.address, formatBytes32String('isTUSDMintRatifier'), 1, notes, {gasLimit: 5_000_000})
+    await registry.setAttribute(ratifier2.address, formatBytes32String('isTUSDMintRatifier'), 1, notes, {gasLimit: 5_000_000})
+    await registry.setAttribute(ratifier3.address, formatBytes32String('isTUSDMintRatifier'), 1, notes, {gasLimit: 5_000_000})
+    await registry.setAttribute(pauseKey.address, formatBytes32String('isTUSDMintPausers'), 1, notes)
+    await controller.setMintThresholds(parseEther('200'), parseEther('1000'), parseEther('1001'))
+    await controller.setMintLimits(parseEther('200'), parseEther('300'), parseEther('3000'))
+    await controller.refillMultiSigMintPool()
+    await controller.refillRatifiedMintPool()
+    await controller.refillInstantMintPool()
+    await controller.instantMint(thirdWallet.address, parseEther('100'))
+    await controller.setMintLimits(0, 0, 0)
   })
 
   describe('setCanBurn', function () {
