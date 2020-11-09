33746283146d93cfbe58043663bcc05cd75994b1
Anze1m <bartek@ethworks.io>
2020-11-09 18:47:26 +0100

    Remove unused files

--- .solcover.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-    compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    skipFiles: ['Migrations.sol', 'mocks', 'deployed'],
-    copyPackages: ['openzeppelin-solidity'],
-}
