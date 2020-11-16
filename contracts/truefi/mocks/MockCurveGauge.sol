cd99b54341b67540a45108dbaf8d13e13f1fe3e7
Anze1m <bartek@ethworks.io>
2020-11-16 14:13:13 +0100

    Minor fix

--- contracts/truefi/mocks/MockCurveGauge.sol
+++ contracts/truefi/mocks/MockCurveGauge.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.10;
 
-import {ICurveGauge} from "../interface/ICurve.sol";
+import {ICurveGauge, ICurveMinter} from "../interface/ICurve.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MockCurveGauge is ICurveGauge {
