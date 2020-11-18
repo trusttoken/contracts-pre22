c2ba010100eb77c8791bc4949bf0117be87b6cb5
Hal Hyatt <feewet@gmail.com>
2020-11-17 22:44:53 -0600

    lint

--- scripts/deploy_trusttoken.ts
+++ scripts/deploy_trusttoken.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /**
  * ts-node scripts/deploy_trusttoken.ts "{private_key}" "{network}"
  */
@@ -6,9 +7,10 @@ import { TrustTokenFactory } from '../build/types'
 import { MockTrustTokenFactory } from '../build/types'
 
 async function deploy() {
-    // await deployTrustToken().catch(console.error)
-    await deployMockTrustToken().catch(console.error)
+  // await deployTrustToken().catch(console.error)
+  await deployMockTrustToken().catch(console.error)
 }
+
 async function deployTrustToken () {
   const txnArgs = { gasLimit: 4_500_000, gasPrice: 20_000_000_000 }
   const provider = new providers.InfuraProvider(process.argv[3], '81447a33c1cd4eb09efb1e8c388fb28e')
