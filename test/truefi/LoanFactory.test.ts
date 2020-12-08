a7936a2e2b3e03d819ef9af8464a883ffe3cee57
Mikołaj Jakubowski <miksuxp@gmail.com>
2020-12-08 11:21:19 +0100

    Prevent LoanFactory from creating 0 loans (#388)
    * Prevent LoanFactory from creating 0 loans
    
    * Start error messages from capital letter
    
    Co-authored-by: Bartek Rutkowski <bartek@ethworks.io>

--- test/truefi/LoanFactory.test.ts
+++ test/truefi/LoanFactory.test.ts
@@ -36,4 +36,9 @@ describe('LoanFactory', () => {
   it('marks deployed contract as loan token', async () => {
     expect(await factory.isLoanToken(contractAddress)).to.be.true
   })
+
+  it('prevents 0 loans', async () => {
+    await expect(factory.createLoanToken(borrower.address, 0, 100, 200))
+      .to.be.revertedWith('LoanFactory: Loans of amount 0, will not be approved')
+  })
 })
