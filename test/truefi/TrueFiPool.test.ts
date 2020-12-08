1caf44bff562e6035942701787445615a4488ea2
Ivan Rukhavets <ivanruch@gmail.com>
2020-12-08 15:21:33 +0100

    Fix lint

--- test/truefi/TrueFiPool.test.ts
+++ test/truefi/TrueFiPool.test.ts
@@ -382,6 +382,6 @@ describe('TrueFiPool', () => {
 
     it('emits event', async () => {
       await expect(pool.liquidExit(amount.div(2))).to.emit(pool, 'Exited').withArgs(owner.address, amount.div(2))
-    });
+    })
   })
 })
