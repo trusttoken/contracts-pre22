93c54d89a570cd13f742579751535f1e2d78cff6
Anze1m <bartek@ethworks.io>
2020-12-08 10:51:50 +0100

    Remove blank spaces

--- contracts/truefi/interface/IArbitraryDistributor.sol
+++ contracts/truefi/interface/IArbitraryDistributor.sol
@@ -5,7 +5,7 @@ interface IArbitraryDistributor {
     function amount() external returns (uint256);
 
     function remaining() external returns (uint256);
-    
+
     function beneficiary() external returns (address);
 
     function distribute(uint256 _amount) external;
