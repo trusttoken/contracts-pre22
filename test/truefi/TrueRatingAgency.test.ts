762c37f3e4f74546d83265b34c3c60057eb5e687
Anze1m <bartek@ethworks.io>
2020-12-08 10:52:06 +0100

    Fix linting in tests

--- test/truefi/TrueRatingAgency.test.ts
+++ test/truefi/TrueRatingAgency.test.ts
@@ -23,7 +23,7 @@ import {
   ArbitraryDistributorFactory,
   ArbitraryDistributor,
   ILoanFactoryJson,
-  ArbitraryDistributorJson
+  ArbitraryDistributorJson,
 } from 'contracts'
 
 describe('TrueRatingAgency', () => {
