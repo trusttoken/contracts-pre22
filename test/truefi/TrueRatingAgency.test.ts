1e80d580b0241010f5087f50ed7740a1b45dc5d3
Mikołaj Jakubowski <mikolaj.jakubowski@protonmail.com>
2020-12-10 16:00:59 +0100

    Update test/truefi/TrueRatingAgency.test.ts
    Co-authored-by: Bartek Rutkowski <bartek@ethworks.io>

--- test/truefi/TrueRatingAgency.test.ts
+++ test/truefi/TrueRatingAgency.test.ts
@@ -731,7 +731,7 @@ describe('TrueRatingAgency', () => {
 
       expect(event.args[0]).eq(loanToken.address)
       expect(event.args[1]).eq(owner.address)
-      expectCloseTo(BigNumber.from(event.args[2]), BigNumber.from(25000000000000))
+      expectCloseTo(BigNumber.from(event.args[2]), parseTT(250000))
     })
 
     describe('Running', () => {
