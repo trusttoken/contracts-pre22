893a07ce32792d3d01de11e3d1f379c2b5d95660
Ivan Rukhavets <ivanruch@gmail.com>
2020-12-14 11:32:00 +0100

    add expectBalanceChangeCloseTo matcher (#404)

--- test/utils/expectCloseTo.ts
+++ test/utils/expectCloseTo.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { BigNumber } from 'ethers'
+import { BigNumber, BigNumberish, Contract } from 'ethers'
 
 export const expectCloseTo = (a: BigNumber, b: BigNumber) => {
   if (b.eq(a)) {
@@ -13,4 +13,14 @@ export const expectCloseTo = (a: BigNumber, b: BigNumber) => {
   } catch (e) {
     throw new Error(`Expected ${a.toString()} to be close to ${b.toString()}. But it wasn't.`)
   }
+}
+
+export const expectBalanceChangeCloseTo = async (callback: () => Promise<unknown>, token: Contract, account: {address: string} | string, expectedChange: BigNumberish, epsilon = '10000000') => {
+  const address = typeof account === 'string' ? account : account.address
+  const balanceBefore = await token.balanceOf(address)
+  await callback()
+  const balanceAfter = await token.balanceOf(address)
+  const diff = balanceAfter.sub(balanceBefore).sub(expectedChange).abs()
+  expect(diff, `Expected balance to change by approx. ${expectedChange.toString()}, but it changed by ${balanceAfter.sub(balanceBefore).toString()}`)
+    .to.be.lte(epsilon)
 }
