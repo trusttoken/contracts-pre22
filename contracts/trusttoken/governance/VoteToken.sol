b26cf64dba285a3d2fac4ce06ee4c973a77c46ad
yubo-ruan <yubo@8dcapital.com>
2020-12-14 17:28:11 -0800

    fix bugs

--- contracts/trusttoken/governance/VoteToken.sol
+++ contracts/trusttoken/governance/VoteToken.sol
@@ -143,15 +143,6 @@ abstract contract VoteToken is TimeLockedToken {
         assembly { chainId := chainid() }
         return chainId;
     }
-    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
-        assembly {
-            result := mload(add(source, 32))
-        }
-    }
 }
 
 
