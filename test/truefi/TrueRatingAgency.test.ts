d11573b4acce02d3fef419a93522fe084184650a
hal <61718761+hal909@users.noreply.github.com>
2021-02-06 20:18:02 -0800

    Revert "Add TRU to LoanToken for easier future modifications (#464)" (#472)
    This reverts commit ae8e149cfc1eb559252352327d7a9a90d1853e65.
    
    Co-authored-by: Hal Hyatt <feewet@gmail.com>

--- test/truefi/TrueRatingAgency.test.ts
+++ test/truefi/TrueRatingAgency.test.ts
@@ -65,7 +65,6 @@ describe('TrueRatingAgency', () => {
 
     loanToken = await new LoanTokenFactory(owner).deploy(
       tusd.address,
-      trustToken.address,
       owner.address,
       owner.address,
       owner.address,
@@ -730,7 +729,6 @@ describe('TrueRatingAgency', () => {
     beforeEach(async () => {
       loanToken = await new LoanTokenFactory(owner).deploy(
         tusd.address,
-        trustToken.address,
         owner.address,
         owner.address,
         owner.address,
