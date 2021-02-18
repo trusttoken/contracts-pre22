df0392ed80dcf1801f6ea35b37eeda2abc36ad37
hal <61718761+hal909@users.noreply.github.com>
2021-02-18 15:36:37 -0700

    Update chainlink documentation (#502)
    * Add Chainlink oracle
    
    * Add TRU to pool value
    
    * fix tests
    
    * add comments
    
    * add await to fix nonce issue
    
    Co-authored-by: Ivan Rukhavets <ivanruch@gmail.com>
    Co-authored-by: Hal Hyatt <feewet@gmail.com>

--- contracts/truefi/TruPriceChainlinkOracle.sol
+++ contracts/truefi/TruPriceChainlinkOracle.sol
@@ -3,7 +3,6 @@ pragma solidity 0.6.10;
 
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {ITruPriceOracle} from "./interface/ITruPriceOracle.sol";
-import {IUniswapPair} from "./interface/IUniswapPair.sol";
 
 interface IChainLink {
     function latestAnswer() external view returns (int256);
@@ -14,12 +13,18 @@ contract TruPriceChainLinkOracle is ITruPriceOracle {
 
     IChainLink public chainlinkOracle;
 
+    /**
+     * @param _chainlinkOracle ChainLink Oracle address
+     */
     constructor(IChainLink _chainlinkOracle) public {
         chainlinkOracle = _chainlinkOracle;
     }
 
     /**
      * @dev converts from USD with 18 decimals to TRU with 8 decimals
+     * Divide by 100 since Chainlink returns 10 decimals and TRU is 8 decimals
+     * @param amount Amount in USD
+     * @return TRU value of USD input
      */
     function usdToTru(uint256 amount) external override view returns (uint256) {
         return amount.div(safeUint(chainlinkOracle.latestAnswer())).div(100);
@@ -27,11 +32,18 @@ contract TruPriceChainLinkOracle is ITruPriceOracle {
 
     /**
      * @dev converts from TRU with 8 decimals to USD with 18 decimals
+     * Multiply by 100 since Chainlink returns 10 decimals and TRU is 8 decimals
+     * @param amount Amount in TRU
+     * @return USD value of TRU input
      */
     function truToUsd(uint256 amount) external override view returns (uint256) {
         return amount.mul(safeUint(chainlinkOracle.latestAnswer())).mul(100);
     }
 
+    /**
+     * @dev convert int256 to uint256
+     * @param value to convert to uint
+     */
     function safeUint(int256 value) internal pure returns (uint256) {
         require(value >= 0, "TruPriceChainLinkOracle: uint underflow");
         return uint256(value);
