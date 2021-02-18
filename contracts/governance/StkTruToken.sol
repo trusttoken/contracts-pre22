5f80349ab639d39d609fe73465586ed7b6a0af6d
Ivan Rukhavets <ivanruch@gmail.com>
2021-02-18 22:28:32 +0100

    Reset cooldown on stake (#494)
    * Reset cooldown on stake
    
    * rm test
    
    * add event

--- contracts/governance/StkTruToken.sol
+++ contracts/governance/StkTruToken.sol
@@ -165,8 +165,10 @@ contract StkTruToken is VoteToken, ClaimableContract, ReentrancyGuard {
     function stake(uint256 amount) external distribute update(msg.sender) {
         require(amount > 0, "StkTruToken: Cannot stake 0");
 
-        if (cooldowns[msg.sender] != 0 && cooldowns[msg.sender].add(cooldownTime) > block.timestamp) {
+        if (cooldowns[msg.sender] != 0 && cooldowns[msg.sender].add(cooldownTime).add(unstakePeriodDuration) > block.timestamp) {
             cooldowns[msg.sender] = block.timestamp;
+
+            emit Cooldown(msg.sender, block.timestamp.add(cooldownTime));
         }
 
         uint256 amountToMint = stakeSupply == 0 ? amount : amount.mul(totalSupply).div(stakeSupply);
