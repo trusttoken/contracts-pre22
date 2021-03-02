e468d54627eaf1be8106d649df3e10a4cc5dada1
Anze1m <bartek@ethworks.io>
2021-03-02 12:20:12 +0100

    Add MasterChefInterface

---
+++ contracts/truefi/interface/IMasterChef.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+interface IMasterChef {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+}
