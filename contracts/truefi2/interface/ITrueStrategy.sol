c1ad02d88db5d869c5073ed322764dd91be845c9
Ivan Rukhavets <ivanruch@gmail.com>
2021-03-17 14:51:33 +0300

    Rename strategy methods

--- contracts/truefi2/interface/ITrueStrategy.sol
+++ contracts/truefi2/interface/ITrueStrategy.sol
@@ -3,10 +3,13 @@ pragma solidity 0.6.10;
 
 interface ITrueStrategy {
     /// @dev put `amount` of token kept in Pool into strategy
-    function putIn(uint256 amount) external;
+    function deposit(uint256 amount) external;
 
     /// @dev pull at least `minAmount` of tokens from strategy
-    function pullOut(uint256 minAmount) external;
+    function withdraw(uint256 minAmount) external;
+
+    /// @dev withdraw everything from strategy
+    function withdrawAll() external;
 
     /// @dev value converted to Pool's tokens
     function value() external view returns (uint256);
