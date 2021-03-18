3f4a14a90c076f4c17897d6ace388b7a2f97b86d
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-03-18 16:00:12 +0100

    Add multiple pools tests

--- test/truefi2/PoolFactory.test.ts
+++ test/truefi2/PoolFactory.test.ts
@@ -15,7 +15,8 @@ describe('PoolFactory', () => {
   let poolImplementation: TrueFiPool2
   let implementationReference: ImplementationReference
   let factory: PoolFactory
-  let token: MockErc20Token
+  let token1: MockErc20Token
+  let token2: MockErc20Token
 
   beforeEachWithFixture(async (wallets) => {
     [owner] = wallets
@@ -23,7 +24,8 @@ describe('PoolFactory', () => {
     implementationReference = await new ImplementationReferenceFactory(owner).deploy(poolImplementation.address)
 
     factory = await new PoolFactoryFactory(owner).deploy()
-    token = await new MockErc20TokenFactory(owner).deploy()
+    token1 = await new MockErc20TokenFactory(owner).deploy()
+    token2 = await new MockErc20TokenFactory(owner).deploy()
 
     await factory.initialize(implementationReference.address)
   })
@@ -36,13 +38,13 @@ describe('PoolFactory', () => {
   })
 
   describe('Creating new pool', () => {
-    let creationEventArgs: {}
+    let creationEventArgs: any
     let proxy: OwnedProxyWithReference
 
     beforeEach(async () => {
-      const tx = await factory.createPool(token.address)
+      const tx = await factory.createPool(token1.address)
       creationEventArgs = (await tx.wait()).events[4].args
-      proxy = OwnedProxyWithReferenceFactory.connect(await factory.correspondingPool(token.address), owner)
+      proxy = OwnedProxyWithReferenceFactory.connect(await factory.correspondingPool(token1.address), owner)
       await proxy.claimProxyOwnership()
     })
 
@@ -51,26 +53,76 @@ describe('PoolFactory', () => {
     })
 
     it('adds pool to token -> pool mapping', async () => {
-      expect(await factory.correspondingPool(token.address)).to.eq(proxy.address)
+      expect(await factory.correspondingPool(token1.address)).to.eq(proxy.address)
     })
 
     it('adds pool to isPool mapping', async () => {
       expect(await factory.isPool(proxy.address)).to.eq(true)
     })
 
+    it('proxy gets correct implementation', async () => {
+      expect(await proxy.implementation()).to.eq(poolImplementation.address)
+    })
+
     it('cannot create pool for token that already has a pool', async () => {
-      await expect(factory.createPool(token.address))
+      await expect(factory.createPool(token1.address))
         .to.be.revertedWith('PoolFactory: This token already has a corresponding pool')
     })
 
     it('emits event', async () => {
-      const proxyAddress = await factory.correspondingPool(token.address)
-      expect(creationEventArgs['token']).to.eq(token.address)
+      const proxyAddress = await factory.correspondingPool(token1.address)
+      expect(creationEventArgs['token']).to.eq(token1.address)
       expect(creationEventArgs['pool']).to.eq(proxyAddress)
     })
   })
 
   describe('Creating multiple pools', () => {
-    
+    let proxy1: OwnedProxyWithReference
+    let proxy2: OwnedProxyWithReference
+
+    beforeEach(async () => {
+      await factory.createPool(token1.address)
+      await factory.createPool(token2.address)
+      proxy1 = OwnedProxyWithReferenceFactory.connect(await factory.correspondingPool(token1.address), owner)
+      proxy2 = OwnedProxyWithReferenceFactory.connect(await factory.correspondingPool(token2.address), owner)
+      await proxy1.claimProxyOwnership()
+      await proxy2.claimProxyOwnership()
+    })
+
+    it('adds 2 pools for 2 tokens', async () => {
+      expect(await proxy1.proxyOwner()).to.eq(owner.address)
+      expect(await proxy2.proxyOwner()).to.eq(owner.address)
+
+      expect(await factory.isPool(proxy1.address)).to.eq(true)
+      expect(await factory.isPool(proxy2.address)).to.eq(true)
+
+      expect(await factory.correspondingPool(token1.address)).to.eq(proxy1.address)
+      expect(await factory.correspondingPool(token2.address)).to.eq(proxy2.address)
+
+      expect(await proxy1.implementation()).to.eq(poolImplementation.address)
+      expect(await proxy2.implementation()).to.eq(poolImplementation.address)
+    })
+
+    it('changing reference, changes implementation for both', async () => {
+      const newPoolImplementation = await new TrueFiPool2Factory(owner).deploy()
+      await implementationReference.setImplementation(newPoolImplementation.address)
+
+      expect(await proxy1.implementation()).to.eq(newPoolImplementation.address)
+      expect(await proxy2.implementation()).to.eq(newPoolImplementation.address)
+    })
+
+    it('one reference changed, second remains, then change initial implementation', async () => {
+      const newPoolImplementation1 = await new TrueFiPool2Factory(owner).deploy()
+      const newReference = await new ImplementationReferenceFactory(owner).deploy(newPoolImplementation1.address)
+      const newPoolImplementation2 = await new TrueFiPool2Factory(owner).deploy()
+
+      await proxy1.changeImplementationReference(newReference.address)
+      expect(await proxy1.implementation()).to.eq(newPoolImplementation1.address)
+      expect(await proxy2.implementation()).to.eq(poolImplementation.address)
+
+      await implementationReference.setImplementation(newPoolImplementation2.address)
+      expect(await proxy1.implementation()).to.eq(newPoolImplementation1.address)
+      expect(await proxy2.implementation()).to.eq(newPoolImplementation2.address)
+    })
   })
 })
