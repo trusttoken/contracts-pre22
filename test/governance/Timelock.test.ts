ff32ed01f4ccadeb4edc0cc6c0b8e0fe4d3a2eae
Ivan Rukhavets <ivanruch@gmail.com>
2021-03-23 17:51:00 +0300

    Start timelock tests

--- test/governance/Timelock.test.ts
+++ test/governance/Timelock.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { beforeEachWithFixture, timeTravel } from 'utils'
-import { Wallet } from 'ethers'
+import { utils, Wallet } from 'ethers'
 import { deployContract } from 'scripts/utils/deployContract'
 import { OwnedUpgradeabilityProxyFactory, Timelock, TimelockFactory } from 'contracts'
 import { AddressZero } from '@ethersproject/constants'
@@ -14,6 +14,16 @@ describe('Timelock', () => {
     timelock = await deployContract(admin, TimelockFactory)
     await timelock.initialize(admin.address, 200000)
   })
+
+  const queueAndExecute = async <Sig extends keyof Timelock['functions']>(signature: Sig, args: Parameters<Timelock['functions'][Sig]>) => {
+    const abi = [`function ${signature}`]
+    const iface = new utils.Interface(abi)
+    const data = `0x${iface.encodeFunctionData(signature, args).slice(10)}`
+    const block = await admin.provider.getBlock('latest')
+    await timelock.queueTransaction(timelock.address, 0, signature, data, block.timestamp + 200100)
+    await timeTravel(admin.provider as any, 200200)
+    await timelock.executeTransaction(timelock.address, 0, signature, data, block.timestamp + 200100)
+  }
 
   describe('emergency pause', () => {
     it('upgrades proxy implementation to address(0)', async () => {
@@ -32,6 +42,29 @@ describe('Timelock', () => {
     it('is onlyAdmin', async () => {
       const proxy = await deployContract(admin, OwnedUpgradeabilityProxyFactory)
       await expect(timelock.connect(notAdmin).emergencyPause(proxy.address)).to.be.revertedWith('Timelock::emergencyPause: Call must come from admin.')
+    })
+  })
+
+  describe('setDelay', async () => {
+    it('can only be called by itself', async () => {
+      expect(await timelock.setDelay(10)).to.be.revertedWith('Timelock::setDelay: Call must come from Timelock.')
+    })
+
+    it('changes delay', async () => {
+      await queueAndExecute('setDelay(uint256)', [10 * 60 * 60 * 24])
+      expect(await timelock.delay()).to.equal(10 * 60 * 60 * 24)
+    })
+
+    it('reverts if delay too small', async () => {
+      const tooSmallDelay = 60 * 60 * 24 // 1 day
+      await expect(queueAndExecute('setDelay(uint256)', [tooSmallDelay]))
+        .to.be.revertedWith('Timelock::executeTransaction: Transaction execution reverted.')
+    })
+
+    it('reverts if delay too big', async () => {
+      const tooBigDelay = 31 * 60 * 60 * 24 // 31 day
+      await expect(queueAndExecute('setDelay(uint256)', [tooBigDelay]))
+        .to.be.revertedWith('Timelock::executeTransaction: Transaction execution reverted.')
     })
   })
 })
