6429527f751fb34fe0e0b41322ae4e04fcb611fe
Ivan Rukhavets <ivanruch@gmail.com>
2021-03-26 17:36:54 +0300

    fix lint

--- test/truefi/TrueFiPool.test.ts
+++ test/truefi/TrueFiPool.test.ts
@@ -1,5 +1,5 @@
 import { expect, use } from 'chai'
-import { constants, Wallet, BigNumber, utils } from 'ethers'
+import { constants, Wallet, BigNumber } from 'ethers'
 import { deployMockContract, MockContract, MockProvider, solidity } from 'ethereum-waffle'
 import fetch from 'node-fetch'
 import { toTrustToken } from 'scripts/utils'
