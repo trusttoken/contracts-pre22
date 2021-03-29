a66f593f689c69fa1474d52660b1d9accdcce3b2
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-03-29 08:13:53 -0500

    Close repaid in full loans before reclaiming them

--- contracts/truefi/bots/TrueLenderReclaimer.sol
+++ contracts/truefi/bots/TrueLenderReclaimer.sol
@@ -22,6 +22,12 @@ import {TrueLender} from "../TrueLender.sol";
     // ======= STORAGE DECLARATION END ============
 
     /**
+     * @dev Emitted when a LoanToken contract is settled
+     * @param loanToken LoanToken which has been closed
+     */
+    event Closed(address indexed loanToken);
+
+    /**
      * @dev Emitted when funds are reclaimed from a LoanToken contract
      * @param loanToken LoanToken from which funds were reclaimed
      */
@@ -33,16 +39,31 @@ import {TrueLender} from "../TrueLender.sol";
 
     /**
      * @dev Reclaim funds from all settled loans in lender
-     * Only uses publicly accessible functions of TrueLender, so this is safe to run by anyone.
+     * Only uses public functions, so this is safe to run by anyone.
      */
     function reclaimAll() public {
         ILoanToken[] memory loans = _lender.loans();
+        // TODO avoid iterating through an unbounded array
         for (uint256 index = 0; index < loans.length; index++) {
             ILoanToken loanToken = loans[index];
+            require(loanToken.isLoanToken(), "Only LoanTokens can be reclaimed");
+            if (_isRepaidInFull(loanToken)) {
+                emit Closed(address(loanToken));
+                loanToken.close();
+            }
             if (loanToken.status() == ILoanToken.Status.Settled) {
-                _lender.reclaim(loanToken);
                 emit Reclaimed(address(loanToken));
+                _lender.reclaim(loanToken);
             }
         }
+    }
+
+    /**
+     * @dev Check whether this loan has been repaid in full
+     * Only uses public functions
+     * @param loanToken LoanToken to check
+     */
+    function _isRepaidInFull(ILoanToken loanToken) private view returns (bool) {
+        return loanToken.status() == ILoanToken.Status.Withdrawn && loanToken.balance() >= loanToken.debt();
     }
  }
