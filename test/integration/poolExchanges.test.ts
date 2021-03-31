affd2ad54b6c038755314b41336b8c6bcdbbb688
Ivan Rukhavets <ivanruch@gmail.com>
2021-03-31 14:07:35 +0300

    add use(solidity)

--- test/integration/poolExchanges.test.ts
+++ test/integration/poolExchanges.test.ts
@@ -1,7 +1,10 @@
 import { upgradeSuite } from './suite'
 import { TrueFiPoolFactory, TrueUsdFactory } from 'contracts'
 import fetch from 'node-fetch'
-import { expect } from 'chai'
+import { expect, use } from 'chai'
+import { solidity } from 'ethereum-waffle'
+
+use(solidity)
 
 describe('Pool exchanges', () => {
   it('sells all available CRV with 1inch', async () => {
@@ -9,7 +12,7 @@ describe('Pool exchanges', () => {
     const tusd = TrueUsdFactory.connect('0x0000000000085d4780B73119b644AE5ecd22b376', pool.signer)
     const crv = TrueUsdFactory.connect('0xD533a949740bb3306d119CC777fa900bA034cd52', pool.signer)
     const crvBalanceBefore = await crv.balanceOf(pool.address)
-    const dataUrl = `https://api.1inch.exchange/v2.0/swap?disableEstimate=true&fromTokenAddress=0xD533a949740bb3306d119CC777fa900bA034cd52&toTokenAddress=0x0000000000085d4780B73119b644AE5ecd22b376&amount=${crvBalanceBefore.toString()}&fromAddress=0xa1e72267084192Db7387c8CC1328fadE470e4149&slippage=1`
+    const dataUrl = `https://api.1inch.exchange/v2.0/swap?disableEstimate=true&fromTokenAddress=0xD533a949740bb3306d119CC777fa900bA034cd52&toTokenAddress=0x0000000000085d4780B73119b644AE5ecd22b376&amount=${crvBalanceBefore.toString()}&fromAddress=0xa1e72267084192Db7387c8CC1328fadE470e4149&slippage=2`
     const body = await (await fetch(dataUrl)).json()
     const data = body.tx.data
     await (await pool.set1InchAddress('0x111111125434b319222cdbf8c261674adb56f3ae')).wait()
