c04d4cfd771b260c4e7b3a314178d4d4e5580cfa
hal <61718761+hal909@users.noreply.github.com>
2021-04-01 06:00:32 -0600

    timelock pause (#538)

---
+++ contracts/governance/mocks/MockTimeLock.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import {Timelock} from "../Timelock.sol";
+
+contract MockTimeLock is Timelock {
+    /**
+     * @dev Initialize sets the addresses of admin and the delay timestamp
+     * @param admin_ The address of admin
+     * @param delay_ The timestamp of delay for timelock contract
+     */
+    function mockInitialize(address admin_, uint256 delay_) external {
+        require(!initalized, "Already initialized");
+        admin = admin_;
+        pauser = admin_;
+        delay = delay_;
+
+        owner_ = msg.sender;
+        initalized = true;
+    }
+}
