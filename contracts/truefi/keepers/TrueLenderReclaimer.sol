24e5bb236739dc319063da6a2e2d45de24877e8e
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-04-02 13:52:49 -0500

    Add a TrueLenderReclaimer keeper contract (#565)
    * Add a bot contract to automate TrueLender.reclaim()
    
    * Replace tabs with spaces
    
    * Close repaid in full loans before reclaiming them
    
    * Rename contract bots dir to keepers
    
    * Deploy TrueLenderReclaimer to Ropsten
    
    * Add source contract to reclaimAll LoanToken revert message
    
    * Add TrueLenderReclaimer unit tests
    
    * Fix lint
    
    * Fix typecheck

---
+++ contracts/truefi/keepers/TrueLenderReclaimer.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+
+import {ILoanToken} from "../interface/ILoanToken.sol";
+import {TrueLender} from "../TrueLender.sol";
+
+/**
+ * @title TrueLenderReclaimer
+ * @dev Bot to automate reclamation of settled TrueLender loans.
+ * Only accesses public/external functions, hence safe to run by anyone
+ */
+contract TrueLenderReclaimer {
+    using SafeMath for uint256;
+
+    // ================ WARNING ==================
+    // ===== THIS CONTRACT IS INITIALIZABLE ======
+    // === STORAGE VARIABLES ARE DECLARED BELOW ==
+    // REMOVAL OR REORDER OF VARIABLES WILL RESULT
+    // ========= IN STORAGE CORRUPTION ===========
+
+    TrueLender public _lender;
+
+    // ======= STORAGE DECLARATION END ============
+
+    /**
+     * @dev Emitted when a LoanToken contract is settled
+     * @param loanToken LoanToken which has been closed
+     */
+    event Closed(address indexed loanToken);
+
+    /**
+     * @dev Emitted when funds are reclaimed from a LoanToken contract
+     * @param loanToken LoanToken from which funds were reclaimed
+     */
+    event Reclaimed(address indexed loanToken);
+
+    constructor(TrueLender lender) public {
+        _lender = lender;
+    }
+
+    /**
+     * @dev Reclaim funds from all settled loans in lender
+     * Only uses public functions, so this is safe to run by anyone.
+     */
+    function reclaimAll() public {
+        ILoanToken[] memory loans = _lender.loans();
+        // TODO avoid iterating through an unbounded array
+        for (uint256 index = 0; index < loans.length; index++) {
+            ILoanToken loanToken = loans[index];
+            require(loanToken.isLoanToken(), "TrueLenderReclaimer: Only LoanTokens can be reclaimed");
+            if (_isRepaidInFull(loanToken)) {
+                emit Closed(address(loanToken));
+                loanToken.close();
+            }
+            if (loanToken.status() == ILoanToken.Status.Settled) {
+                emit Reclaimed(address(loanToken));
+                _lender.reclaim(loanToken);
+            }
+        }
+    }
+
+    /**
+     * @dev Check whether this loan has been repaid in full
+     * Only uses public functions
+     * @param loanToken LoanToken to check
+     */
+    function _isRepaidInFull(ILoanToken loanToken) private view returns (bool) {
+        return
+            loanToken.status() == ILoanToken.Status.Withdrawn &&
+            loanToken.start().add(loanToken.term()) <= block.timestamp &&
+            loanToken.balance() >= loanToken.debt();
+    }
+}
---
+++ scripts/deploy_true_lender_reclaimer.ts
@@ -0,0 +1,23 @@
+/**
+ * PRIVATE_KEY={private_key} ts-node scripts/deploy_true_lender_reclaimer.ts "{network}"
+ */
+import { ethers, providers } from 'ethers'
+
+import {
+  TrueLenderReclaimerFactory,
+} from '../build'
+
+async function deployTrueLenderReclaimer () {
+  const TRUE_LENDER_ADDRESS = '0xb1f283F554995F666bD3238F229ADf0aF7d54fC4'
+  const INFURA_ENDPOINT = 'ec659e9f6af4425c8a13aeb0af9f2809'
+  const GAS_LIMIT = 3_500_000
+  const GAS_PRICE = 1_000_000_000
+  const txnArgs = { gasLimit: GAS_LIMIT, gasPrice: GAS_PRICE }
+  const provider = new providers.InfuraProvider(process.argv[2], INFURA_ENDPOINT)
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+
+  const trueLenderReclaimerImpl = await (await new TrueLenderReclaimerFactory(wallet).deploy(TRUE_LENDER_ADDRESS, txnArgs)).deployed()
+  console.log(`TrueLenderReclaimer: ${trueLenderReclaimerImpl.address}`)
+}
+
+deployTrueLenderReclaimer().catch(console.error)
