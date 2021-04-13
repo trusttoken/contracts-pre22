0f4c9fac49c77741fe97d8d1dc68ad0e94fc415f
Hal Hyatt <feewet@gmail.com>
2021-04-12 22:30:48 -0600

    fix docs

--- contracts/governance/GovernorAlpha.sol
+++ contracts/governance/GovernorAlpha.sol
@@ -6,10 +6,7 @@
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 // 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Ctrl+f for OLD to see all the modifications.
 
-// OLD: pragma solidity ^0.5.16;
 pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
@@ -453,6 +450,10 @@ contract GovernorAlpha is UpgradeableClaimable {
         return totalVote;
     }
 
+    /**
+     * @dev safe96 add function
+     * @return a + b
+     */
     function add96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
         uint96 c = a + b;
         require(c >= a, errorMessage);
--- contracts/governance/mocks/MockTimeLock.sol
+++ contracts/governance/mocks/MockTimeLock.sol
@@ -2,23 +2,27 @@
 pragma solidity 0.6.10;
 
 import {Timelock} from "../Timelock.sol";
-import {UpgradeableClaimable as Claimable} from "../../common/UpgradeableClaimable.sol";
+import {UpgradeableClaimable} from "../../common/UpgradeableClaimable.sol";
 
 contract MockTimeLock is Timelock {
+
     /**
      * @dev Initialize sets the addresses of admin and the delay timestamp
      * @param admin_ The address of admin
      * @param delay_ The timestamp of delay for timelock contract
      */
     function mockInitialize(address admin_, uint256 delay_) external {
-        Claimable.initialize(msg.sender);
+        UpgradeableClaimable.initialize(msg.sender);
         admin = admin_;
         pauser = admin_;
         delay = delay_;
     }
 
-    // test function to set delay
-    function setDelayMock(uint256 _delay) external {
+    /** 
+     * @dev Function to set delay for testing
+     * @param _delay new delay
+     */
+    function mockSetDelay(uint256 _delay) external {
         delay = _delay;
     }
 }
