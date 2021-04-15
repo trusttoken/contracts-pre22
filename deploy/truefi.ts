06d38d6925f6dacfc0be12c9c674719524302560
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-04-14 21:00:12 -0500

    Fix deploy script compiler dependency loops

--- deploy/truefi.ts
+++ deploy/truefi.ts
@@ -7,6 +7,7 @@ import {
   LoanFactory,
   OwnedUpgradeabilityProxy,
   StkTruToken,
+  TestTrustToken,
   Timelock,
   TimeOwnedUpgradeabilityProxy,
   TruPriceOracle,
@@ -27,7 +28,7 @@ const STAKE_DISTRIBUTION_AMOUNT_IN_TRU = 10
 const STAKE_DISTRIBUTION_AMOUNT = parseTRU(STAKE_DISTRIBUTION_AMOUNT_IN_TRU)
 const RATING_AGENCY_DISTRIBUTION_AMOUNT_IN_TRU = 10
 const RATING_AGENCY_DISTRIBUTION_AMOUNT = parseTRU(RATING_AGENCY_DISTRIBUTION_AMOUNT_IN_TRU)
-const TIMELOCK_DELAY = 10
+const TIMELOCK_DELAY = 2 * DAY
 const VOTING_PERIOD = 10
 
 deploy({}, (deployer, config) => {
@@ -39,30 +40,31 @@ deploy({}, (deployer, config) => {
   const timeOwnedProxy = createProxy(TimeOwnedUpgradeabilityProxy)
 
   const trueUSD = proxy(contract('trueUSD', TrueUSD), () => {})
-  const trustToken = timeOwnedProxy(contract('trustToken', TrustToken), 'initialize',
-    [],
-  )
   const trueFiPool = proxy(contract('trueFiPool', TrueFiPool), () => {})
+  const stkTruToken = proxy(contract('stkTruToken', StkTruToken), () => {})
+  const trustToken = is_mainnet ?
+    timeOwnedProxy(contract('trustToken', TrustToken), 'initialize',
+      [],
+    ) : timeOwnedProxy(contract('testTrustToken', TestTrustToken), 'initialize',
+      [],
+    )
+  const truPriceOracle = contract('truPriceOracle', TruPriceOracle)
   const loanFactory = proxy(contract('loanFactory', LoanFactory), 'initialize',
     [trueUSD],
   )
-  const truPriceOracle = contract('truPriceOracle', TruPriceOracle)
   const liquidator = proxy(contract('liquidator', Liquidator), 'initialize',
     [trueFiPool, stkTruToken, trustToken, truPriceOracle, loanFactory],
   )
   const stkTruToken_LinearTrueDistributor = proxy(contract('stkTruToken_LinearTrueDistributor', LinearTrueDistributor), 'initialize',
     [DISTRIBUTION_START, DISTRIBUTION_DURATION, STAKE_DISTRIBUTION_AMOUNT, trustToken],
   )
-  const stkTruToken = proxy(contract('stkTruToken', StkTruToken), 'initialize',
-    [trustToken, trueFiPool, stkTruToken_LinearTrueDistributor, liquidator],
-  )
   stkTruToken_LinearTrueDistributor.setFarm(stkTruToken)
+  stkTruToken.initialize(trustToken, trueFiPool, stkTruToken_LinearTrueDistributor, liquidator)
+  const trueRatingAgencyV2 = proxy(contract('trueRatingAgencyV2', TrueRatingAgencyV2), () => {})
   const arbitraryDistributor = proxy(contract('arbitraryDistributor', ArbitraryDistributor), 'initialize',
     [trueRatingAgencyV2, trustToken, RATING_AGENCY_DISTRIBUTION_AMOUNT],
   )
-  const trueRatingAgencyV2 = proxy(contract('trueRatingAgency2', TrueRatingAgencyV2), 'initialize',
-    [trustToken, stkTruToken, arbitraryDistributor, loanFactory],
-  )
+  trueRatingAgencyV2.initialize(trustToken, stkTruToken, arbitraryDistributor, loanFactory)
   const timelock = proxy(contract(Timelock), 'initialize',
     [TIMELOCK_ADMIN, TIMELOCK_DELAY],
   )
