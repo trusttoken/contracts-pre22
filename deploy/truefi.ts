1d4232e1eddb205b40a772d677104aab7f1f684e
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-04-19 11:59:00 -0500

    Update mars deploy script (#600)
    * Add a 'yarn deploy:truefi' script with default mars flags
    
    * Rename deploy/governance.ts -> deploy/truefi.ts
    
    * Standardize deployments.json contract naming to buildContractName
    
    * Draft fix for deploy script
    
    * Pull --network flag from deploy callback config
    
    * Fix deploy script compiler dependency loops
    
    * Rename timeOwnedProxy to timeProxy
    
    * Attempt to add TrueLender to deploy script
    
    * Update rating agency distributor in deploy script
    
    * Check before re-initializing non-atomic inits
    
    * Uncomment TrueLender deployment
    
    * Add TODOs for all unguarded function calls in deploy script
    
    * Revert "Uncomment TrueLender deployment"
    
    This reverts commit 4f4595ea094ef9e5174bf2e371cfa472d90e239f.
    
    * Use Future<boolean>-friendly conditional execution for deploys
    
    * Etherscan verify contracts directly instead of flatten
    
    * Remove contract name params when they're redundant
    
    mars already defaults to naming deployed ExampleContract instances as
    exampleContract
    
    * Fix build from merging #588

---
+++ deploy/truefi.ts
@@ -0,0 +1,82 @@
+import { contract, createProxy, deploy, runIf } from 'ethereum-mars'
+import {
+  GovernorAlpha,
+  LinearTrueDistributor,
+  Liquidator,
+  LoanFactory,
+  OwnedUpgradeabilityProxy,
+  RatingAgencyV2Distributor,
+  StkTruToken,
+  TestTrustToken,
+  Timelock,
+  TimeOwnedUpgradeabilityProxy,
+  TruPriceOracle,
+  TrueFiPool,
+  TrueLender,
+  TrueRatingAgencyV2,
+  TrueUSD,
+  TrustToken,
+} from '../build/artifacts'
+import { DAY, parseTRU } from '../test/utils'
+import { AddressZero } from '@ethersproject/constants'
+
+// TODO Fill values
+const DISTRIBUTION_DURATION_IN_DAYS = 10
+const DISTRIBUTION_DURATION = DISTRIBUTION_DURATION_IN_DAYS * DAY
+const DISTRIBUTION_START_DATE = '02/18/2021'
+const DISTRIBUTION_START = Date.parse(DISTRIBUTION_START_DATE) / 1000
+const STAKE_DISTRIBUTION_AMOUNT_IN_TRU = 10
+const STAKE_DISTRIBUTION_AMOUNT = parseTRU(STAKE_DISTRIBUTION_AMOUNT_IN_TRU)
+const TIMELOCK_DELAY = 2 * DAY
+const VOTING_PERIOD = 10
+
+deploy({}, (deployer, config) => {
+  const TIMELOCK_ADMIN = deployer
+  const GOV_GUARDIAN = deployer
+  const is_mainnet = config.network == 'mainnet'
+
+  const proxy = createProxy(OwnedUpgradeabilityProxy)
+  const timeProxy = createProxy(TimeOwnedUpgradeabilityProxy)
+
+  const trueUSD = proxy(contract(TrueUSD), () => {})
+  const trustToken = is_mainnet ?
+    timeProxy(contract(TrustToken), 'initialize',
+      [],
+    ) : timeProxy(contract(TestTrustToken), 'initialize',
+      [],
+    )
+  const trueFiPool = proxy(contract(TrueFiPool), () => {})
+  const stkTruToken = proxy(contract(StkTruToken), () => {})
+  const truPriceOracle = contract(TruPriceOracle)
+  const loanFactory = proxy(contract(LoanFactory), 'initialize',
+    [trueUSD],
+  )
+  const liquidator = proxy(contract(Liquidator), 'initialize',
+    [trueFiPool, stkTruToken, trustToken, truPriceOracle, loanFactory],
+  )
+  const stkTruToken_LinearTrueDistributor = proxy(contract('stkTruToken_LinearTrueDistributor', LinearTrueDistributor), 'initialize',
+    [DISTRIBUTION_START, DISTRIBUTION_DURATION, STAKE_DISTRIBUTION_AMOUNT, trustToken],
+  )
+  runIf(stkTruToken_LinearTrueDistributor.farm().equals(stkTruToken).not(), () => {
+    stkTruToken_LinearTrueDistributor.setFarm(stkTruToken)
+  })
+  runIf(stkTruToken.initalized().not(), () => {
+    stkTruToken.initialize(trustToken, trueFiPool, trueFiPool, stkTruToken_LinearTrueDistributor, liquidator)
+  })
+  const trueRatingAgencyV2 = proxy(contract(TrueRatingAgencyV2), () => {})
+  const ratingAgencyV2Distributor = proxy(contract(RatingAgencyV2Distributor), 'initialize',
+    [trueRatingAgencyV2, trustToken],
+  )
+  // TODO check whether trueRatingAgencyV2 has already been initialized, else this will revert
+  trueRatingAgencyV2.initialize(trustToken, stkTruToken, ratingAgencyV2Distributor, loanFactory)
+  // TODO figure out what's going wrong with deploying TrueLender
+  // const trueLender = proxy(contract(TrueLender), 'initialize',
+  //   [trueFiPool, trueRatingAgencyV2, stkTruToken],
+  // )
+  const timelock = proxy(contract(Timelock), 'initialize',
+    [TIMELOCK_ADMIN, TIMELOCK_DELAY],
+  )
+  const governorAlpha = proxy(contract(GovernorAlpha), 'initialize',
+    [timelock, trustToken, stkTruToken, GOV_GUARDIAN, VOTING_PERIOD],
+  )
+})
