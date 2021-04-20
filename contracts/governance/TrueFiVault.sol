7610567166d1aaa666808f6f043918c27067a848
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-04-20 11:08:36 -0500

    Refactor TrueFiVault (proposed merge into #605) (#606)
    * Remove redundant + broken internal stake()/unstake() functions
    
    * Clarify language around deployer
    
    * Restrict voting to TrueFiVault owner
    
    * Reorder functions for clarity: withdrawals, governance, staking
    
    * Change semantics from / to withdraw to reduce overloaded terminology
    
    * Update documentation to explain what TrueFiVault is for
    
    * Consolidate withdrawal logic to be identical between owner and deployer
    
    * Remove unnecessary view functions
    
    The caller could themselves check the public stkTru claimable()
    and balanceOf() for this vault address. No need to add it here
    and thereby introduce boilerplate.
    
    * Add claim + restake delegation
    
    * Pass governance in as a constructor arg instead of hardcoding mainnet addresses

--- contracts/governance/TrueFiVault.sol
+++ contracts/governance/TrueFiVault.sol
@@ -4,14 +4,19 @@ pragma solidity 0.6.10;
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {VoteToken} from "./VoteToken.sol";
 import {GovernorAlpha} from "./GovernorAlpha.sol";
 import {StkTruToken} from "./StkTruToken.sol";
 
 /**
  * @title TrueFiVault
- * @dev Vault for TRU tokens to create lockup period
- * Allows staking TRU and using TRU in governance
+ * @dev Vault for granting TRU tokens from deployer to owner after a lockout period.
+ *
+ * After the lockout period, owner may withdraw any TRU in the vault.
+ * During the lockout period, the vault still allows owner to stake TRU
+ * and cast votes in governance.
+ *
+ * In case of emergency or error, deployer reserves the ability to withdraw all
+ * funds in vault.
  */
 contract TrueFiVault {
     using SafeMath for uint256;
@@ -19,19 +24,28 @@ contract TrueFiVault {
     address deployer;
     uint256 expiry;
 
-    VoteToken tru = VoteToken(0x4C19596f5aAfF459fA38B0f7eD92F11AE6543784);
-    StkTruToken stkTru = StkTruToken(0x23696914Ca9737466D8553a2d619948f548Ee424);
-    GovernorAlpha governance = GovernorAlpha(0x0000000000000000000000000000000000000000);
+    IERC20 tru;
+    StkTruToken stkTru;
+    GovernorAlpha governance;
 
     uint256 constant LOCKOUT = 180 days;
 
-    event Unlock();
+    event WithdrawTo(address recipient);
 
-    constructor(address _owner, uint256 _amount) public {
+    constructor(
+        address _owner,
+        uint256 _amount,
+        GovernorAlpha _governance
+    ) public {
         owner = _owner;
         deployer = msg.sender;
         expiry = block.timestamp.add(LOCKOUT);
-        require(tru.transferFrom(msg.sender, address(this), _amount), "TrueFiVault: insufficient balance.");
+
+        governance = _governance;
+        tru = IERC20(address(governance.trustToken()));
+        stkTru = StkTruToken(address(governance.stkTRU()));
+
+        require(tru.transferFrom(deployer, address(this), _amount), "TrueFiVault: insufficient balance.");
     }
 
     /**
@@ -43,7 +57,7 @@ contract TrueFiVault {
     }
 
     /**
-     * @dev Throws if called by any account other than the owner.
+     * @dev Throws if called by any account other than the deployer.
      */
     modifier onlyDeployer() {
         require(msg.sender == deployer, "only deployer");
@@ -51,44 +65,27 @@ contract TrueFiVault {
     }
 
     /**
-     * @dev Claim TRU after expiry time
+     * @dev Allow deployer to withdraw funds in case of emergency or mistake
      */
-    function unlock() public onlyOwner {
-        require(block.timestamp >= expiry, "TrueFiVault: cannot claim before expiration");
-        require(tru.balanceOf(address(this)) > 0, "TrueFiVault: already claimed");
-
-        // unstake if staked balance
-        // will revert if not in cooldown
-        if (stakedBalance() > 0) {
-            _unstake(stkTru.balanceOf(address(this)));
-        }
-
-        tru.transfer(owner, tru.balanceOf(address(this)));
-        emit Unlock();
+    function withdrawToDeployer() public onlyDeployer {
+        _withdrawTo(deployer);
     }
 
     /**
-     * @dev Get claimable TRU amount from staking contract
-     * @return Claimable TRU amount
+     * @dev Withdraw funds to owner after expiry time
      */
-    function claimable() public view returns (uint256) {
-        return stkTru.claimable(address(this), IERC20(address(tru)));
+    function withdrawToOwner() public onlyOwner {
+        require(block.timestamp >= expiry, "TrueFiVault: owner cannot withdraw before expiration");
+        _withdrawTo(owner);
     }
 
     /**
-     * @dev Get amount of TRU staked
-     * @return Balance of stkTRU
+     * @dev Internal function to withdraw funds to recipient
      */
-    function stakedBalance() public view returns (uint256) {
-        return stkTru.balanceOf(address(this));
-    }
-
-    /**
-     * @dev Stake `amount` TRU in staking contract
-     * @param amount Amount of TRU to stake
-     */
-    function stake(uint256 amount) public onlyOwner {
-        _stake(amount);
+    function _withdrawTo(address recipient) private {
+        tru.transfer(recipient, tru.balanceOf(address(this)));
+        stkTru.transfer(recipient, stkTru.balanceOf(address(this)));
+        emit WithdrawTo(recipient);
     }
 
     /**
@@ -97,8 +94,16 @@ contract TrueFiVault {
      * @param proposalId Proposal ID
      * @param support Vote boolean
      */
-    function castVote(uint256 proposalId, bool support) public {
+    function castVote(uint256 proposalId, bool support) public onlyOwner {
         governance.castVote(proposalId, support);
+    }
+
+    /**
+     * @dev Stake `amount` TRU in staking contract
+     * @param amount Amount of TRU to stake
+     */
+    function stake(uint256 amount) public onlyOwner {
+        stkTru.stake(amount);
     }
 
     /**
@@ -106,7 +111,7 @@ contract TrueFiVault {
      * @param amount Amount of TRU to unstake
      */
     function unstake(uint256 amount) public onlyOwner {
-        _unstake(amount);
+        stkTru.unstake(amount);
     }
 
     /**
@@ -120,30 +125,14 @@ contract TrueFiVault {
      * @dev Claim TRU rewards from staking contract
      */
     function claimRewards() public onlyOwner {
-        stkTru.claimRewards(IERC20(address(tru)));
+        stkTru.claimRewards(tru);
     }
 
     /**
-     * @dev Allow deployer to reclaim funds in case of emergency or mistake
+     * @dev Claim TRU rewards, then restake without transferring
+     * Allows account to save more gas by avoiding out-and-back transfers
      */
-    function reclaim() public onlyDeployer {
-        tru.transfer(deployer, tru.balanceOf(address(this)));
-        stkTru.transfer(deployer, stkTru.balanceOf(address(this)));
-    }
-
-    /**
-     * @dev Internal function to stake `amount` of TRU
-     * @param amount Amount of TRU to stake
-     */
-    function _stake(uint256 amount) internal {
-        stkTru.unstake(amount);
-    }
-
-    /**
-     * @dev Internal function to unstake `amount` of TRU
-     * @param amount Amount of TRU to unstake
-     */
-    function _unstake(uint256 amount) internal {
-        stkTru.stake(amount);
+    function claimRestake() public onlyOwner {
+        stkTru.claimRestake();
     }
 }
