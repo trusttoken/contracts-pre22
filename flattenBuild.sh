cc146a94146902ede4f4a1f57cae1e12211ce864
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-04-20 18:54:22 +0200

    debug

--- flattenBuild.sh
+++ flattenBuild.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-
+echo "0"
 set -e
+echo "1"
 
 ODline=$(grep 'artifacts' hardhat.config.ts)
+echo "2"
 regex=": \'(.+)\'"
+echo "3"
 [[ $ODline =~ $regex ]]
+echo "4"
 outputDir=${BASH_REMATCH[1]}
+echo "5"
 
 touch $outputDir/index.ts
+echo "6"
 > $outputDir/index.ts
 
 for file in $(find $outputDir -name '[a-zA-Z_]*.json' -not -name '*.dbg.json')
 do
+	echo "7"
 	mv $file $outputDir
 done
 
