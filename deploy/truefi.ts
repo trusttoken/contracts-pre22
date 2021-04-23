1e3815a4d187a4d47308819b59495ef2017b9e45
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-04-23 12:45:34 -0500

    Address PR #611 comments

--- deploy/truefi.ts
+++ deploy/truefi.ts
@@ -4,19 +4,20 @@ import {
   LinearTrueDistributor,
   Liquidator,
   LoanFactory,
+  MockCurveGauge,
   OwnedUpgradeabilityProxy,
   RatingAgencyV2Distributor,
   StkTruToken,
+  TestTrueFiPool,
   TestTrustToken,
   Timelock,
   TimeOwnedUpgradeabilityProxy,
   TruPriceOracle,
-  TestTrueFiPool,
+  TrueFiPool,
   TrueRatingAgencyV2,
   TrueUSD,
   TrustToken,
   TrueLender,
-  MockCurveGauge,
 } from '../build/artifacts'
 import { utils } from 'ethers'
 import { AddressZero } from '@ethersproject/constants'
@@ -25,7 +26,15 @@ const DAY = 60 * 60 * 24
 
 // TODO Fill values
 const deployParams = {
-  kovan: {
+  mainnet: {
+    Y_CRV_GAUGE: "0xFA712EE4788C042e2B7BB55E6cb8ec569C4530c1",
+    DISTRIBUTION_DURATION: 180 * DAY,
+    DISTRIBUTION_START: Date.parse('04/24/2021') / 1000,
+    STAKE_DISTRIBUTION_AMOUNT: utils.parseUnits('10', 8),
+    TIMELOCK_DELAY: 2 * DAY,
+    VOTING_PERIOD: 10, // blocks
+  },
+  testnet: {
     DISTRIBUTION_DURATION: 180 * DAY,
     DISTRIBUTION_START: Date.parse('04/24/2021') / 1000,
     STAKE_DISTRIBUTION_AMOUNT: utils.parseUnits('10', 8),
@@ -38,6 +47,7 @@ deploy({}, (deployer, config) => {
   const TIMELOCK_ADMIN = deployer
   const GOV_GUARDIAN = deployer
   const is_mainnet = config.network === 'mainnet'
+  const NETWORK = is_mainnet ? 'mainnet' : 'testnet'
 
   const proxy = createProxy(OwnedUpgradeabilityProxy)
   const timeProxy = createProxy(TimeOwnedUpgradeabilityProxy)
@@ -49,11 +59,15 @@ deploy({}, (deployer, config) => {
     ) : timeProxy(contract(TestTrustToken), 'initialize',
       [],
     )
-  const lender = proxy(contract(TrueLender), () => {})
+  const trueLender = proxy(contract(TrueLender), () => {})
   const stkTruToken = proxy(contract(StkTruToken), () => {})
-  const gauge = contract(MockCurveGauge)
-  const trueFiPool = proxy(contract(TestTrueFiPool), 'initialize',
-    [AddressZero, gauge, trueUSD, lender, AddressZero, stkTruToken, AddressZero, AddressZero],
+  const yCrvGauge = is_mainnet
+    ? deployParams['mainnet'].Y_CRV_GAUGE
+    : contract(MockCurveGauge)
+  const trueFiPool = is_mainnet
+    ? proxy(contract(TrueFiPool), () => {})
+    : proxy(contract(TestTrueFiPool), 'initialize',
+    [AddressZero, yCrvGauge, trueUSD, trueLender, AddressZero, stkTruToken, AddressZero, AddressZero],
   )
   const truPriceOracle = contract(TruPriceOracle)
   const loanFactory = proxy(contract(LoanFactory), 'initialize',
@@ -63,7 +77,7 @@ deploy({}, (deployer, config) => {
     [trueFiPool, stkTruToken, trustToken, truPriceOracle, loanFactory],
   )
   const stkTruToken_LinearTrueDistributor = proxy(contract('stkTruToken_LinearTrueDistributor', LinearTrueDistributor), 'initialize',
-    [deployParams[config.network].DISTRIBUTION_START, deployParams[config.network].DISTRIBUTION_DURATION, deployParams[config.network].STAKE_DISTRIBUTION_AMOUNT, trustToken],
+    [deployParams[NETWORK].DISTRIBUTION_START, deployParams[NETWORK].DISTRIBUTION_DURATION, deployParams[NETWORK].STAKE_DISTRIBUTION_AMOUNT, trustToken],
   )
   runIf(stkTruToken_LinearTrueDistributor.farm().equals(stkTruToken).not(), () => {
     stkTruToken_LinearTrueDistributor.setFarm(stkTruToken)
@@ -78,13 +92,13 @@ deploy({}, (deployer, config) => {
   runIf(trueRatingAgencyV2.isInitialized().not(), () => {
     trueRatingAgencyV2.initialize(trustToken, stkTruToken, ratingAgencyV2Distributor, loanFactory)
   })
-  runIf(lender.isInitialized().not(), () => {
-    lender.initialize(trueFiPool, trueRatingAgencyV2, stkTruToken)
+  runIf(trueLender.isInitialized().not(), () => {
+    trueLender.initialize(trueFiPool, trueRatingAgencyV2, stkTruToken)
   })
   const timelock = proxy(contract(Timelock), 'initialize',
-    [TIMELOCK_ADMIN, deployParams[config.network].TIMELOCK_DELAY],
+    [TIMELOCK_ADMIN, deployParams[NETWORK].TIMELOCK_DELAY],
   )
-  proxy(contract(GovernorAlpha), 'initialize',
-    [timelock, trustToken, stkTruToken, GOV_GUARDIAN, deployParams[config.network].VOTING_PERIOD],
+  const governorAlpha = proxy(contract(GovernorAlpha), 'initialize',
+    [timelock, trustToken, stkTruToken, GOV_GUARDIAN, deployParams[NETWORK].VOTING_PERIOD],
   )
 })
