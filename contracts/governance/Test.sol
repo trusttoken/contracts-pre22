ec51b8c0cc6cbd2c6a5be8b81d77675cab1f889a
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-04-24 06:41:50 -0500

    Another minimum contract to trigger Timelock test failure

---
+++ contracts/governance/Test.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+
+import {StkTruToken} from "./StkTruToken.sol";
+import {ITrueRatingAgencyV2} from "../truefi/interface/ITrueRatingAgencyV2.sol";
+
+contract Test {
+    constructor() public {
+        block.timestamp;
+    }
+}
