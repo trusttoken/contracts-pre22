7ff22067dc12e57f481bdf6c66af6d5e9dddb83f
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-04-26 12:45:57 -0500

    Try deploying governance on ropsten without using Mars for etherscan verification

--- scripts/deploy_mock_governance.ts
+++ scripts/deploy_mock_governance.ts
@@ -8,28 +8,18 @@ import {
   MockTimeLock__factory,
   GovernorAlpha__factory,
   OwnedUpgradeabilityProxy__factory,
-} from 'contracts'
+} from '../build'
 
 async function deployMockGovernance () {
   const txnArgs = { gasLimit: 4_500_000, gasPrice: 1_000_000_000 }
   const provider = new providers.InfuraProvider(process.argv[2], 'e33335b99d78415b82f8b9bc5fdc44c0')
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
-  const trustTokenImpl = await (await new TestTrustToken__factory(wallet).deploy(txnArgs)).deployed()
-  console.log(`TRU: ${trustTokenImpl.address}`)
-
-  const timelockImpl = await (await new MockTimeLock__factory(wallet).deploy(txnArgs)).deployed()
-  console.log(`Timelock: ${timelockImpl.address}`)
-
   const governanceImpl = await (await new GovernorAlpha__factory(wallet).deploy(txnArgs)).deployed()
   console.log(`Governance: ${governanceImpl.address}`)
 
-  const tru = OwnedUpgradeabilityProxy__factory.connect('0x12b2f909d9ea91c86dc7fbba272d8abbcddfd72c', wallet)
-  const timelock = OwnedUpgradeabilityProxy__factory.connect('0x418Daf88CCfe083324f8c91e6FE8da3e86d9a182', wallet)
   const governance = OwnedUpgradeabilityProxy__factory.connect('0x01cACF74540154B7c759657A73f74a578CFEA69b', wallet)
 
-  await tru.upgradeTo(trustTokenImpl.address)
-  await timelock.upgradeTo('0x2eF8FF8D4cd628948891e37f3AAc3d76bF14E4a0')
   await governance.upgradeTo(governanceImpl.address)
 }
 
