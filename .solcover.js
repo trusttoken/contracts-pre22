a71c2f5376e844047db6eb7fb47268670a1ab29d
Roman Moskalenko <skalermo@gmail.com>
2021-04-26 09:50:48 +0200

    Ignore mocks and interfaces when running coverage

---
+++ .solcover.js
@@ -0,0 +1,20 @@
+module.exports = {
+  skipFiles: [
+    './governance/interface',
+    './governance/mocks',
+    './proxy/interface',
+    './proxy/mocks',
+    './registry/interface',
+    './registry/mocks',
+    './true-currencies/interface',
+    './true-currencies/mocks',
+    './true-gold/interface',
+    './true-gold/mocks',
+    './truefi/interface',
+    './truefi/mocks',
+    './truefi2/interface',
+    './truefi2/mocks',
+    './trusttoken/interface',
+    './trusttoken/mocks',
+  ]
+}
