60235886b6ed07066802e6bd7a1290299ab66ac0
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-04-28 21:05:02 -0500

    Rename deploy script variables to be more explicit

--- deploy/truefi2.ts
+++ deploy/truefi2.ts
@@ -18,6 +18,8 @@ import {
 } from '../build/artifacts'
 import { AddressZero } from '@ethersproject/constants'
 
+const ONE_INCH_EXCHANGE = '0x11111112542d85b3ef69ae05771c2dccff4faa26'
+
 deploy({}, (_, config) => {
   const proxy = createProxy(OwnedUpgradeabilityProxy)
   const timeProxy = createProxy(TimeOwnedUpgradeabilityProxy)
@@ -29,28 +31,28 @@ deploy({}, (_, config) => {
 
   const trueLender2 = proxy(contract(TrueLender2), () => {})
 
-  const poolImplementation = contract(TrueFiPool2)
-  const implementationReference = contract(ImplementationReference, [poolImplementation])
+  const trueFiPool2 = contract(TrueFiPool2)
+  const implementationReference = contract(ImplementationReference, [trueFiPool2])
   const poolFactory = proxy(contract(PoolFactory), 'initialize', [implementationReference, trustToken, trueLender2])
 
-  runIf(trueLender2.isInitialized().not(), () => trueLender2.initialize(stkTruToken, poolFactory, trueRatingAgencyV2, '0x11111112542d85b3ef69ae05771c2dccff4faa26'))
+  runIf(trueLender2.isInitialized().not(), () => trueLender2.initialize(stkTruToken, poolFactory, trueRatingAgencyV2, ONE_INCH_EXCHANGE))
   const liquidator2 = proxy(contract(Liquidator2), () => {})
   const loanFactory2 = proxy(contract(LoanFactory2), 'initialize', [poolFactory, trueLender2, liquidator2])
   runIf(liquidator2.isInitialized().not(), () => liquidator2.initialize(stkTruToken, trustToken, loanFactory2))
 
-  const usdc = contract('usdc_MockERC20Token', MockERC20Token)
+  const usdc_MockERC20Token = contract('usdc_MockERC20Token', MockERC20Token)
 
-  runIf(poolFactory.pool(usdc).equals(AddressZero), () => {
-    poolFactory.whitelist(usdc, true)
-    poolFactory.createPool(usdc)
+  runIf(poolFactory.pool(usdc_MockERC20Token).equals(AddressZero), () => {
+    poolFactory.whitelist(usdc_MockERC20Token, true)
+    poolFactory.createPool(usdc_MockERC20Token)
   })
-  const usdcPool = poolFactory.pool(usdc)
+  const usdc_TrueFiPool2 = poolFactory.pool(usdc_MockERC20Token)
 
-  runIf(trueLender2.feePool().equals(AddressZero), () => trueLender2.setFeePool(usdcPool))
+  runIf(trueLender2.feePool().equals(AddressZero), () => trueLender2.setFeePool(usdc_TrueFiPool2))
   if (!isMainnet) {
     trueLender2.setFee(500)
   }
 
-  const oldPool = isMainnet ? proxy(contract(TrueFiPool), () => {}) : proxy(contract(TestTrueFiPool), () => {})
-  runIf(poolFactory.isPool(oldPool).not(), () => poolFactory.addLegacyPool(oldPool))
+  const legacy_TrueFiPool = isMainnet ? proxy(contract(TrueFiPool), () => {}) : proxy(contract(TestTrueFiPool), () => {})
+  runIf(poolFactory.isPool(legacy_TrueFiPool).not(), () => poolFactory.addLegacyPool(legacy_TrueFiPool))
 })
