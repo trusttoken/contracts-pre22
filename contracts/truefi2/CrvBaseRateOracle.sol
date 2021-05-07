0fa1b3ebcb7d924e7acedc36f4ac687df4e7a74e
Roman Moskalenko <skalermo@gmail.com>
2021-05-07 14:55:10 +0200

    Change the formula of time-weighted average

--- contracts/truefi2/CrvBaseRateOracle.sol
+++ contracts/truefi2/CrvBaseRateOracle.sol
@@ -39,6 +39,8 @@ contract CrvBaseRateOracle {
             histBuffer.baseRates[i] = curCrvBaseRate;
             histBuffer.timestamps[i] = curTimestamp;
         }
+        // prevent first calculateAverageRate call from division by zero
+        histBuffer.timestamps[0] = histBuffer.timestamps[0].sub(1);
     }
 
     function getHistBuffer() public view returns (uint256[BUFFER_SIZE] memory, uint256[BUFFER_SIZE] memory, uint8) {
@@ -53,50 +55,18 @@ contract CrvBaseRateOracle {
     }
 
     function calculateAverageRate() public view returns (uint256) {
-        // Average rate is calculated by taking
-        // the time-weighted average of the speeds
-        // of virtual price changes.
-        //
-        // let 'd_{i,j}' be the speed of virtual price change:
-        //           v_i - v_j
-        // d_{i,j} = --------- , i != j
-        //           t_i - t_j
-        // where v_i, v_j are values of rates,
-        // t_i and t_j are their timestamps respectively.
-        //
-        // To calculate time-weighted average one has to
-        // multiply each element by respective amount time
-        // and then divide the sum of the products
-        // by the sum of those times.
-        //
-        // Average rate is calculated based on formula below:
-        //           d_{n,n-1} * (t_n - t_{n-1}) + ... + d_{2,1} * (t_2 - t_1)
-        // avgRate = --------------------------------------------------------- = (*)
-        //                       (t_n - t_{n-1}) + ... + (t_2 - t_1)
-        //
-        // which can be simplified to:
-        //       (v_n - v_{n-1}) + ... + (v_2 - v_1)
-        // (*) = ----------------------------------- = (**)
-        //       (t_n - t_{n-1}) + ... + (t_2 - t_1)
-        //
-        // which can be simplified even further to:
-        //
-        //        v_n - v_1
-        // (**) = ---------
-        //        t_n - t_1
-        //
-        // where v_n is the most recent rate, v_1 is the oldest rate,
-        // t_n and t_1 are their timestamps respectively.
-        //
-        // For v_n and t_n we use current values
-        // instead of the values stored in histBuffer
-        // to be more sensitive to virtual price changes.
-        uint256 v_n = curve.get_virtual_price();
-        uint256 t_n = block.timestamp;
-        uint256 v_1 = histBuffer.baseRates[histBuffer.insertIndex];
-        uint256 t_1 = histBuffer.timestamps[histBuffer.insertIndex];
-        // todo handle case when (v_n - v_1) < 0
-        return (v_n.sub(v_1)).mul(100_00).div(t_n.sub(t_1));
+        uint8 iidx = histBuffer.insertIndex;
+        uint256 sum;
+        for (uint8 i = 1; i < BUFFER_SIZE; i++) {
+            uint8 idx = uint8(iidx.add(i) % BUFFER_SIZE);
+            uint8 prevIdx = uint8(iidx.add(i).sub(1) % BUFFER_SIZE);
+            uint256 dt = histBuffer.timestamps[idx].sub(histBuffer.timestamps[prevIdx]);
+            sum = sum.add(histBuffer.baseRates[idx].mul(dt));
+        }
+        // amount of time covered by the buffer
+        uint256 totalTime = histBuffer.timestamps[iidx.add(BUFFER_SIZE).sub(1) % BUFFER_SIZE]
+        .sub(histBuffer.timestamps[iidx]);
+        return sum.mul(100_00).div(totalTime);
     }
 
     function rate() public view returns (uint256, uint256, uint256) {
