2b787bbd8fd337d94a66158f07ba9a2cf8979023
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-05-19 21:36:21 -0500

    Deploy tfUSDC distributor + farm to ropsten (#658)
    * Add tfUSDC distributor and farm to deploy:truefi2
    
    * Deploy tfUSDC distributor and farm
    
    * Move tfUSDC distributor + farm impls above proxy upgrades to follow PR #656's style

--- deploy/truefi2.ts
+++ deploy/truefi2.ts
@@ -1,6 +1,7 @@
 import { contract, createProxy, deploy, runIf } from 'ethereum-mars'
 import {
   ImplementationReference,
+  LinearTrueDistributor,
   Liquidator2,
   LoanFactory2, Mock1InchV3,
   TestUSDCToken,
@@ -10,22 +11,32 @@ import {
   TestTrueFiPool,
   TestTrustToken,
   TimeOwnedUpgradeabilityProxy,
+  TrueFarm,
   TrueFiPool,
   TrueFiPool2,
   TrueLender2,
   TrueRatingAgencyV2,
   TrustToken,
 } from '../build/artifacts'
+import { utils } from 'ethers'
 import { AddressZero } from '@ethersproject/constants'
+
+const DAY = 60 * 60 * 24
 
 // TODO set this properly for testnets or deploy a mock
 const ONE_INCH_EXCHANGE = '0x11111112542d85b3ef69ae05771c2dccff4faa26'
 const deployParams = {
   mainnet: {
     USDC: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    DISTRIBUTION_DURATION: 180 * DAY,
+    DISTRIBUTION_START: Date.parse('04/24/2021') / 1000,
+    STAKE_DISTRIBUTION_AMOUNT: utils.parseUnits('10', 8),
   },
   testnet: {
     LOAN_INTEREST_FEE: 500,
+    DISTRIBUTION_DURATION: 180 * DAY,
+    DISTRIBUTION_START: Date.parse('04/24/2021') / 1000,
+    STAKE_DISTRIBUTION_AMOUNT: utils.parseUnits('10', 8),
   },
 }
 
@@ -33,6 +44,7 @@ deploy({}, (_, config) => {
   const proxy = createProxy(OwnedUpgradeabilityProxy)
   const timeProxy = createProxy(TimeOwnedUpgradeabilityProxy)
   const isMainnet = config.network === 'mainnet'
+  const NETWORK = isMainnet ? 'mainnet' : 'testnet'
 
   // Existing contracts
   const trustToken = isMainnet
@@ -54,6 +66,8 @@ deploy({}, (_, config) => {
   const poolFactory_impl = contract(PoolFactory)
   const liquidator2_impl = contract(Liquidator2)
   const loanFactory2_impl = contract(LoanFactory2)
+  const usdc_TrueFiPool2_LinearTrueDistributor_impl = contract('usdc_TrueFiPool2_LinearTrueDistributor', LinearTrueDistributor)
+  const usdc_TrueFiPool2_TrueFarm_impl = contract('usdc_TrueFiPool2_TrueFarm', TrueFarm)
 
   // New contract proxies
   const trueLender2 = proxy(trueLender2_impl, () => {})
@@ -78,6 +92,16 @@ deploy({}, (_, config) => {
   const usdc_TrueFiPool2 = poolFactory.pool(usdc)
   runIf(trueLender2.feePool().equals(AddressZero), () => {
     trueLender2.setFeePool(usdc_TrueFiPool2)
+  })
+  const usdc_TrueFiPool2_LinearTrueDistributor = proxy(usdc_TrueFiPool2_LinearTrueDistributor_impl, 'initialize',
+    [deployParams[NETWORK].DISTRIBUTION_START, deployParams[NETWORK].DISTRIBUTION_DURATION, deployParams[NETWORK].STAKE_DISTRIBUTION_AMOUNT, usdc_TrueFiPool2],
+  )
+  const usdc_TrueFiPool2_TrueFarm = proxy(usdc_TrueFiPool2_TrueFarm_impl, () => {})
+  runIf(usdc_TrueFiPool2_LinearTrueDistributor.farm().equals(usdc_TrueFiPool2_TrueFarm).not(), () => {
+    usdc_TrueFiPool2_LinearTrueDistributor.setFarm(usdc_TrueFiPool2_TrueFarm)
+  })
+  runIf(usdc_TrueFiPool2_TrueFarm.isInitialized().not(), () => {
+    usdc_TrueFiPool2_TrueFarm.initialize(usdc_TrueFiPool2, usdc_TrueFiPool2_LinearTrueDistributor, 'tfUSDC')
   })
   if (!isMainnet) {
     trueLender2.setFee(deployParams['testnet'].LOAN_INTEREST_FEE)
