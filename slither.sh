1a224bf41a9a04988b1bea3c94fca862bd8ec767
Roman Moskalenko <skalermo@gmail.com>
2021-05-21 14:38:38 +0200

    Make slither analyzer script work

--- slither.sh
+++ slither.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
-pip3 install slither-analyzer
-pip3 install solc-select
+
+if [ ! $(which python3) ]; then
+  echo "python3 is required to run this script"
+  exit 1
+fi
+if [ ! $(which pip3) ]; then
+  echo "pip3 is required to run this script"
+  exit 1
+fi
+if [ ! -d "venv" ]; then
+  echo "Generating python virtual environment..."
+  python3 -m venv venv
+fi
+
+echo "Activating python virtual environment..."
+source venv/bin/activate
+
+pip3 install slither-analyzer --disable-pip-version-check
+pip3 install solc-select --disable-pip-version-check
 solc-select install 0.6.10
 solc-select use 0.6.10
 
@@ -15,3 +32,7 @@ slither flatten/TrueLender.sol --print human-summary
 slither flatten/TrueRatingAgencyV2.sol --print human-summary
 slither flatten/TrustToken.sol --print human-summary
 slither flatten/TimeLock.sol --print human-summary
+
+echo "Deactivating python virtual environment..."
+deactivate
+echo "Done."
