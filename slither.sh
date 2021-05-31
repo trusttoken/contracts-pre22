fb3657560be4950f440b6da4c2f49ccb8bc6c13d
Roman Moskalenko <skalermo@gmail.com>
2021-05-31 13:08:15 +0200

    Make CI fail on slither

--- slither.sh
+++ slither.sh
@@ -23,14 +23,43 @@ solc-select use 0.6.10
 
 yarn flatten
 
-slither flatten/GovernorAlpha.sol --print human-summary
-slither flatten/Liquidator.sol --print human-summary
-slither flatten/LoanFactory.sol --print human-summary
-slither flatten/LoanToken.sol --print human-summary
-slither flatten/StkTruToken.sol --print human-summary
-slither flatten/TrueLender.sol --print human-summary
-slither flatten/TrueRatingAgencyV2.sol --print human-summary
-slither flatten/TrustToken.sol --print human-summary
-slither flatten/Timelock.sol --print human-summary
+slither_args="--solc-disable-warnings --exclude-low --exclude-informational --exclude=naming-convention,unused-state-variables,solc-version,assembly-usage,low-level-call"
+
+if ! slither flatten/GovernorAlpha.sol ${slither_args}; then
+  echo "slither failed: GovernorAlpha"
+  exit 1
+fi
+if ! slither flatten/Liquidator.sol ${slither_args}; then
+  echo "slither failed: Liquidator"
+  exit 1
+fi
+if ! slither flatten/LoanFactory.sol ${slither_args}; then
+  echo "slither failed: LoanFactory"
+  exit 1
+fi
+if ! slither flatten/LoanToken.sol ${slither_args}; then
+  echo "slither failed: LoanToken"
+  exit 1
+fi
+if ! slither flatten/StkTruToken.sol ${slither_args}; then
+  echo "slither failed: StkTruToken"
+  exit 1
+fi
+if ! slither flatten/TrueLender.sol ${slither_args}; then
+  echo "slither failed: TrueLender"
+  exit 1
+fi
+if ! slither flatten/TrueRatingAgencyV2.sol ${slither_args}; then
+  echo "slither failed: TrueRatingAgencyV2"
+  exit 1
+fi
+if ! slither flatten/TrustToken.sol ${slither_args}; then
+  echo "slither failed: TrustToken"
+  exit 1
+fi
+if ! slither flatten/Timelock.sol ${slither_args}; then
+  echo "slither failed: Timelock"
+  exit 1
+fi
 
 echo "Done."
