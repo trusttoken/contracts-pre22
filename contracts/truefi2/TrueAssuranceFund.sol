a290dc9df1da94fff41c030d8e31c01a835c473f
Bartek Rutkowski <bartek@ethworks.io>
2021-06-11 11:25:50 +0200

    Add initializer and basic loan validations (#692)
    * Add initializer and basic loan validations
    
    * Lint fix
    
    * fix description
    
    * Add minor tweak

--- contracts/truefi2/TrueAssuranceFund.sol
+++ contracts/truefi2/TrueAssuranceFund.sol
@@ -5,14 +5,24 @@ import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import {UpgradeableClaimable} from "../common/UpgradeableClaimable.sol";
 import {ILoanToken2} from "./interface/ILoanToken2.sol";
 import {ITrueFiPool2} from "./interface/ITrueFiPool2.sol";
+import {ILoanFactory2} from "./interface/ILoanFactory2.sol";
 
-contract TrueAssuranceFund {
+contract TrueAssuranceFund is UpgradeableClaimable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
+    ILoanFactory2 public loanFactory;
+
+    function initialize(ILoanFactory2 _loanFactory) public initializer {
+        UpgradeableClaimable.initialize(msg.sender);
+        loanFactory = _loanFactory;
+    }
+
     function liquidate(ILoanToken2 loan) external {
+        require(loanFactory.isLoanToken(address(loan)), "TrueAssuranceFund: Unknown loan");
         require(loan.status() == ILoanToken2.Status.Defaulted, "TrueAssuranceFund: Loan is not defaulted");
         ITrueFiPool2 pool = ITrueFiPool2(loan.pool());
         IERC20(pool.token()).safeTransfer(address(pool), loan.debt());
