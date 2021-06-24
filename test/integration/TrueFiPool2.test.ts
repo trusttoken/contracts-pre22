9fc0e99b39a69a24c07b2c5b2f02a927f2e415fd
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-06-24 16:10:24 +0200

    Extract addresses to constants

--- test/integration/TrueFiPool2.test.ts
+++ test/integration/TrueFiPool2.test.ts
@@ -24,6 +24,8 @@ use(solidity)
 describe('TrueFiPool2', () => {
   const TRU_ADDRESS = '0x4c19596f5aaff459fa38b0f7ed92f11ae6543784'
   const USDC_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+  const TFUSDT_ADDRESS = '0x6002b1dcb26e7b1aa797a17551c6f487923299d7'
+  const TFUSDT_STRATEGY_ADDRESS = '0x8D162Caa649e981E2a0b0ba5908A77f2536B11A8'
   const TRU_HOLDER = '0x23696914ca9737466d8553a2d619948f548ee424'
   const OWNER = '0x52bc44d5378309EE2abF1539BF71dE1b7d7bE3b5'
   const PROXY_OWNER = '0x16cea306506c387713c70b9c1205fd5ac997e78e'
@@ -52,9 +54,9 @@ describe('TrueFiPool2', () => {
   })
 
   it('tether flush', async () => {
-    const usdtPool = TrueFiPool2__factory.connect('0x6002b1dcb26e7b1aa797a17551c6f487923299d7', powner)
-    const proxy = OwnedProxyWithReference__factory.connect('0x6002b1dcb26e7b1aa797a17551c6f487923299d7', powner)
-    const strategyProxy = OwnedUpgradeabilityProxy__factory.connect('0x8D162Caa649e981E2a0b0ba5908A77f2536B11A8', powner)
+    const usdtPool = TrueFiPool2__factory.connect(TFUSDT_ADDRESS, powner)
+    const proxy = OwnedProxyWithReference__factory.connect(TFUSDT_ADDRESS, powner)
+    const strategyProxy = OwnedUpgradeabilityProxy__factory.connect(TFUSDT_STRATEGY_ADDRESS, powner)
     await proxy.changeImplementationReference(implementationReference.address)
     const newStrategy = await deployContract(CurveYearnStrategy__factory)
     await strategyProxy.upgradeTo(newStrategy.address)
