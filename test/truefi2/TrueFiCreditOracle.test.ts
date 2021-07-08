56e24c2d6610f4a1213a19011af434cca9dba555
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-07-08 16:06:26 +0200

    Add manager role to CreditOracle

--- test/truefi2/TrueFiCreditOracle.test.ts
+++ test/truefi2/TrueFiCreditOracle.test.ts
@@ -11,14 +11,34 @@ use(solidity)
 
 describe('TrueFiCreditOracle', () => {
   let owner: Wallet
+  let manager: Wallet
   let firstAccount: Wallet
   let oracle: TrueFiCreditOracle
 
   beforeEachWithFixture(async (wallets) => {
-    ([owner, firstAccount] = wallets)
+    ([owner, manager, firstAccount] = wallets)
 
     oracle = await new TrueFiCreditOracle__factory(owner).deploy()
     await oracle.initialize()
+    await oracle.setManager(manager.address)
+  })
+
+  describe('setManager', () => {
+    it('only owner can set manager', async () => {
+      await expect(oracle.connect(manager).setManager(manager.address))
+        .to.be.revertedWith('Ownable: caller is not the owner')
+    })
+
+    it('sets manager address', async () => {
+      await oracle.setManager(owner.address)
+      expect(await oracle.manager()).to.eq(owner.address)
+    })
+
+    it('emits event', async () => {
+      await expect(oracle.setManager(owner.address))
+        .to.emit(oracle, 'ManagerChanged')
+        .withArgs(owner.address)
+    })
   })
 
   describe('set and get credit scores', () => {
@@ -26,20 +46,20 @@ describe('TrueFiCreditOracle', () => {
     const secondScore = 200
 
     beforeEach(async () => {
-      await oracle.connect(owner).setScore(firstAccount.address, firstScore)
+      await oracle.connect(manager).setScore(firstAccount.address, firstScore)
+    })
+
+    it('only manager can set scores', async () => {
+      await expect(oracle.connect(owner).setScore(firstAccount.address, firstScore))
+        .to.be.revertedWith('TrueFiCreditOracle: Caller is not the manager')
     })
 
     it('score is set correctly for account', async () => {
       expect(await oracle.getScore(firstAccount.address)).to.equal(firstScore)
     })
 
-    it('cannot set from non-owner account', async () => {
-      await expect(oracle.connect(firstAccount).setScore(firstAccount.address, 1))
-        .to.be.revertedWith('Ownable: caller is not the owner')
-    })
-
     it('change existing score', async () => {
-      await oracle.setScore(firstAccount.address, secondScore)
+      await oracle.connect(manager).setScore(firstAccount.address, secondScore)
       expect(await oracle.getScore(firstAccount.address)).to.equal(secondScore)
     })
   })
