7b7db0aaa7710235b31f3a98f88a4a2c836200b6
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-07-09 18:33:44 +0200

    Update claimable interest in TrueCreditLine

--- contracts/truefi2/TrueCreditLine.sol
+++ contracts/truefi2/TrueCreditLine.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.10;
-
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ITrueFiPool2} from "./interface/ITrueFiPool2.sol";
 
 import {ERC20} from "../common/UpgradeableERC20.sol";
 
@@ -12,9 +13,26 @@ contract TrueCreditLine is ERC20 {
     using SafeERC20 for ERC20;
 
     address public borrower;
-    address public pool;
+    ITrueFiPool2 public pool;
 
     uint256 public principalDebt;
+
+    uint256 unpaidAccumulatedInterest;
+
+    // track overall cumulative rewards
+    uint256 public cumulativeTotalRewards;
+    /**
+     * track previous cumulative rewards for accounts
+     * @notice this is not cumulated reward for address,
+     * it is total cumulative total reward written, last time an update has been made for that address
+     */
+    mapping(address => uint256) public previousCumulatedRewards;
+
+    // track claimable rewards for accounts
+    mapping(address => uint256) public claimableRewards;
+
+    uint256 public totalInterestRewards;
+    uint256 public totalClaimedRewards;
 
     /**
      * @dev Create Credit Line
@@ -24,7 +42,7 @@ contract TrueCreditLine is ERC20 {
      */
     constructor(
         address _borrower,
-        address _pool,
+        ITrueFiPool2 _pool,
         uint256 _principalDebt
     ) public {
         ERC20.__ERC20_initialize("TrueFi Credit Line", "tfCL");
@@ -33,7 +51,50 @@ contract TrueCreditLine is ERC20 {
         pool = _pool;
 
         principalDebt = _principalDebt;
-        _mint(_pool, _principalDebt);
+        _mint(address(_pool), _principalDebt);
+    }
+
+    /**
+     * @dev Update state of rewards
+     */
+    modifier update(address account) {
+        // calculate total rewards
+        uint256 newTotalInterestRewards = token().balanceOf(address(this)).add(totalClaimedRewards);
+        // calculate new reward
+        uint256 totalNewRewards = newTotalInterestRewards.sub(totalInterestRewards);
+        // update interest rewards
+        totalInterestRewards = newTotalInterestRewards;
+        cumulativeTotalRewards = cumulativeTotalRewards.add(totalNewRewards);
+        // update claimable reward for sender
+        claimableRewards[account] = claimableRewards[account].add(
+            balanceOf(account).mul(cumulativeTotalRewards.sub(previousCumulatedRewards[account])).div(totalSupply())
+        );
+        // update previous cumulative for sender
+        previousCumulatedRewards[account] = cumulativeTotalRewards;
+        _;
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override update(msg.sender) update(recipient) returns (bool) {
+        super.transfer(recipient, amount);
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override update(sender) update(recipient) returns (bool) {
+        super.transferFrom(sender, recipient, amount);
+    }
+
+    /**
+     * @dev Function used by the borrower to pay periodic interest
+     */
+    function payInterest(uint256 amount) external update(address(pool)) {
+        token().safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function token() public view returns (ERC20) {
+        return pool.token();
     }
 
     function version() external pure returns (uint8) {
--- test/truefi2/TrueCreditLine.test.ts
+++ test/truefi2/TrueCreditLine.test.ts
@@ -1,7 +1,8 @@
 import {
   TrueFiPool2,
-  TrueCreditLine,
-  TrueCreditLine__factory,
+  TestTrueCreditLine,
+  TestTrueCreditLine__factory,
+  MockTrueCurrency,
 } from 'contracts'
 import { expect, use } from 'chai'
 import { deployContract, solidity } from 'ethereum-waffle'
@@ -13,21 +14,27 @@ use(solidity)
 
 describe('TrueCreditLine', () => {
   let owner: Wallet
+  let borrower: Wallet
+  let holder: Wallet
   let pool: TrueFiPool2
-  let creditLine: TrueCreditLine
+  let token: MockTrueCurrency
+  let creditLine: TestTrueCreditLine
 
   beforeEachWithFixture(async (wallets) => {
-    [owner] = wallets
+    [owner, borrower, holder] = wallets
 
-    ;({ standardPool: pool } = await setupTruefi2(owner))
-    creditLine = await deployContract(owner, TrueCreditLine__factory, [owner.address, pool.address, parseEth(1000)])
+    ;({ standardPool: pool, standardToken: token } = await setupTruefi2(owner))
+    creditLine = await deployContract(owner, TestTrueCreditLine__factory, [owner.address, pool.address, parseEth(1000)])
+
+    await token.mint(borrower.address, parseEth(1000))
+    await token.connect(borrower).approve(creditLine.address, parseEth(1000))
   })
 
   describe('on creation', () => {
     it('sets borrower', async () => {
       expect(await creditLine.borrower()).to.eq(owner.address)
     })
-  
+
     it('sets pool', async () => {
       expect(await creditLine.pool()).to.eq(pool.address)
     })
@@ -35,13 +42,104 @@ describe('TrueCreditLine', () => {
     it('sets principal debt', async () => {
       expect(await creditLine.principalDebt()).to.eq(parseEth(1000))
     })
-  
+
     it('mints tokens to the pool', async () => {
       expect(await creditLine.balanceOf(pool.address)).to.eq(parseEth(1000))
     })
-  
+
     it('returns correct version', async () => {
       expect(await creditLine.version()).to.eq(0)
+    })
+  })
+
+  describe('updating rewards', () => {
+    const fullReward = parseEth(1)
+    const splitReward = fullReward.div(2)
+
+    describe('only pool holds CreditLine tokens', () => {
+      it('initially 0 rewards', async () => {
+        expect(await creditLine.cumulativeTotalRewards()).to.eq(0)
+        expect(await creditLine.previousCumulatedRewards(pool.address)).to.eq(0)
+        expect(await creditLine.claimableRewards(pool.address)).to.eq(0)
+        expect(await creditLine.totalInterestRewards()).to.eq(0)
+        expect(await creditLine.totalClaimedRewards()).to.eq(0)
+      })
+
+      it('updates rewards after payInterest', async () => {
+        await creditLine.connect(borrower).payInterest(fullReward)
+
+        expect(await creditLine.cumulativeTotalRewards()).to.eq(0)
+        expect(await creditLine.previousCumulatedRewards(pool.address)).to.eq(0)
+        expect(await creditLine.claimableRewards(pool.address)).to.eq(0)
+        expect(await creditLine.totalInterestRewards()).to.eq(0)
+        expect(await creditLine.totalClaimedRewards()).to.eq(0)
+
+        // second call just to update
+        await creditLine.connect(borrower).payInterest(parseEth(0))
+
+        expect(await creditLine.cumulativeTotalRewards()).to.eq(fullReward)
+        expect(await creditLine.previousCumulatedRewards(pool.address)).to.eq(fullReward)
+        expect(await creditLine.claimableRewards(pool.address)).to.eq(fullReward)
+        expect(await creditLine.totalInterestRewards()).to.eq(fullReward)
+        expect(await creditLine.totalClaimedRewards()).to.eq(0)
+      })
+    })
+
+    describe('two holders of CreditLine', () => {
+      beforeEach(async () => {
+        await creditLine.mint(holder.address, parseEth(1000))
+        await creditLine.connect(borrower).payInterest(fullReward)
+      })
+
+      it('updates rewards after payInterest', async () => {
+        expect(await creditLine.cumulativeTotalRewards()).to.eq(0)
+        expect(await creditLine.previousCumulatedRewards(pool.address)).to.eq(0)
+        expect(await creditLine.claimableRewards(pool.address)).to.eq(0)
+        expect(await creditLine.totalInterestRewards()).to.eq(0)
+        expect(await creditLine.totalClaimedRewards()).to.eq(0)
+
+        expect(await creditLine.claimableRewards(holder.address)).to.eq(0)
+
+        // second call just to update
+        await creditLine.connect(borrower).payInterest(parseEth(0))
+
+        expect(await creditLine.cumulativeTotalRewards()).to.eq(fullReward)
+        expect(await creditLine.previousCumulatedRewards(pool.address)).to.eq(fullReward)
+        expect(await creditLine.claimableRewards(pool.address)).to.eq(splitReward)
+        expect(await creditLine.totalInterestRewards()).to.eq(fullReward)
+        expect(await creditLine.totalClaimedRewards()).to.eq(0)
+
+        expect(await creditLine.claimableRewards(holder.address)).to.eq(0)
+      })
+
+      it('updates holders rewards after transfer', async () => {
+        await creditLine.connect(holder).transfer(owner.address, parseEth(1000))
+
+        expect(await creditLine.claimableRewards(holder.address)).to.eq(splitReward)
+        expect(await creditLine.claimableRewards(owner.address)).to.eq(0)
+
+        await creditLine.connect(borrower).payInterest(fullReward)
+        await creditLine.connect(owner).transfer(holder.address, parseEth(1000))
+
+        expect(await creditLine.claimableRewards(holder.address)).to.eq(splitReward)
+        expect(await creditLine.claimableRewards(owner.address)).to.eq(splitReward)
+      })
+
+      it('updates holders rewards after transferFrom', async () => {
+        await creditLine.connect(holder).approve(owner.address, parseEth(1000))
+        await creditLine.connect(owner).approve(holder.address, parseEth(1000))
+
+        await creditLine.connect(owner).transferFrom(holder.address, owner.address, parseEth(1000))
+
+        expect(await creditLine.claimableRewards(holder.address)).to.eq(splitReward)
+        expect(await creditLine.claimableRewards(owner.address)).to.eq(0)
+
+        await creditLine.connect(borrower).payInterest(fullReward)
+        await creditLine.connect(holder).transferFrom(owner.address, holder.address, parseEth(1000))
+
+        expect(await creditLine.claimableRewards(holder.address)).to.eq(splitReward)
+        expect(await creditLine.claimableRewards(owner.address)).to.eq(splitReward)
+      })
     })
   })
 })
