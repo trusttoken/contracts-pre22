e21d39eb7b9094ddff46c79c86804d5a413d0bdf
Ivan Rukhavets <ivanruch@gmail.com>
2021-07-09 13:06:38 +0200

    Calculate credit score rate adjustment

--- contracts/truefi2/TrueCreditAgency.sol
+++ contracts/truefi2/TrueCreditAgency.sol
@@ -3,11 +3,15 @@ pragma solidity 0.6.10;
 
 import {ITrueFiPool2} from "./interface/ITrueFiPool2.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import {ERC20, IERC20} from "../common/UpgradeableERC20.sol";
+import {ERC20, IERC20, SafeMath} from "../common/UpgradeableERC20.sol";
 import {UpgradeableClaimable} from "../common/UpgradeableClaimable.sol";
+import {ITrueFiCreditOracle} from "./interface/ITrueFiCreditOracle.sol";
 
 contract TrueCreditAgency is UpgradeableClaimable {
     using SafeERC20 for ERC20;
+    using SafeMath for uint256;
+
+    uint8 MAX_CREDIT_SCORE = 255;
 
     // ================ WARNING ==================
     // ===== THIS CONTRACT IS INITIALIZABLE ======
@@ -22,6 +26,11 @@ contract TrueCreditAgency is UpgradeableClaimable {
     // basis precision: 10000 = 100%
     uint256 public riskPremium;
 
+    // basis precision: 10000 = 100%
+    uint256 public creditAdjustmentCoefficient;
+
+    ITrueFiCreditOracle public creditOracle;
+
     // ======= STORAGE DECLARATION END ============
 
     event RiskPremiumChanged(uint256 newRate);
@@ -30,9 +39,11 @@ contract TrueCreditAgency is UpgradeableClaimable {
 
     event PoolAllowed(ITrueFiPool2 pool, bool isAllowed);
 
-    function initialize(uint256 _riskPremium) public initializer {
+    function initialize(ITrueFiCreditOracle _creditOracle, uint256 _riskPremium) public initializer {
         UpgradeableClaimable.initialize(msg.sender);
         riskPremium = _riskPremium;
+        creditOracle = _creditOracle;
+        creditAdjustmentCoefficient = 1000;
     }
 
     function setRiskPremium(uint256 newRate) external onlyOwner {
@@ -53,6 +64,14 @@ contract TrueCreditAgency is UpgradeableClaimable {
     function allowPool(ITrueFiPool2 pool, bool isAllowed) external onlyOwner {
         isPoolAllowed[pool] = isAllowed;
         emit PoolAllowed(pool, isAllowed);
+    }
+
+    function creditScoreAdjustmentRate(address borrower) public view returns (uint256) {
+        uint8 score = creditOracle.getScore(borrower);
+        if (score == 0) {
+            return type(uint256).max;
+        }
+        return creditAdjustmentCoefficient.mul(MAX_CREDIT_SCORE - score).div(score);
     }
 
     function borrow(ITrueFiPool2 pool, uint256 amount) external onlyAllowedBorrowers {
