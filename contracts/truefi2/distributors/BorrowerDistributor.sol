33d52ee404026ada572db83ea9a74f90628e8e6f
jpalmer-trust <joshua.palmer@trusttoken.com>
2021-07-12 13:34:25 -0400

    Update BorrowerDistributor.sol

--- contracts/truefi2/distributors/BorrowerDistributor.sol
+++ contracts/truefi2/distributors/BorrowerDistributor.sol
@@ -4,13 +4,14 @@ pragma solidity 0.6.10;
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {ITrueLender2} from "../interface/ITrueLender2.sol";
 import {UpgradeableClaimable} from "../../common/UpgradeableClaimable.sol";
 
 /**
- * @title BorrowerRewardDistributor
+ * @title BorrowerDistributor
  * @dev Reward borrowers for taking out loans in TRU
  */
-contract BorrowerRewardDistributor is UpgradeableClaimable {
+contract BorrowerDistributor is UpgradeableClaimable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
@@ -26,39 +27,48 @@ contract BorrowerRewardDistributor is UpgradeableClaimable {
     IERC20 public rewardCurrency;
 
     /**
-     * @notice Only lenders can adjust the reward rate and distribute the reward
+     * @notice Only lenders can distribute the reward
      */
-    address public lender;
+    ITrueLender2 public lender;
 
     /**
-     * @dev Reward rate is set as 1% by default. 2 decimals
+     * @dev Reward rate is set as 1% by default(100). 2 decimals
      * so the actual reward can be calculated as: loan * rewardRate / 10000
      */
-    uint256 public rewardRate = 100;
+    uint256 public rewardRate;
+
+    // ======= STORAGE DECLARATION END ============
+
+    uint256 public constant BASIS_RATIO = 10000;
 
     modifier onlyLender() {
         // Modifier
-        require(msg.sender == lender, "Only lender can call this.");
+        require(msg.sender == address(lender), "Only lender can call this.");
         _;
     }
 
     /** @notice setting TRU address
      */
-    function initialize(IERC20 _rewardCurrency, address _lender) external initializer {
+    function initialize(
+        IERC20 _rewardCurrency,
+        ITrueLender2 _lender,
+        uint256 _rewardRate
+    ) external initializer {
         rewardCurrency = _rewardCurrency;
         lender = _lender;
+        rewardRate = _rewardRate;
     }
 
     /** @notice setting reward rate
      */
-    function setRewardRate(uint256 _rewardRate) external onlyLender {
+    function setRewardRate(uint256 _rewardRate) external onlyOwner {
         rewardRate = _rewardRate;
     }
 
     /** @notice Distribute reward
      */
     function distribute(address borrower, uint256 loanAmount) external onlyLender {
-        uint256 rewardAmount = loanAmount.mul(rewardRate).div(10000);
+        uint256 rewardAmount = loanAmount.mul(rewardRate).div(BASIS_RATIO);
         rewardCurrency.safeTransfer(borrower, rewardAmount);
     }
 }
