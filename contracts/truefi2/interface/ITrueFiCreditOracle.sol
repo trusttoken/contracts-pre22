e6d8e1da603b6300698e41b7f232fa16b1114388
Ivan Rukhavets <ivanruch@gmail.com>
2021-07-12 16:13:22 +0200

    Store borrow limits in CreditOracle

--- contracts/truefi2/interface/ITrueFiCreditOracle.sol
+++ contracts/truefi2/interface/ITrueFiCreditOracle.sol
@@ -4,5 +4,7 @@ pragma solidity 0.6.10;
 interface ITrueFiCreditOracle {
     function getScore(address account) external view returns (uint8);
 
+    function getBorrowLimit(address account) external view returns (uint256);
+
     function getMaxBorrowerLimit(address account) external view returns (uint256);
 }
