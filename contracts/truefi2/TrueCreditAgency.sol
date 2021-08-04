8001e2b3aeb4332c43aab406031669fcee6a0869
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-08-04 11:04:42 +0200

    Extract interest calculation

--- contracts/truefi2/TrueCreditAgency.sol
+++ contracts/truefi2/TrueCreditAgency.sol
@@ -369,7 +369,7 @@ contract TrueCreditAgency is UpgradeableClaimable {
             usedBucketsBitmap &= ~(uint256(1) << bucketNumber);
         }
         bucket.totalBorrowed = bucket.totalBorrowed.sub(borrowed[pool][borrower]);
-        totalBorrowerInterest = _interest(pool, bucket, borrower).add(totalPaidInterest[pool][borrower]);
+        totalBorrowerInterest = _totalBorrowerInterest(pool, bucket, borrower);
         delete bucket.savedInterest[borrower];
     }
 
@@ -386,7 +386,7 @@ contract TrueCreditAgency is UpgradeableClaimable {
         bucket.totalBorrowed = bucket.totalBorrowed.add(borrowed[pool][borrower]);
     }
 
-    function _interest(
+    function _totalBorrowerInterest(
         ITrueFiPool2 pool,
         CreditScoreBucket storage bucket,
         address borrower
@@ -405,9 +405,15 @@ contract TrueCreditAgency is UpgradeableClaimable {
                 .mul(bucket.rate)
                 .div(10000)
                 .div(365 days)
-            ).sub(
-                totalPaidInterest[pool][borrower]
             );
+    }
+
+    function _interest(
+        ITrueFiPool2 pool,
+        CreditScoreBucket storage bucket,
+        address borrower
+    ) internal view returns (uint256) {
+        return _totalBorrowerInterest(pool, bucket, borrower).sub(totalPaidInterest[pool][borrower]);
     }
 
     function _payInterestWithoutTransfer(ITrueFiPool2 pool, uint256 amount) internal {
