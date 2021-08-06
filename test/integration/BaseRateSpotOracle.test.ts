29c738eeb25ccaaf60734efb02260b4dfc760f5f
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-08-06 16:11:28 +0200

    Remove only

--- test/integration/BaseRateSpotOracle.test.ts
+++ test/integration/BaseRateSpotOracle.test.ts
@@ -5,7 +5,7 @@ import { solidity } from 'ethereum-waffle'
 
 use(solidity)
 
-describe.only('BaseRateSpotOracle', () => {
+describe('BaseRateSpotOracle', () => {
   const AAVE_LENDING_POOL = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9'
   const USDC_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
   const provider = forkChain('https://eth-mainnet.alchemyapi.io/v2/Vc3xNXIWdxEbDOToa69DhWeyhgFVBDWl', [CONTRACTS_OWNER])
