fd1df25c010d34c0d5c59d3461b107bb4dfe4770
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2021-08-10 17:02:25 +0200

    Introduce time-averaged base rate oracle (#816)
    * Make getRate() public
    
    * Refactor AaveBaseRateOracle.sol -> TimeAveragedBaseRateOracle.sol
    
    * Add test contract to TimeAveragedBaseRateOracle
    
    * Refactor AaveBaseRateOracle.test.ts -> TimeAveragedBaseRateOracle.test.ts
    
    * Revert making getRate() public
    
    * Remove no longer needed oracle instance with a shorter cooldown time
    
    * Make TimeAveragedBaseRateOracle upgradable

---
+++ contracts/truefi2/mocks/TestTimeAveragedBaseRateOracle.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import {TimeAveragedBaseRateOracle} from "../TimeAveragedBaseRateOracle.sol";
+import {SpotBaseRateOracle} from "../SpotBaseRateOracle.sol";
+
+contract TestTimeAveragedBaseRateOracle is TimeAveragedBaseRateOracle {
+    function bufferSize() public override pure returns (uint16) {
+        return 7 + 1;
+    }
+}
