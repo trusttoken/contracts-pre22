817a57122fe74d339fb3d8988b1e19dcab5ccadc
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-08-13 17:31:33 -0500

    Revert "Comment out all but CurveYearnStrategy impl"
    This reverts commit 2189443a831bdd64ebab9b251897a56f1a7a5fe9.

--- deploy/strategy.ts
+++ deploy/strategy.ts
@@ -18,53 +18,53 @@ const CURVE_MINTER = '0xd061d61a4d941c39e5453435b6345dc261c2fce0'
 const ONE_INCH_EXCHANGE = '0x11111112542d85b3ef69ae05771c2dccff4faa26'
 
 deploy({}, (_, config) => {
-  // const proxy = createProxy(OwnedUpgradeabilityProxy)
-  // const isMainnet = config.network === 'mainnet'
-  // const NETWORK = isMainnet ? 'mainnet' : 'testnet'
+  const proxy = createProxy(OwnedUpgradeabilityProxy)
+  const isMainnet = config.network === 'mainnet'
+  const NETWORK = isMainnet ? 'mainnet' : 'testnet'
 
-  // // Existing contracts
-  // const usdc = isMainnet
-  //   ? USDC
-  //   : contract(TestUSDCToken)
-  // const usdt = isMainnet
-  //   ? USDT
-  //   : contract(TestUSDTToken)
-  // const poolFactory = proxy(contract(PoolFactory), () => {})
-  // const usdc_TrueFiPool2 = poolFactory.pool(usdc)
-  // const usdt_TrueFiPool2 = poolFactory.pool(usdt)
+  // Existing contracts
+  const usdc = isMainnet
+    ? USDC
+    : contract(TestUSDCToken)
+  const usdt = isMainnet
+    ? USDT
+    : contract(TestUSDTToken)
+  const poolFactory = proxy(contract(PoolFactory), () => {})
+  const usdc_TrueFiPool2 = poolFactory.pool(usdc)
+  const usdt_TrueFiPool2 = poolFactory.pool(usdt)
 
-  // // New contract impls
+  // New contract impls
   const usdc_CurveYearnStrategy_impl = contract('usdc_CurveYearnStrategy', CurveYearnStrategy)
-  // const usdt_CurveYearnStrategy_impl = contract('usdt_CurveYearnStrategy', CurveYearnStrategy)
+  const usdt_CurveYearnStrategy_impl = contract('usdt_CurveYearnStrategy', CurveYearnStrategy)
 
-  // // New contract proxies
-  // const usdc_CurveYearnStrategy = proxy(usdc_CurveYearnStrategy_impl, () => {})
-  // const usdt_CurveYearnStrategy = proxy(usdt_CurveYearnStrategy_impl, () => {})
+  // New contract proxies
+  const usdc_CurveYearnStrategy = proxy(usdc_CurveYearnStrategy_impl, () => {})
+  const usdt_CurveYearnStrategy = proxy(usdt_CurveYearnStrategy_impl, () => {})
 
-  // // New bare contracts
-  // const crvPriceOracle = isMainnet
-  //   ? contract(CrvPriceOracle)
-  //   : contract(MockCrvPriceOracle)
-  // const usdc_MockStrategy = contract('usdc_MockStrategy', MockStrategy, [usdc, usdc_TrueFiPool2])
-  // const usdt_MockStrategy = contract('usdt_MockStrategy', MockStrategy, [usdt, usdt_TrueFiPool2])
+  // New bare contracts
+  const crvPriceOracle = isMainnet
+    ? contract(CrvPriceOracle)
+    : contract(MockCrvPriceOracle)
+  const usdc_MockStrategy = contract('usdc_MockStrategy', MockStrategy, [usdc, usdc_TrueFiPool2])
+  const usdt_MockStrategy = contract('usdt_MockStrategy', MockStrategy, [usdt, usdt_TrueFiPool2])
 
-  // // Contract initialization
-  // runIf(usdc_CurveYearnStrategy.isInitialized().not(), () => {
-  //   usdc_CurveYearnStrategy.initialize(usdc_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, 1)
+  // Contract initialization
+  runIf(usdc_CurveYearnStrategy.isInitialized().not(), () => {
+    usdc_CurveYearnStrategy.initialize(usdc_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, 1)
+  })
+  runIf(usdt_CurveYearnStrategy.isInitialized().not(), () => {
+    usdt_CurveYearnStrategy.initialize(usdt_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, 2)
+  })
+  const usdc_Strategy = isMainnet
+    ? usdc_CurveYearnStrategy
+    : usdc_MockStrategy
+  const usdt_Strategy = isMainnet
+    ? usdt_CurveYearnStrategy
+    : usdt_MockStrategy
+  // runIf(usdc_TrueFiPool2.strategy().equals(usdc_Strategy).not(), () => {
+  //   usdc_TrueFiPool2.switchStrategy(usdc_Strategy)
   // })
-  // runIf(usdt_CurveYearnStrategy.isInitialized().not(), () => {
-  //   usdt_CurveYearnStrategy.initialize(usdt_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, 2)
+  // runIf(usdt_TrueFiPool2.strategy().equals(usdt_Strategy).not(), () => {
+  //   usdt_TrueFiPool2.switchStrategy(usdt_Strategy)
   // })
-  // const usdc_Strategy = isMainnet
-  //   ? usdc_CurveYearnStrategy
-  //   : usdc_MockStrategy
-  // const usdt_Strategy = isMainnet
-  //   ? usdt_CurveYearnStrategy
-  //   : usdt_MockStrategy
-  // // runIf(usdc_TrueFiPool2.strategy().equals(usdc_Strategy).not(), () => {
-  // //   usdc_TrueFiPool2.switchStrategy(usdc_Strategy)
-  // // })
-  // // runIf(usdt_TrueFiPool2.strategy().equals(usdt_Strategy).not(), () => {
-  // //   usdt_TrueFiPool2.switchStrategy(usdt_Strategy)
-  // // })
 })
