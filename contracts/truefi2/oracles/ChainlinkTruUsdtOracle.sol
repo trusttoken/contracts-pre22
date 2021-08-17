85a56a97c350042b00e67578c492968fcfb37857
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-08-17 12:56:36 -0500

    Update USDT oracle

--- contracts/truefi2/oracles/ChainlinkTruUsdtOracle.sol
+++ contracts/truefi2/oracles/ChainlinkTruUsdtOracle.sol
@@ -4,9 +4,9 @@ pragma solidity 0.6.10;
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {ChainlinkTruOracle, IERC20WithDecimals} from "./ChainlinkTruOracle.sol";
 
-contract ChainlinkTruUsdcOracle is ChainlinkTruOracle {
+contract ChainlinkTruUsdtOracle is ChainlinkTruOracle {
     function token() public override view returns (IERC20WithDecimals) {
-        return IERC20WithDecimals(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        return IERC20WithDecimals(0xdAC17F958D2ee523a2206206994597C13D831ec7);
     }
 
     /*
