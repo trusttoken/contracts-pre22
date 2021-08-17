8af47d7e7c6dda9c4d382139b5f2467d58c7f621
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-08-17 08:33:23 -0500

    Update strategy deploy script with new tfTUSD (#868)
    * Add new tfTUSD to strategy deploy script
    
    * Update strategy deploy script to include maxPriceSlippage param

--- deploy/strategy.ts
+++ deploy/strategy.ts
@@ -4,18 +4,21 @@ import {
   CurveYearnStrategy,
   MockCrvPriceOracle,
   MockStrategy,
+  MockTrueUSD,
   OwnedUpgradeabilityProxy,
   PoolFactory,
   TestUSDCToken,
   TestUSDTToken,
 } from '../build/artifacts'
 
+const TUSD = '0x0000000000085d4780B73119b644AE5ecd22b376'
 const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
 const CURVE_GAUGE = '0xFA712EE4788C042e2B7BB55E6cb8ec569C4530c1'
 const CURVE_POOL = '0xbbc81d23ea2c3ec7e56d39296f0cbb648873a5d3'
 const CURVE_MINTER = '0xd061d61a4d941c39e5453435b6345dc261c2fce0'
 const ONE_INCH_EXCHANGE = '0x11111112542d85b3ef69ae05771c2dccff4faa26'
+const MAX_PRICE_SLIPPAGE = 75
 
 deploy({}, (_, config) => {
   const proxy = createProxy(OwnedUpgradeabilityProxy)
@@ -23,6 +26,9 @@ deploy({}, (_, config) => {
   const NETWORK = isMainnet ? 'mainnet' : 'testnet'
 
   // Existing contracts
+  const tusd = isMainnet
+    ? TUSD
+    : contract(MockTrueUSD)
   const usdc = isMainnet
     ? USDC
     : contract(TestUSDCToken)
@@ -30,14 +36,17 @@ deploy({}, (_, config) => {
     ? USDT
     : contract(TestUSDTToken)
   const poolFactory = proxy(contract(PoolFactory), () => {})
+  const tusd_TrueFiPool2 = poolFactory.pool(tusd)
   const usdc_TrueFiPool2 = poolFactory.pool(usdc)
   const usdt_TrueFiPool2 = poolFactory.pool(usdt)
 
   // New contract impls
+  const tusd_CurveYearnStrategy_impl = contract('tusd_CurveYearnStrategy', CurveYearnStrategy)
   const usdc_CurveYearnStrategy_impl = contract('usdc_CurveYearnStrategy', CurveYearnStrategy)
   const usdt_CurveYearnStrategy_impl = contract('usdt_CurveYearnStrategy', CurveYearnStrategy)
 
   // New contract proxies
+  const tusd_CurveYearnStrategy = proxy(tusd_CurveYearnStrategy_impl, () => {})
   const usdc_CurveYearnStrategy = proxy(usdc_CurveYearnStrategy_impl, () => {})
   const usdt_CurveYearnStrategy = proxy(usdt_CurveYearnStrategy_impl, () => {})
 
@@ -45,22 +54,32 @@ deploy({}, (_, config) => {
   const crvPriceOracle = isMainnet
     ? contract(CrvPriceOracle)
     : contract(MockCrvPriceOracle)
+  const tusd_MockStrategy = contract('tusd_MockStrategy', MockStrategy, [tusd, tusd_TrueFiPool2])
   const usdc_MockStrategy = contract('usdc_MockStrategy', MockStrategy, [usdc, usdc_TrueFiPool2])
   const usdt_MockStrategy = contract('usdt_MockStrategy', MockStrategy, [usdt, usdt_TrueFiPool2])
 
   // Contract initialization
+  runIf(tusd_CurveYearnStrategy.isInitialized().not(), () => {
+    tusd_CurveYearnStrategy.initialize(tusd_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, MAX_PRICE_SLIPPAGE, 3)
+  })
   runIf(usdc_CurveYearnStrategy.isInitialized().not(), () => {
-    usdc_CurveYearnStrategy.initialize(usdc_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, 1)
+    usdc_CurveYearnStrategy.initialize(usdc_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, MAX_PRICE_SLIPPAGE, 1)
   })
   runIf(usdt_CurveYearnStrategy.isInitialized().not(), () => {
-    usdt_CurveYearnStrategy.initialize(usdt_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, 2)
+    usdt_CurveYearnStrategy.initialize(usdt_TrueFiPool2, CURVE_POOL, CURVE_GAUGE, CURVE_MINTER, ONE_INCH_EXCHANGE, crvPriceOracle, MAX_PRICE_SLIPPAGE, 2)
   })
+  const tusd_Strategy = isMainnet
+    ? tusd_CurveYearnStrategy
+    : tusd_MockStrategy
   const usdc_Strategy = isMainnet
     ? usdc_CurveYearnStrategy
     : usdc_MockStrategy
   const usdt_Strategy = isMainnet
     ? usdt_CurveYearnStrategy
     : usdt_MockStrategy
+  // runIf(tusd_TrueFiPool2.strategy().equals(tusd_Strategy).not(), () => {
+  //   tusd_TrueFiPool2.switchStrategy(tusd_Strategy)
+  // })
   // runIf(usdc_TrueFiPool2.strategy().equals(usdc_Strategy).not(), () => {
   //   usdc_TrueFiPool2.switchStrategy(usdc_Strategy)
   // })
