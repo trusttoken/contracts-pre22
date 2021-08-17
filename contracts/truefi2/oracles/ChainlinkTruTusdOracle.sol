4e8e7058b57b0a525623d429706a68472f3de834
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-08-17 12:51:21 -0500

    Update TUSD oracle

--- contracts/truefi2/oracles/ChainlinkTruTusdOracle.sol
+++ contracts/truefi2/oracles/ChainlinkTruTusdOracle.sol
@@ -4,15 +4,15 @@ pragma solidity 0.6.10;
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {ChainlinkTruOracle, IERC20WithDecimals} from "./ChainlinkTruOracle.sol";
 
-contract ChainlinkTruUsdcOracle is ChainlinkTruOracle {
+contract ChainlinkTruTusdOracle is ChainlinkTruOracle {
     function token() public override view returns (IERC20WithDecimals) {
-        return IERC20WithDecimals(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        return IERC20WithDecimals(0x0000000000085d4780B73119b644AE5ecd22b376);
     }
 
     /*
-     * @dev assume price is always 1USD, convert 6 decimal places to 18
+     * @dev assume price is always 1USD, convert 18 decimal places to 18
      */
     function tokenToUsd(uint256 tokenAmount) public override view returns (uint256) {
-        return tokenAmount.mul(1e12);
+        return tokenAmount;
     }
 }
