c5f21e8a8efbb832768a4ddd8c0f054173ffbc90
Roman Moskalenko <skalermo@gmail.com>
2021-08-18 09:22:15 +0200

    Add borrowLimit() function to ITrueCreditAgency

--- contracts/truefi2/interface/ITrueCreditAgency.sol
+++ contracts/truefi2/interface/ITrueCreditAgency.sol
@@ -5,4 +5,6 @@ import {ITrueFiPool2} from "./ITrueFiPool2.sol";
 
 interface ITrueCreditAgency {
     function poolCreditValue(ITrueFiPool2 pool) external view returns (uint256);
+
+    function borrowLimit(ITrueFiPool2 pool, address borrower) external view returns (uint256);
 }
