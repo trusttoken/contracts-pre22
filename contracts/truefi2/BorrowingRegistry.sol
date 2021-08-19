a7ff33bbe6001bf30d8e3db61b1a4629dc0cd3ce
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-08-19 17:38:50 +0200

    Add canLock mapping and corresponding setter function

--- contracts/truefi2/BorrowingRegistry.sol
+++ contracts/truefi2/BorrowingRegistry.sol
@@ -4,7 +4,21 @@ pragma solidity 0.6.10;
 import {UpgradeableClaimable} from "../common/UpgradeableClaimable.sol";
 
 contract BorrowingRegistry is UpgradeableClaimable {
+    // ================ WARNING ==================
+    // ===== THIS CONTRACT IS INITIALIZABLE ======
+    // === STORAGE VARIABLES ARE DECLARED BELOW ==
+    // REMOVAL OR REORDER OF VARIABLES WILL RESULT
+    // ========= IN STORAGE CORRUPTION ===========
+
+    mapping(address => bool) public canLock;
+
+    // ======= STORAGE DECLARATION END ===========
+
     function initialize() external initializer {
         UpgradeableClaimable.initialize(msg.sender);
+    }
+
+    function allowLocking(address allowedAddress) external onlyOwner {
+        canLock[allowedAddress] = true;
     }
 }
--- test/truefi2/BorrowingRegistry.test.ts
+++ test/truefi2/BorrowingRegistry.test.ts
@@ -12,10 +12,11 @@ use(solidity)
 
 describe('TrueRateAdjuster', () => {
   let owner: Wallet
+  let locker: Wallet
   let registry: BorrowingRegistry
 
   beforeEachWithFixture(async (wallets) => {
-    [owner] = wallets
+    [owner, locker] = wallets
     const deployContract = setupDeploy(owner)
     registry = await deployContract(BorrowingRegistry__factory)
     await registry.initialize()
@@ -24,6 +25,22 @@ describe('TrueRateAdjuster', () => {
   describe('initializer', () => {
     it('sets owner', async () => {
       expect(await registry.owner()).to.eq(owner.address)
+    })
+  })
+
+  describe('allowLocking', () => {
+    it('only owner can call', async () => {
+      await expect(registry.connect(locker).allowLocking(locker.address))
+        .to.be.revertedWith('Ownable: caller is not the owner')
+
+      await expect(registry.connect(owner).allowLocking(locker.address))
+        .not.to.be.reverted
+    })
+
+    it('changes allowance status', async () => {
+      expect(await registry.canLock(locker.address)).to.eq(false)
+      await registry.allowLocking(locker.address)
+      expect(await registry.canLock(locker.address)).to.eq(true)
     })
   })
 })
