1254b6538c4de7aa042f719825df3c0e645b41e3
MiksuJak <mikolaj.jakubowski@protonmail.com>
2021-08-23 12:04:50 +0200

    Rename registry to mutex in tests

--- test/truefi2/BorrowingMutex.test.ts
+++ test/truefi2/BorrowingMutex.test.ts
@@ -15,88 +15,88 @@ describe('BorrowingMutex', () => {
   let owner: Wallet
   let locker: Wallet
   let borrower: Wallet
-  let registry: BorrowingMutex
+  let mutex: BorrowingMutex
 
   beforeEachWithFixture(async (wallets) => {
     [owner, locker, borrower] = wallets
     const deployContract = setupDeploy(owner)
-    registry = await deployContract(BorrowingMutex__factory)
-    await registry.initialize()
-    await registry.allowLocker(locker.address, true)
+    mutex = await deployContract(BorrowingMutex__factory)
+    await mutex.initialize()
+    await mutex.allowLocker(locker.address, true)
   })
 
   describe('initializer', () => {
     it('sets owner', async () => {
-      expect(await registry.owner()).to.eq(owner.address)
+      expect(await mutex.owner()).to.eq(owner.address)
     })
   })
 
   describe('allowLocker', () => {
     it('only owner can call', async () => {
-      await expect(registry.connect(locker).allowLocker(locker.address, true))
+      await expect(mutex.connect(locker).allowLocker(locker.address, true))
         .to.be.revertedWith('Ownable: caller is not the owner')
 
-      await expect(registry.connect(owner).allowLocker(locker.address, true))
+      await expect(mutex.connect(owner).allowLocker(locker.address, true))
         .not.to.be.reverted
     })
 
     it('changes allowance status', async () => {
-      expect(await registry.canLock(owner.address)).to.eq(false)
+      expect(await mutex.canLock(owner.address)).to.eq(false)
 
-      await registry.allowLocker(owner.address, true)
-      expect(await registry.canLock(owner.address)).to.eq(true)
+      await mutex.allowLocker(owner.address, true)
+      expect(await mutex.canLock(owner.address)).to.eq(true)
 
-      await registry.allowLocker(owner.address, false)
-      expect(await registry.canLock(owner.address)).to.eq(false)
+      await mutex.allowLocker(owner.address, false)
+      expect(await mutex.canLock(owner.address)).to.eq(false)
     })
   })
 
   describe('lock', () => {
     it('sender not in canLock', async () => {
-      await expect(registry.connect(owner).lock(borrower.address, owner.address))
+      await expect(mutex.connect(owner).lock(borrower.address, owner.address))
         .to.be.revertedWith('BorrowingMutex: Sender is not allowed to lock borrowers')
 
-      await expect(registry.connect(locker).lock(borrower.address, owner.address))
+      await expect(mutex.connect(locker).lock(borrower.address, owner.address))
         .not.to.be.reverted
     })
 
     it('changes locker', async () => {
-      await registry.connect(locker).lock(borrower.address, owner.address)
-      expect(await registry.locker(borrower.address)).to.eq(owner.address)
+      await mutex.connect(locker).lock(borrower.address, owner.address)
+      expect(await mutex.locker(borrower.address)).to.eq(owner.address)
     })
 
     it('emits event', async () => {
-      await expect(registry.connect(locker).lock(borrower.address, owner.address))
-        .to.emit(registry, 'BorrowerLocked')
+      await expect(mutex.connect(locker).lock(borrower.address, owner.address))
+        .to.emit(mutex, 'BorrowerLocked')
         .withArgs(borrower.address, owner.address)
     })
   })
 
   describe('unlock', () => {
     beforeEach(async () => {
-      await registry.connect(locker).lock(borrower.address, owner.address)
+      await mutex.connect(locker).lock(borrower.address, owner.address)
     })
 
     it('reverts if other caller tries to unlock', async () => {
-      await expect(registry.connect(borrower).unlock(borrower.address))
+      await expect(mutex.connect(borrower).unlock(borrower.address))
         .to.be.revertedWith('BorrowingMutex: Only locker can unlock')
 
-      await expect(registry.connect(locker).unlock(borrower.address))
+      await expect(mutex.connect(locker).unlock(borrower.address))
         .to.be.revertedWith('BorrowingMutex: Only locker can unlock')
 
-      await expect(registry.connect(owner).unlock(borrower.address))
+      await expect(mutex.connect(owner).unlock(borrower.address))
         .not.to.be.reverted
     })
 
     it('locker unlocks', async () => {
-      expect(await registry.locker(borrower.address)).to.eq(owner.address)
-      await registry.connect(owner).unlock(borrower.address)
-      expect(await registry.locker(borrower.address)).to.eq(AddressZero)
+      expect(await mutex.locker(borrower.address)).to.eq(owner.address)
+      await mutex.connect(owner).unlock(borrower.address)
+      expect(await mutex.locker(borrower.address)).to.eq(AddressZero)
     })
 
     it('emits event', async () => {
-      await expect(registry.connect(owner).unlock(borrower.address))
-        .to.emit(registry, 'BorrowerUnlocked')
+      await expect(mutex.connect(owner).unlock(borrower.address))
+        .to.emit(mutex, 'BorrowerUnlocked')
         .withArgs(borrower.address, owner.address)
     })
   })
