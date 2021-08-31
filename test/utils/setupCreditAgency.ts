81b72f59a002d607ca35dccd184930c79b423c34
Yuchen Lin <yuchen.lin@trusttoken.com>
2021-08-30 22:22:11 -0700

    Fix typecheck

--- test/utils/setupCreditAgency.ts
+++ test/utils/setupCreditAgency.ts
@@ -1,6 +1,7 @@
 import { deployMockContract } from 'ethereum-waffle'
 import { Wallet } from 'ethers'
 import {
+  BorrowingMutex__factory,
   TestTrueFiPool,
   TrueFiPool2,
   TrueCreditAgency__factory,
@@ -10,13 +11,14 @@ import {
 import { TimeAveragedBaseRateOracleJson } from 'build'
 
 export const setupCreditAgency = async (owner: Wallet, pool: TrueFiPool2 | TestTrueFiPool) => {
+  const borrowingMutex = await new BorrowingMutex__factory(owner).deploy()
   const creditAgency = await new TrueCreditAgency__factory(owner).deploy()
   const rateAdjuster = await new TrueRateAdjuster__factory(owner).deploy()
   const creditOracle = await new TrueFiCreditOracle__factory(owner).deploy()
   const mockBaseRateOracle = await deployMockContract(owner, TimeAveragedBaseRateOracleJson.abi)
   await mockBaseRateOracle.mock.getWeeklyAPY.returns(300)
 
-  await creditAgency.initialize(creditOracle.address, rateAdjuster.address)
+  await creditAgency.initialize(creditOracle.address, rateAdjuster.address, borrowingMutex.address)
   await rateAdjuster.initialize()
   await creditOracle.initialize()
 
