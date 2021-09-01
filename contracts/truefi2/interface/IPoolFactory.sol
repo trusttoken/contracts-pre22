d51317e63d368e6cc16275e97750d4235ab6b085
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-09-01 04:42:41 -0700

    Add array of TrueFi supported pools to PoolFactory (#906)
    * Add array of TrueFi supported pools to PoolFactory
    
    * Fix build
    
    * Redo fix changes
    
    * Undo spurious change vs dev from d6030ff
    
    Co-authored-by: Dymitr Lubczyk <dymitrlubczyk@Dymitrs-MacBook-Pro.local>

--- contracts/truefi2/interface/IPoolFactory.sol
+++ contracts/truefi2/interface/IPoolFactory.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.10;
 
+import {ITrueFiPool2} from "./ITrueFiPool2.sol";
+
 interface IPoolFactory {
     function isPool(address pool) external view returns (bool);
+
+    function getSupportedPools() external view returns (ITrueFiPool2[] memory);
 }
