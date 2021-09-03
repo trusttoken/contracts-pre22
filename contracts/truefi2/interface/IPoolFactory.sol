50a957184ddca40cd42b41bd07920ee7935ceeef
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-09-03 01:32:15 -0700

    Add isSupportedPool() view to PoolFactory (#914)

--- contracts/truefi2/interface/IPoolFactory.sol
+++ contracts/truefi2/interface/IPoolFactory.sol
@@ -6,5 +6,7 @@ import {ITrueFiPool2} from "./ITrueFiPool2.sol";
 interface IPoolFactory {
     function isPool(address pool) external view returns (bool);
 
+    function isSupportedPool(ITrueFiPool2 pool) external view returns (bool);
+
     function getSupportedPools() external view returns (ITrueFiPool2[] memory);
 }
