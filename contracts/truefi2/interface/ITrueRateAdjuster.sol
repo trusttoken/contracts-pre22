abb32f0b1427640c7a1c75e3f8592aa2fd3a0f19
yuchenlintt <80530881+yuchenlintt@users.noreply.github.com>
2021-09-07 01:19:40 -0600

    Remove unused PF.isPool() and TRA.getTVLPools() interface functions (#936)
    * Remove unused PF.isPool() and TRA.getTVLPools() interface functions
    
    * Remove getTVLPools() mock from TrueLender2 integration test

--- contracts/truefi2/interface/ITrueRateAdjuster.sol
+++ contracts/truefi2/interface/ITrueRateAdjuster.sol
@@ -4,8 +4,6 @@ pragma solidity 0.6.10;
 import {ITrueFiPool2} from "./ITrueFiPool2.sol";
 
 interface ITrueRateAdjuster {
-    function getTVLPools() external view returns (ITrueFiPool2[] memory);
-
     function rate(
         ITrueFiPool2 pool,
         uint8 score,
