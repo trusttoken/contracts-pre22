b6aa9e81d887c03dc6ed58ce598e374db137c079
Roman Moskalenko <skalermo@gmail.com>
2021-10-01 10:44:42 +0200

    Remove unused totalBorrowed() function

--- contracts/truefi2/TrueLender2.sol
+++ contracts/truefi2/TrueLender2.sol
@@ -202,33 +202,6 @@ contract TrueLender2 is ITrueLender2, UpgradeableClaimable {
     }
 
     /**
-     * @dev Get total amount borrowed for `borrower` from fixed term loans in USD
-     * @param borrower Borrower to get amount borrowed for
-     * @param decimals Precision to use when calculating total borrowed
-     * @return Total amount borrowed for `borrower` in USD
-     */
-    function totalBorrowed(address borrower, uint8 decimals) public view returns (uint256) {
-        uint256 borrowSum;
-        uint256 resultPrecision = uint256(10)**decimals;
-
-        // loop through loans and sum amount borrowed accounting for precision
-        ITrueFiPool2[] memory pools = factory.getSupportedPools();
-        for (uint256 i = 0; i < pools.length; i++) {
-            ITrueFiPool2 pool = pools[i];
-            uint256 poolPrecision = uint256(10)**ITrueFiPool2WithDecimals(address(pool)).decimals();
-            ILoanToken2[] memory _loans = poolLoans[pool];
-            for (uint256 j = 0; j < _loans.length; j++) {
-                ILoanToken2 loan = _loans[j];
-                if (address(loan.borrower()) == borrower) {
-                    uint256 loanValue = loan.value(loan.balanceOf(address(this)));
-                    borrowSum = borrowSum.add(loanValue.mul(resultPrecision).div(poolPrecision));
-                }
-            }
-        }
-        return borrowSum;
-    }
-
-    /**
      * @dev Helper function to redeem funds from `loanToken` and repay them into the `pool`
      * @param loanToken Loan to reclaim capital from
      * @param pool Pool from which the loan was funded
