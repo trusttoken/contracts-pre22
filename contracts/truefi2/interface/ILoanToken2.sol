48942abb8645a906e5fe3ff06ba4cef8d3e6298c
skalermo <skalermo@gmail.com>
2021-10-27 18:33:25 +0200

    Remove unused import

--- contracts/truefi2/interface/ILoanToken2.sol
+++ contracts/truefi2/interface/ILoanToken2.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.6.10;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ERC20} from "../../common/UpgradeableERC20.sol";
 import {ITrueFiPool2} from "./ITrueFiPool2.sol";
 
 interface ILoanToken2 is IERC20 {
