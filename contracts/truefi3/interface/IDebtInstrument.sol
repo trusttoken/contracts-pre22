10a72b03ec826efd8afa8257243dc573f4106000
Hal Hyatt <feewet@gmail.com>
2021-11-04 13:27:26 -0700

    lint

--- contracts/truefi3/interface/IDebtInstrument.sol
+++ contracts/truefi3/interface/IDebtInstrument.sol
@@ -7,51 +7,51 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 /**
  * @title IDebtInstrument
  * @notice Represents debt in the TrueFi protocol
- * @dev This is the basic interface for representing Debt in TrueFi.
+ * @dev This is the basic interface for representing Debt in TrueFi
  * This interface is meant to be extended to support different debt types
- * 
+ *
  * An NFT represents some debt where:
  *  - Each debt has an owner and a borrower
  *  - The owner mints and funds a loan
- * 	- The borrower can withdraw funds from the loan
+ *  - The borrower can withdraw funds from the loan
  *  - The borrower calls repay() to pay back the loan
- * 	- Repayments are withdrawable by the owner
- * 	- Every DebtInstrument has an underlying token
- * 	- Only principal debt is tracked
- * 
+ *  - Repayments are withdrawable by the owner
+ *  - Every DebtInstrument has an underlying token
+ *  - Only principal debt is tracked
+ *
  **/
 interface IDebtInstrument is IERC721 {
-	/// @dev The owner address lends tokens and withdraws repayments
-	function owner() external view returns (address);
+    /// @dev The owner address lends tokens and withdraws repayments
+    function owner() external view returns (address);
 
-	/// @dev The borrower address borrows and repays the loan
-	function borrower() external view returns (address);
+    /// @dev The borrower address borrows and repays the loan
+    function borrower() external view returns (address);
 
-	/// @dev The underlying token denominating the debt
-	function token() external view returns (address);
+    /// @dev The underlying token denominating the debt
+    function token() external view returns (address);
 
-	/// @dev Principal debt amount
-	function principal() external view returns (uint256);
+    /// @dev Principal debt amount
+    function principal() external view returns (uint256);
 
-	/// @dev Each debt has an expiration timestamp
-	function expiry() external view returns (uint256);
+    /// @dev Each debt has an expiration timestamp
+    function expiry() external view returns (uint256);
 
-	/// @dev Borrower calls this function to borrow `amount` of tokens
-	function borrow(uint256 amount) external;
+    /// @dev Borrower calls this function to borrow `amount` of tokens
+    function borrow(uint256 amount) external;
 
-	/// @dev Borrower calls this function to repay `amount` of tokens
-	function repay(uint256 amount) external;
+    /// @dev Borrower calls this function to repay `amount` of tokens
+    function repay(uint256 amount) external;
 
-	/// @dev Owner funds a loan for `amount` and increases principal
-	function fund(uint256 amount) external;
+    /// @dev Owner funds a loan for `amount` and increases principal
+    function fund(uint256 amount) external;
 
-	/// @dev Owner calls this function to withdraw `amount` of tokens
-	function withdraw(uint256 amount) external;
+    /// @dev Owner calls this function to withdraw `amount` of tokens
+    function withdraw(uint256 amount) external;
 
-	/// @dev Mint a new DebtInstrument
-	function mint(
-		address _borrower,
-		IERC20 _token,
-		uint256 _expiry
-	) external;
+    /// @dev Mint a new DebtInstrument
+    function mint(
+        address _borrower,
+        IERC20 _token,
+        uint256 _expiry
+    ) external;
 }
--- contracts/truefi3/interface/IDebtInstrumentWithSchedule.sol
+++ contracts/truefi3/interface/IDebtInstrumentWithSchedule.sol
@@ -7,25 +7,23 @@ import {IDebtInstrument} from "./IDebtInstrument.sol";
 /**
  * @title Debt Instrument With Schedule
  * @notice TrueFi Debt Instrument with Payment Schedule
- * @dev
- * 
- * This Debt Instrument stores an expected schedule of payment
+ * @dev This Debt Instrument stores an expected schedule of payment
  * Calling the repay() function should store time and amount of payments
  * Minting the token should pass in a schedule which is an array
  * of information about expected payment times and amounts
  **/
 interface IDebtInstrumentWithSchedule is IDebtInstrument {
-	// store payment and schedule data
-	struct Payment {
-		// time of payment
-		uint256 timestamp;
-		// payment amount
-		uint256 amount;
-	}
+    // store payment and schedule data
+    struct Payment {
+        // time of payment
+        uint256 timestamp;
+        // payment amount
+        uint256 amount;
+    }
 
-	/// @dev Get expected schedule of payments
-	function schedule() external view returns (Payment[] memory);
+    /// @dev Get expected schedule of payments
+    function schedule() external view returns (Payment[] memory);
 
-	/// @dev Get history of actual payments
-	function payments() external view returns (Payment[] memory);
+    /// @dev Get history of actual payments
+    function payments() external view returns (Payment[] memory);
 }
