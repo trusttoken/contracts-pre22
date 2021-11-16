799c72fcd0990024cf8636a0796e10f7b45fb624
MaciejNadolski98 <maciejnadolski98@gmail.com>
2021-11-16 11:18:42 +0100

    Add extra line

--- spec/truefi2/scripts/FixedTermLoan.sh
+++ spec/truefi2/scripts/FixedTermLoan.sh
@@ -1,8 +1,8 @@
 certoraRun contracts/truefi2/FixedTermLoan.sol \
-  contracts/truefi2/FixedTermLoanAgency.sol \
   contracts/truefi2/BorrowingMutex.sol \
   contracts/common/UpgradeableERC20.sol:ERC20 \
   --verify FixedTermLoan:spec/truefi2/FixedTermLoan.spec \
   --link FixedTermLoan:token=ERC20 \
   --short_output \
   --optimistic_loop
+  
