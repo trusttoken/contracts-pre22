f89c9c44939a1a50c1f5782f66a9b3d03d5445f6
Shadi Barhoumi <shadi.barhoumi@trusttoken.com>
2021-11-23 10:33:37 +0100

    Add LP token

--- contracts/ragnarok/ManagedPortfolio.sol
+++ contracts/ragnarok/ManagedPortfolio.sol
@@ -2,23 +2,29 @@
 pragma solidity 0.8.10;
 
 import {IERC20} from "@openzeppelin/contracts4/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts4/token/ERC20/ERC20.sol";
 import {IERC721} from "@openzeppelin/contracts4/token/ERC721/IERC721.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts4/token/ERC721/IERC721Receiver.sol";
 import {BulletLoans} from "./BulletLoans.sol";
 
-contract ManagedPortfolio is IERC721Receiver {
-    IERC20 underlyingToken;
+interface IERC20WithDecimals is IERC20 {
+    function decimals() external view returns (uint256);
+}
+
+contract ManagedPortfolio is IERC721Receiver, ERC20 {
+    IERC20WithDecimals underlyingToken;
     BulletLoans bulletLoans;
 
     event BulletLoanCreated(uint256 id);
 
-    constructor(IERC20 _underlyingToken, BulletLoans _bulletLoans) {
+    constructor(IERC20WithDecimals _underlyingToken, BulletLoans _bulletLoans) ERC20("ManagerPortfolio", "MPS") {
         underlyingToken = _underlyingToken;
         bulletLoans = _bulletLoans;
     }
 
     function join(uint256 amount) external {
         underlyingToken.transferFrom(msg.sender, address(this), amount);
+        _mint(msg.sender, (amount * 10**decimals()) / (10**underlyingToken.decimals()));
     }
 
     function withdraw(uint256 amount) external {
