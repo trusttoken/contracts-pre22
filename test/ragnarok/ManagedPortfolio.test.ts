ffb4735fbc3ca01bb2d8474ea18c916a99eaf8f1
barrutko <barrutko.eth@gmail.com>
2021-11-23 12:00:22 +0100

    Compute a proper amount of lp-tokens when joining

--- test/ragnarok/ManagedPortfolio.test.ts
+++ test/ragnarok/ManagedPortfolio.test.ts
@@ -24,12 +24,13 @@ describe('ManagedPortfolio', () => {
   let portfolioOwner: Wallet
   let lender: Wallet
   let lender2: Wallet
+  let lender3: Wallet
   let borrower: Wallet
 
   const parseShares = parseEth
 
   beforeEachWithFixture(async (wallets) => {
-    [portfolioOwner, lender, lender2, borrower] = wallets
+    [portfolioOwner, lender, lender2, lender3, borrower] = wallets
 
     token = await new MockUsdc__factory(portfolioOwner).deploy()
     bulletLoans = await new BulletLoans__factory(portfolioOwner).deploy()
@@ -41,8 +42,9 @@ describe('ManagedPortfolio', () => {
     portfolioAsLender = portfolio.connect(lender)
     tokenAsLender = token.connect(lender)
 
-    await token.mint(lender.address, parseUSDC(10))
-    await token.mint(lender2.address, parseUSDC(10))
+    await token.mint(lender.address, parseUSDC(100))
+    await token.mint(lender2.address, parseUSDC(100))
+    await token.mint(lender3.address, parseUSDC(100))
   })
 
   describe('join', () => {
@@ -50,13 +52,13 @@ describe('ManagedPortfolio', () => {
       await tokenAsLender.approve(portfolio.address, parseUSDC(10))
       await portfolioAsLender.join(parseUSDC(10))
 
-      expect(await token.balanceOf(portfolio.address)).to.eq(parseUSDC(10))
+      expect(await token.balanceOf(portfolio.address)).to.equal(parseUSDC(10))
     })
 
     it('issues portfolio share tokens', async () => {
       await joinPortfolio(10, lender)
 
-      expect(await portfolio.balanceOf(lender.address)).to.eq(parseShares(10))
+      expect(await portfolio.balanceOf(lender.address)).to.equal(parseShares(10))
     })
 
     it('issues tokens for the second lender', async () => {
@@ -65,7 +67,33 @@ describe('ManagedPortfolio', () => {
 
       await joinPortfolio(10, lender2)
 
-      expect(await portfolio.balanceOf(lender2.address)).to.eq(parseShares(10))
+      expect(await portfolio.balanceOf(lender2.address)).to.equal(parseShares(20))
+    })
+
+    it('issues correct shares after pool value grows', async () => {
+      await joinPortfolio(10, lender)
+      await token.mint(portfolio.address, parseUSDC(5))
+
+      await joinPortfolio(10, lender2)
+
+      expect(await portfolio.balanceOf(lender.address)).to.equal(parseShares(10))
+      expect(await portfolio.balanceOf(lender2.address)).to.equal(parseShares(10).mul(10).div(15))
+    })
+
+    it('issues fewer shares per token deposited after the pool value grows', async () => {
+      await joinPortfolio(10, lender)
+      await joinPortfolio(30, lender2)
+      expect(await portfolio.balanceOf(lender.address)).to.equal(parseShares(10))
+      expect(await portfolio.balanceOf(lender2.address)).to.equal(parseShares(30))
+      
+      await token.mint(portfolio.address, parseUSDC(40)) // Doubles the pool value
+
+      await joinPortfolio(10, lender)
+      await joinPortfolio(20, lender2)
+      await joinPortfolio(20, lender3)
+      expect(await portfolio.balanceOf(lender.address)).to.equal(parseShares(10 + 5))
+      expect(await portfolio.balanceOf(lender2.address)).to.equal(parseShares(30 + 10))
+      expect(await portfolio.balanceOf(lender3.address)).to.equal(parseShares(10))
     })
   })
 
@@ -74,7 +102,7 @@ describe('ManagedPortfolio', () => {
       await joinPortfolio(10)
       await portfolio.createBulletLoan(0, borrower.address, parseUSDC(5), parseUSDC(6))
 
-      expect(await token.balanceOf(borrower.address)).to.eq(parseUSDC(5))
+      expect(await token.balanceOf(borrower.address)).to.equal(parseUSDC(5))
     })
 
     it('emits a proper event', async () => {
@@ -95,11 +123,11 @@ describe('ManagedPortfolio', () => {
   })
 
   it('withdraw sends tokens back to the lender', async () => {
-    await joinPortfolio(10)
+    await joinPortfolio(100)
 
-    await portfolioAsLender.withdraw(parseUSDC(5))
+    await portfolioAsLender.withdraw(parseUSDC(50))
 
-    expect(await token.balanceOf(lender.address)).to.eq(parseUSDC(5))
+    expect(await token.balanceOf(lender.address)).to.equal(parseUSDC(50))
   })
 
   async function joinPortfolio(amount: number, wallet: Wallet = lender) {
