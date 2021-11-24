b1e2c56647c1bfa8e992d90e6c263b096a9a8182
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2021-11-24 15:42:29 +0100

    🌌 Withdrawal with portfolio shares (#1146)
    * Test joining portfolio; tweak configuration
    
    * Initialize ManagedPortfolio and BulletLoan
    
    * Add repay in BulletLoans
    
    * prepare CI
    
    * update yarn lock
    
    * Add LP token
    
    * Compute a proper amount of lp-tokens when joining
    
    * Handle end date requirements
    
    * Rename join to deposit
    
    * Implement basic withdrawal logic
    
    * Implement withdraw sending tokens back to lender
    
    * Add Status to ManagedPortfolio
    
    * Make ManagedPortfolio Ownable
    
    * Add tests for owner to update status
    
    * Remove status logic
    
    * Add isClosed() view
    
    * Allow withdraw only on closed Portfolio
    
    Co-authored-by: Mikołaj Jakubowski <mikolaj.jakubowski@protonmail.com>
    
    * Fix lint
    
    Co-authored-by: Anze1m <bartek@ethworks.io>
    Co-authored-by: Shadi Barhoumi <shadi.barhoumi@trusttoken.com>
    Co-authored-by: Krzysztof Jelski <krzysztof@ethworks.io>
    Co-authored-by: Ivan <ivanruch@gmail.com>
    Co-authored-by: barrutko <barrutko.eth@gmail.com>
    Co-authored-by: Mikołaj Jakubowski <mikolaj.jakubowski@protonmail.com>

--- contracts/ragnarok/ManagedPortfolio.sol
+++ contracts/ragnarok/ManagedPortfolio.sol
@@ -13,12 +13,6 @@ interface IERC20WithDecimals is IERC20 {
 }
 
 contract ManagedPortfolio is IERC721Receiver, ERC20, Ownable {
-    enum Status {
-        Open,
-        Frozen,
-        Closed
-    }
-
     IERC20WithDecimals public underlyingToken;
     BulletLoans public bulletLoans;
     uint256 public endDate;
@@ -56,6 +50,7 @@ contract ManagedPortfolio is IERC721Receiver, ERC20, Ownable {
     }
 
     function withdraw(uint256 sharesAmount) external returns (uint256) {
+        require(isClosed(), "ManagedPortfolio: Cannot withdraw when Portfolio is not closed");
         uint256 liquidFunds = underlyingToken.balanceOf(address(this));
         uint256 amountToWithdraw = (sharesAmount * liquidFunds) / totalSupply();
         _burn(msg.sender, sharesAmount);
@@ -78,6 +73,10 @@ contract ManagedPortfolio is IERC721Receiver, ERC20, Ownable {
         underlyingToken.transfer(borrower, principalAmount);
         uint256 loanId = bulletLoans.createLoan(underlyingToken);
         emit BulletLoanCreated(loanId);
+    }
+
+    function isClosed() public view returns (bool) {
+        return block.timestamp > endDate;
     }
 
     function onERC721Received(
