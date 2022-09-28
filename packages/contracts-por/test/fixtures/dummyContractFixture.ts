ed13cd0e582e10f22a64753204e7d6d0bf33ccc6
Michał Sieczkowski <michal.sieczkowski@archblock.com>
2022-09-28 11:23:01 +0200

    Remove DummyContract and its test files

--- packages/contracts-por/test/fixtures/dummyContractFixture.ts
+++
@@ -1,7 +0,0 @@
-import { DummyContract__factory } from 'build/types'
-import { Wallet } from 'ethers'
-
-export async function dummyContractFixture([wallet]: Wallet[]) {
-  const dummyContract = await new DummyContract__factory(wallet).deploy()
-  return { dummyContract }
-}
