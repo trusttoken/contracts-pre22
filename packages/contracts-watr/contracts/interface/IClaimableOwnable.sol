cb8fc572a5f01b82239520936abe46b7fff3a778
abam-iksde <assdlik@gmail.com>
2023-03-02 11:38:02 +0100

    Remove constructor from interface

--- packages/contracts-watr/contracts/interface/IClaimableOwnable.sol
+++ packages/contracts-watr/contracts/interface/IClaimableOwnable.sol
@@ -4,11 +4,6 @@ pragma solidity 0.6.10;
 interface IClaimableOwnable {
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    constructor() public {
-        owner = msg.sender;
-        emit OwnershipTransferred(address(0), owner);
-    }
-
     function transferOwnership(address newOwner) external;
 
     function claimOwnership() external;
