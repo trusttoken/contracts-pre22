a39c7980ee15fe903ee58495001ee26e52968cda
abam-iksde <assdlik@gmail.com>
2023-03-09 11:58:29 +0100

    Use balance matchers in `verifyDeployment` tests

--- packages/contracts-watr/test/verifyDeployment/verifyDeployment.test.ts
+++ packages/contracts-watr/test/verifyDeployment/verifyDeployment.test.ts
@@ -1,9 +1,12 @@
 import { Contract, ethers } from 'ethers'
 import { JsonRpcProvider } from '@ethersproject/providers'
-import { expect } from 'chai'
+import { expect, use } from 'chai'
 import { unknown as deployments } from '../../deployments-watr_local.json'
 import { TokenControllerV3__factory, TrueUSD__factory } from 'contracts'
 import { parseEther } from '@ethersproject/units'
+import { solidity } from 'ethereum-waffle'
+
+use(solidity)
 
 describe('verify deployment', () => {
   const provider = new JsonRpcProvider('http://127.0.0.1:8822', 688)
@@ -49,13 +52,9 @@ describe('verify deployment', () => {
     const token = TrueUSD__factory.connect(deployments.trueUSD_proxy.address, deployer)
     const accountAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
 
-    const balanceBefore = await token.balanceOf(accountAddress)
+    const tx = () => waitFor(tokenController.instantMint(accountAddress, parseEther('1')))
 
-    await waitFor(tokenController.instantMint(accountAddress, parseEther('1')))
-
-    const balanceAfter = await token.balanceOf(accountAddress)
-
-    expect(balanceAfter.sub(balanceBefore).toString()).to.eq(parseEther('1').toString())
+    await expect(tx).to.changeTokenBalance(token, toAccount(accountAddress), parseEther('1'))
   })
 
   it('can transfer', async () => {
@@ -64,25 +63,18 @@ describe('verify deployment', () => {
     const token = TrueUSD__factory.connect(deployments.trueUSD_proxy.address, deployer)
     const otherAddress = '0x50D1c9771902476076eCFc8B2A83Ad6b9355a4c9'
 
-    const balancesBefore = [
-      await token.balanceOf(deployer.address),
-      await token.balanceOf(otherAddress),
-    ]
-
     await waitFor(tokenController.instantMint(deployer.address, parseEther('1')))
 
-    await waitFor(token.transfer(otherAddress, parseEther('0.5')))
+    const tx = () => waitFor(token.transfer(otherAddress, parseEther('0.5')))
 
-    const balancesAfter = [
-      await token.balanceOf(deployer.address),
-      await token.balanceOf(otherAddress),
-    ]
-
-    expect(balancesAfter[0].sub(balancesBefore[0]).toString()).to.eq(parseEther('0.5').toString())
-    expect(balancesAfter[1].sub(balancesBefore[1]).toString()).to.eq(parseEther('0.5').toString())
+    await expect(tx).to.changeTokenBalances(token, [deployer, toAccount(otherAddress)], [parseEther('-0.5'), parseEther('0.5')])
   }).timeout(100000)
 })
 
 async function waitFor<T>(tx: Promise<{ wait: () => Promise<T> }>) {
   return (await tx).wait()
+}
+
+function toAccount(address: string) {
+  return { getAddress: () => address }
 }
