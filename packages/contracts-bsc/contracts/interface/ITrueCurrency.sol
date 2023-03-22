281eb003a136d16c647945328b8f1e8396b314d5
qiwihui <qwh005007@gmail.com>
2023-03-22 11:50:25 +0800

    feat: Add IBEP20 and Change ERC20 to BEP20

--- packages/contracts-bsc/contracts/interface/ITrueCurrency.sol
+++ packages/contracts-bsc/contracts/interface/ITrueCurrency.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.10;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IBEP20} from "./IBEP20.sol";
 
 interface ITrueCurrency {
     function mint(address account, uint256 amount) external;
@@ -14,7 +14,7 @@ interface ITrueCurrency {
 
     function reclaimEther(address payable _to) external;
 
-    function reclaimToken(IERC20 token, address _to) external;
+    function reclaimToken(IBEP20 token, address _to) external;
 
     function setBlacklisted(address account, bool isBlacklisted) external;
 }
