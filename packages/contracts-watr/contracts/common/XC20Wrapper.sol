ede1393684d79cb5d6998fc4a18a5cd18af585d8
abam-iksde <assdlik@gmail.com>
2023-04-13 14:09:51 +0200

    Reorder `XC20Wrapper` functions to match `IERC20` interface

--- packages/contracts-watr/contracts/common/XC20Wrapper.sol
+++ packages/contracts-watr/contracts/common/XC20Wrapper.sol
@@ -12,23 +12,17 @@ import {IERC20Plus} from "../interface/IERC20Plus.sol";
 abstract contract XC20Wrapper is IERC20, ClaimableOwnable, Context {
     using SafeMath for uint256;
 
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "XC20: mint to the zero address");
-        IERC20Plus(nativeToken).mint(account, amount);
-        emit Transfer(address(0), account, amount);
-    }
-
-    function _burn(address account, uint256 amount) internal virtual {
-        IERC20Plus(nativeToken).burn(account, amount);
-        emit Transfer(account, address(0), amount);
-    }
-
-    function decimals() public view virtual returns (uint8) {
-        return IERC20Plus(nativeToken).decimals();
-    }
-
     function totalSupply() public view virtual override returns (uint256) {
         return IERC20Plus(nativeToken).totalSupply();
+    }
+
+    function balanceOf(address account) external view virtual override returns (uint256) {
+        return IERC20Plus(nativeToken).balanceOf(account);
+    }
+
+    function transfer(address recipient, uint256 amount) external virtual override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
     }
 
     function allowance(address owner, address spender) external view virtual override returns (uint256) {
@@ -40,41 +34,12 @@ abstract contract XC20Wrapper is IERC20, ClaimableOwnable, Context {
         return true;
     }
 
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual {
-        require(owner != address(0) && spender != address(0), "XC20: approve to the zero address");
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
     function decreaseAllowance(address spender, uint256 amount) external virtual {
         _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(amount, "XC20: decreased allowance below zero"));
     }
 
     function increaseAllowance(address spender, uint256 amount) external virtual {
         _approve(msg.sender, spender, _allowances[msg.sender][spender].add(amount));
-    }
-
-    function balanceOf(address account) external view virtual override returns (uint256) {
-        return IERC20Plus(nativeToken).balanceOf(account);
-    }
-
-    function _transfer(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal virtual {
-        require(recipient != address(0), "XC20: transfer to the zero address");
-        _forceTransfer(sender, recipient, amount);
-        emit Transfer(sender, recipient, amount);
-    }
-
-    function transfer(address recipient, uint256 amount) external virtual override returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        return true;
     }
 
     function transferFrom(
@@ -88,14 +53,8 @@ abstract contract XC20Wrapper is IERC20, ClaimableOwnable, Context {
         return true;
     }
 
-    function _forceTransfer(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal virtual {
-        require(IERC20Plus(nativeToken).balanceOf(sender) >= amount, "XC20: amount exceeds balance");
-        IERC20Plus(nativeToken).burn(sender, amount);
-        IERC20Plus(nativeToken).mint(recipient, amount);
+    function decimals() public view virtual returns (uint8) {
+        return IERC20Plus(nativeToken).decimals();
     }
 
     function name() public pure virtual returns (string memory) {
@@ -104,5 +63,46 @@ abstract contract XC20Wrapper is IERC20, ClaimableOwnable, Context {
 
     function symbol() public pure virtual returns (string memory) {
         return "";
+    }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "XC20: mint to the zero address");
+        IERC20Plus(nativeToken).mint(account, amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual {
+        IERC20Plus(nativeToken).burn(account, amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
+        require(recipient != address(0), "XC20: transfer to the zero address");
+        _forceTransfer(sender, recipient, amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0) && spender != address(0), "XC20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _forceTransfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
+        require(IERC20Plus(nativeToken).balanceOf(sender) >= amount, "XC20: amount exceeds balance");
+        IERC20Plus(nativeToken).burn(sender, amount);
+        IERC20Plus(nativeToken).mint(recipient, amount);
     }
 }
