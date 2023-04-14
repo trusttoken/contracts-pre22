aa2e08b2261eb9dffbe6086052a5107f2f431062
abam-iksde <assdlik@gmail.com>
2023-04-14 10:19:47 +0200

    Further lower amount in `Pool Curve integration` test

--- test/integration/poolExchanges.test.ts
+++ test/integration/poolExchanges.test.ts
@@ -9,6 +9,6 @@ use(solidity)
 describe('Pool Curve integration', () => {
   it('deposit TUSD to Curve', async () => {
     const pool = await upgradeSuite(TrueFiPool__factory, '0xa1e72267084192Db7387c8CC1328fadE470e4149', [])
-    await expect(pool.flush(parseUnits('15000', 18))).to.be.not.reverted
+    await expect(pool.flush(parseUnits('1000', 18))).to.be.not.reverted
   })
 })
