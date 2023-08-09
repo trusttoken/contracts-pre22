21a6ade5f474cdb33fe49006aa772f914343cab4
qiwihui <qwh005007@gmail.com>
2023-08-09 14:12:02 +0800

    fix: add mock contracts for test

---
+++ packages/contracts-tron/contracts/mocks/RegistryMock.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import {Registry} from "../Registry.sol";
+
+contract RegistryMock is Registry {
+
+}
---
+++ packages/contracts-tron/contracts/mocks/TokenControllerMock.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import {OwnedUpgradeabilityProxy} from "../OwnedUpgradeabilityProxy.sol";
+import {Registry} from "../Registry.sol";
+
+import {TrueCurrency} from "../common/TrueCurrency.sol";
+
+import {TokenController} from "../TronTokenController.sol";
+
+interface HasOwner {
+    function claimOwnership() external;
+
+    function transferOwnership(address newOwner) external;
+}
+
+/**
+ * Token Controller with custom init function for testing
+ */
+contract TokenControllerMock is TokenController {
+
+    // initalize controller. useful for tests
+    function initialize_owner() public {
+        require(owner == address(0));
+        owner = msg.sender;
+    }
+
+    // initialize with paramaters. useful for tests
+    // sets initial paramaters on testnet
+    function initializeWithParams(TrueCurrency _token, Registry _registry) external {
+        require(!initialized, "already initialized");
+        owner = msg.sender;
+        initialized = true;
+        token = _token;
+        emit SetToken(_token);
+        registry = _registry;
+        emit SetRegistry(address(_registry));
+        //gasRefunder = owner;
+        //registryAdmin = owner;
+        // set mint limits & thresholds
+        // instant = 1M, ratified = 10M, multisig = 100M
+        uint256 instant = 1000000000000000000000000;
+        uint256 ratified = 10000000000000000000000000;
+        uint256 multiSig = 100000000000000000000000000;
+        instantMintThreshold = instant;
+        ratifiedMintThreshold = ratified;
+        multiSigMintThreshold = multiSig;
+        instantMintLimit = instant;
+        ratifiedMintLimit = ratified;
+        multiSigMintLimit = multiSig;
+        instantMintPool = instant;
+        ratifiedMintPool = ratified;
+        multiSigMintPool = multiSig;
+        emit MintThresholdChanged(instant, ratified, multiSig);
+        emit MintLimitsChanged(instant, ratified, multiSig);
+        emit InstantPoolRefilled();
+        emit RatifyPoolRefilled();
+        emit MultiSigPoolRefilled();
+    }
+
+}
+
+contract TokenControllerPauseMock is TokenControllerMock {
+    address public pausedImplementation;
+
+    function setPausedImplementation(address _pausedToken) external {
+        pausedImplementation = _pausedToken;
+    }
+
+    /**
+     * @dev pause all pausable actions on TrueUSD, mints/burn/transfer/approve
+     */
+    function pauseToken() external override onlyOwner {
+        OwnedUpgradeabilityProxy(uint160(address(token))).upgradeTo(pausedImplementation);
+    }
+}
